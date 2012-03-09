097010a403ca681576da68fa8e5bf3fbe854a171
Tony Burdett <tburdett@ebi.ac.uk>
2012-03-09 15:52:56 +0000

    goci-pussycat: adding reasoner initialization checks so that the webapp can startup and provide updates whilst reasoner classifies in the background

--- goci-diagram/goci-pussycat/src/main/java/uk/ac/ebi/fgpt/goci/pussycat/session/CacheableReasonerSession.java
+++ goci-diagram/goci-pussycat/src/main/java/uk/ac/ebi/fgpt/goci/pussycat/session/CacheableReasonerSession.java
@@ -1,13 +1,12 @@
 package uk.ac.ebi.fgpt.goci.pussycat.session;
 
 import com.googlecode.ehcache.annotations.Cacheable;
-import net.sf.ehcache.CacheManager;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import uk.ac.ebi.fgpt.goci.exception.OWLConversionException;
+import uk.ac.ebi.fgpt.goci.lang.Initializable;
 import uk.ac.ebi.fgpt.goci.service.GWASOWLPublisher;
 
 /**
@@ -16,7 +15,7 @@ import uk.ac.ebi.fgpt.goci.service.GWASOWLPublisher;
  * @author Tony Burdett
  * @date 05/03/12
  */
-public class CacheableReasonerSession implements ReasonerSession {
+public class CacheableReasonerSession extends Initializable implements ReasonerSession {
     private GWASOWLPublisher publisher;
 
     private Logger log = LoggerFactory.getLogger(getClass());
@@ -25,12 +24,22 @@ public class CacheableReasonerSession implements ReasonerSession {
         return log;
     }
 
+    @Override
+    protected void doInitialization() throws Exception {
+        getLog().info("Initializing reasoner session... this may take some time");
+        getReasoner();
+    }
+
     public GWASOWLPublisher getPublisher() {
         return publisher;
     }
 
     public void setPublisher(GWASOWLPublisher publisher) {
         this.publisher = publisher;
+    }
+
+    @Override public boolean isReasonerInitialized() {
+        return isReady();
     }
 
     @Override
--- goci-diagram/goci-pussycat/src/main/java/uk/ac/ebi/fgpt/goci/pussycat/session/ReasonerSession.java
+++ goci-diagram/goci-pussycat/src/main/java/uk/ac/ebi/fgpt/goci/pussycat/session/ReasonerSession.java
@@ -2,6 +2,7 @@ package uk.ac.ebi.fgpt.goci.pussycat.session;
 
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import uk.ac.ebi.fgpt.goci.exception.OWLConversionException;
+import uk.ac.ebi.fgpt.goci.pussycat.exception.PussycatSessionNotReadyException;
 
 /**
  * A cacheable reasoner session that can be used to load reasoners for the GWAS ontology given a GWASOWLPublisher.
@@ -10,6 +11,20 @@ import uk.ac.ebi.fgpt.goci.exception.OWLConversionException;
  * @date 05/03/12
  */
 public interface ReasonerSession {
+    /**
+     * Returns true if this reasoner session is fully initialized and able to respond to a request to {@link
+     * #getReasoner()}, and false if the reasoner is not yet ready.
+     * <p/>
+     * Implementations are free to define their own strategies for initializing reasoners - some may choose to lazily
+     * initialize the reasoner, whereas others may prefer up-front initialization to occur in a parallel thread.
+     * Regardless of strategy, this method should return true or false immediately, whereas calls to {@link
+     * #getReasoner()} must block whilst the reasoner initializes.  This method provides a means for clients to avoid
+     * being blocked in cases where the reasoner can be initialized in the background.
+     *
+     * @return true if the reasoner for this session is fully initialized, false otherwise
+     */
+    boolean isReasonerInitialized();
+
     /**
      * Acquire a reasoner that serves up a reasoned view of the GWAS OWL ontology
      *
