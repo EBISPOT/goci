e9beb4946a7cd3ce5fb43f2a0f925ed7bc95a876
Dani Welter <dwelter@ebi.ac.uk>
2012-08-02 14:44:57 +0100

    Larger knowledge base

---
+++ goci-diagram/goci-pussycat-renderlet/src/main/java/uk/ac/ebi/fgpt/goci/pussycat/layout/CoordinateMapper.java
@@ -0,0 +1,11 @@
+package uk.ac.ebi.fgpt.goci.pussycat.layout;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dwelter
+ * Date: 02/08/12
+ * Time: 13:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class CoordinateMapper {
+}
