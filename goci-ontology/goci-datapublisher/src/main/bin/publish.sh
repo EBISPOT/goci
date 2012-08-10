70c07fdd08baa0546060cd4b35846dcc290e853c
Tony Burdett <tburdett@ebi.ac.uk>
2012-08-10 11:48:26 +0100

    goci: configuring profiles for modifying scripts by deployment environment

--- goci-ontology/goci-datapublisher/src/main/bin/publish.sh
+++ goci-ontology/goci-datapublisher/src/main/bin/publish.sh
@@ -2,8 +2,8 @@
 
 base=${0%/*}/..;
 current=`pwd`;
-java=/ebi/research/software/Linux_x86_64/opt/java/jdk1.7/bin/java;
-args="-Dhttp.proxyHost=wwwcache.ebi.ac.uk -Dhttp.proxyPort=3128 -Dhttp.nonProxyHosts=*.ebi.ac.uk -DproxyHost=wwwcache.ebi.ac.uk -DproxyPort=3128 -DproxySet=true -Xmx16g";
+java=${java.location};
+args="${java.args}";
 
 for file in `ls $base/lib`
 do
