b99c180dd8913ca9be8fbe752bb6d6d906402268
Tony Burdett <tburdett@ebi.ac.uk>
2013-10-15 12:09:54 +0100

    debugging logging tuning and tidyig up

--- goci-diagram/goci-pussycat-metrics/src/main/java/uk/ac/ebi/fgpt/goci/pussycat/GOCIPussycatMetricsDriver.java
+++ goci-diagram/goci-pussycat-metrics/src/main/java/uk/ac/ebi/fgpt/goci/pussycat/GOCIPussycatMetricsDriver.java
@@ -30,11 +30,6 @@ import java.util.*;
 public class GOCIPussycatMetricsDriver {
 
     public static void main(String[] args) {
-//        try {
-//            // parse arguments
-//            int parseArgs = parseArguments(args);
-//            if (parseArgs == 0) {
-//                // execute publisher
         GOCIPussycatMetricsDriver driver = new GOCIPussycatMetricsDriver();
         try {
             driver.runBenchmark();
@@ -44,104 +39,10 @@ public class GOCIPussycatMetricsDriver {
             e.printStackTrace();
             System.exit(1);
         }
-//            }
-//            else {
-//                // could not parse arguments, exit with exit code >1 (depending on parsing problem)
-//                System.err.println("Failed to parse supplied arguments");
-//                System.exit(1 + parseArgs);
-//            }
-//        }
-//        catch (Exception e) {
-//            // failed to execute, exit with exit code 1
-//            System.err.println("An unexpected error occurred\n\t(" + e.getMessage() + ")");
-//            System.exit(1);
-//        }
     }
-
-//    private static int parseArguments(String[] args) {
-//        CommandLineParser parser = new GnuParser();
-//        HelpFormatter help = new HelpFormatter();
-//        Options options = bindOptions();
-//
-//        int parseArgs = 0;
-//        try {
-//            CommandLine cl = parser.parse(options, args, true);
-//
-//            // check for mode help option
-//            if (cl.hasOption("")) {
-//                // print out mode help
-//                help.printHelp("benchmark", options, true);
-//                parseArgs += 1;
-//            }
-//            else {
-//                // find -o option (for asserted output file)
-//                if (cl.hasOption("o")) {
-////                    String assertedOutputFileName = cl.getOptionValue("o");
-////         //           assertedOntologyFile = new File(assertedOutputFileName);
-////
-////                    if (cl.hasOption("i")) {
-////                        String inferredOutputFileName = cl.getOptionValue("i");
-////                        inferredOntologyFile = new File(inferredOutputFileName);
-////                    }
-////
-////                    if(cl.hasOption("p")){
-////                        String pvalueFilter = cl.getOptionValue("p");
-////           //             FilterProperties.setPvalueFilter(pvalueFilter);
-////                    }
-////
-////                    if(cl.hasOption("d")) {
-////                        String dateFilter = cl.getOptionValue("d");
-////             //           FilterProperties.setDateFilter(dateFilter);
-////                    }
-//                }
-//                else {
-//                    System.err.println("-o (ontology output file) argument is required");
-//                    help.printHelp("benchmark", options, true);
-//                    parseArgs += 2;
-//                }
-//            }
-//        }
-//        catch (ParseException e) {
-//            System.err.println("Failed to read supplied arguments");
-//            help.printHelp("publish", options, true);
-//            parseArgs += 4;
-//        }
-//        return parseArgs;
-//    }
-//
-//    private static Options bindOptions() {
-//        Options options = new Options();
-//
-//        // help
-//        Option helpOption = new Option("h", "help", false, "Print the help");
-//        options.addOption(helpOption);
-//
-//        // add output file arguments
-//        Option outputFileOption = new Option("o", "output", true,
-//                "The output file to write the published ontology to");
-//        outputFileOption.setArgName("file");
-//        outputFileOption.setRequired(true);
-//        options.addOption(outputFileOption);
-//
-//        Option inferredOutputFileOption = new Option("i", "inferred", true,
-//                "The output file to write the inferred version of the published ontology to");
-//        inferredOutputFileOption.setArgName("file");
-//        options.addOption(inferredOutputFileOption);
-//
-//        Option pvalueFilterOption = new Option("p", "pvalue", true, "The minimum p-value on which to filter the
-// knowledge base, in format nE-x, e.g. 5E-8");
-//        options.addOption(pvalueFilterOption);
-//
-//        Option dateFilterOption = new Option("d", "date", true, "The date on which to filter the knowledge base,
-// in format YYYY-MM-DD");
-//        options.addOption(dateFilterOption);
-//
-//        return options;
-//    }
 
     private OntologyConfiguration config;
     private ReasonerSession reasonerSession;
-    //    private BenchmarkPussycatSession pussycatSession;
     private PussycatSession pussycatSession;
     private PussycatManager pussycatManager;
     private RenderletNexus renderletNexus;
@@ -154,14 +55,12 @@ public class GOCIPussycatMetricsDriver {
         return log;
     }
 
-
     public GOCIPussycatMetricsDriver() {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("goci-pussycat.xml");
         config = ctx.getBean("config", OntologyConfiguration.class);
         reasonerSession = ctx.getBean("reasonerSession", OntologyLoadingCacheableReasonerSession.class);
         pussycatSession = ctx.getBean("pussycatSession", PussycatSession.class);
         pussycatManager = ctx.getBean("pussycatManager", PussycatManager.class);
-
     }
 
     public void runBenchmark() throws IOException {
@@ -171,9 +70,9 @@ public class GOCIPussycatMetricsDriver {
         start = System.currentTimeMillis();
         while (!reasonerSession.isReasonerInitialized()) {
             try {
-                getLog().info("No reasoner session available, holding");
+                getLog().debug("No reasoner session available, holding");
                 synchronized (this) {
-                    wait(30000);
+                    wait(300000);
                 }
             }
             catch (InterruptedException e) {
@@ -198,26 +97,26 @@ public class GOCIPussycatMetricsDriver {
 
         while (queryDates.hasMoreElements()) {
             query = createClassExpression((String) dates.get(queryDates.nextElement()));
-            bm_log.info(DateTimeStamp.getCurrentTimeStamp() + " Acquiring SVG for OWL class expression " + query);
+            bm_log.info(DateTimeStamp.getCurrentTimeStamp() + " Acquiring SVG for OWL class expression '" + query + "'");
             String svg = "";
             boolean nexusReady = true;
             do {
                 try {
-                    getLog().info("Performing SVG rendering for '" + query + "'");
+                    getLog().debug("Performing SVG rendering for '" + query + "'");
                     svg = pussycatSession.performRendering(query, renderletNexus);
                 }
                 catch (PussycatSessionNotReadyException e) {
-                    getLog().error("Pussycat session not ready yet - waiting to generate SVG for '" + query + "'");
+                    getLog().debug("Pussycat session not ready yet - waiting to generate SVG for '" + query + "'");
                     nexusReady = false;
                     synchronized (this) {
                         try {
-                            wait(30000);
+                            wait(300000);
                         }
                         catch (InterruptedException e1) {
                             // do nothing
                         }
                     }
-                    getLog().info("Retrying query for '" + query + "'");
+                    getLog().debug("Retrying query for '" + query + "'");
                 }
             } while (!nexusReady);
             allSVG.put(query, svg);
@@ -287,5 +186,3 @@ public class GOCIPussycatMetricsDriver {
         }
     }
 }
-
-
