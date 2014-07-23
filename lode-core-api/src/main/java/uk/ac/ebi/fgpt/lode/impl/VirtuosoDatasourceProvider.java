d40c83352a0a21a43b0d4e381b0c22bb8db81def
Tony Burdett <tburdett@ebi.ac.uk>
2014-07-23 12:10:37 +0100

    refactoring VirtuosoDatasourceProvdier to create new connection pool rather than performing JNDI lookup and overwriting

--- lode-core-api/src/main/java/uk/ac/ebi/fgpt/lode/impl/VirtuosoDatasourceProvider.java
+++ lode-core-api/src/main/java/uk/ac/ebi/fgpt/lode/impl/VirtuosoDatasourceProvider.java
@@ -12,17 +12,9 @@
 
 package uk.ac.ebi.fgpt.lode.impl;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import uk.ac.ebi.fgpt.lode.utils.DatasourceProvider;
 import virtuoso.jdbc4.VirtuosoConnectionPoolDataSource;
-import virtuoso.jdbc4.VirtuosoDataSource;
 
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.ConnectionPoolDataSource;
 import javax.sql.DataSource;
 import java.sql.SQLException;
 
@@ -34,42 +26,66 @@ import java.sql.SQLException;
 public class VirtuosoDatasourceProvider implements DatasourceProvider {
 
 
-    private Logger logger = LoggerFactory.getLogger(getClass());
+//    private Logger logger = LoggerFactory.getLogger(getClass());
+//
+//    private VirtuosoDataSource virtuosoSource = null;
+//
+//    public VirtuosoDatasourceProvider(){
+//
+//        // Get the DataSource
+//        if (virtuosoSource == null) {
+//            try {
+//                Context context = (Context) (new InitialContext()).lookup("java:comp/env");
+//                virtuosoSource = (VirtuosoDataSource) context.lookup("jdbc/virtuoso");
+//
+//            } catch (NamingException e) {
+//                throw new IllegalStateException("Virtuoso JNDI datasource not configured: " + e.getMessage());
+//            }
+//        }
+//    }
+//
+//    public VirtuosoDatasourceProvider(String endpointUrl, int port){
+//
+//        // Get the DataSource
+//        if (virtuosoSource == null) {
+//            try {
+//                Context context = (Context) (new InitialContext()).lookup("java:comp/env");
+//                virtuosoSource = (VirtuosoDataSource) context.lookup("jdbc/virtuoso");
+//
+//                virtuosoSource.setServerName(endpointUrl);
+//                virtuosoSource.setPortNumber(port);
+//            } catch (NamingException e) {
+//                throw new IllegalStateException("Virtuoso JNDI datasource not configured: " + e.getMessage());
+//            }
+//        }
+//    }
 
-    private VirtuosoDataSource virtuosoSource = null;
+    private final VirtuosoConnectionPoolDataSource virtuosoSource;
 
-    public VirtuosoDatasourceProvider(){
-
-        // Get the DataSource
-        if (virtuosoSource == null) {
-            try {
-                Context context = (Context) (new InitialContext()).lookup("java:comp/env");
-                virtuosoSource = (VirtuosoDataSource) context.lookup("jdbc/virtuoso");
-
-            } catch (NamingException e) {
-                throw new IllegalStateException("Virtuoso JNDI datasource not configured: " + e.getMessage());
-            }
+    public VirtuosoDatasourceProvider() {
+        try {
+            VirtuosoConnectionPoolDataSource ds = new VirtuosoConnectionPoolDataSource();
+            ds.setInitialPoolSize(0);
+            ds.setMinPoolSize(0);
+            ds.setMaxPoolSize(4);
+            ds.setCharset("UTF-8");
+            ds.setUser("dba");
+            ds.setPassword("dba");
+            virtuosoSource = ds;
+        }
+        catch (SQLException e) {
+            throw new IllegalStateException("Failed to create " + getClass().getSimpleName() + ": " + e.getMessage());
         }
     }
 
-    public VirtuosoDatasourceProvider(String endpointUrl, int port){
-
-        // Get the DataSource
-        if (virtuosoSource == null) {
-            try {
-                Context context = (Context) (new InitialContext()).lookup("java:comp/env");
-                virtuosoSource = (VirtuosoDataSource) context.lookup("jdbc/virtuoso");
-
-                virtuosoSource.setServerName(endpointUrl);
-                virtuosoSource.setPortNumber(port);
-            } catch (NamingException e) {
-                throw new IllegalStateException("Virtuoso JNDI datasource not configured: " + e.getMessage());
-            }
-        }
+    public VirtuosoDatasourceProvider(String endpointUrl, int port) {
+        this();
+        // now override server name and port number
+        virtuosoSource.setServerName(endpointUrl);
+        virtuosoSource.setPortNumber(port);
     }
 
     public DataSource getDataSource() throws SQLException {
         return virtuosoSource;
     }
-
 }
