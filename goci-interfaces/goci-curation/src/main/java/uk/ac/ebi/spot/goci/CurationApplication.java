ddc5b9eaabd2a5998d7cd948c2d1681c7cfc45a8
Tony Burdett <tburdett@ebi.ac.uk>
2015-01-14 14:38:31 +0000

    curation interface refactored to use refactored modules

---
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/CurationApplication.java
@@ -0,0 +1,11 @@
+package uk.ac.ebi.spot.goci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CurationApplication.class, args);
+    }
+}
--- goci-interfaces/goci-curation/src/test/java/uk/ac/ebi/spot/goci/curation/ApplicationTests.java
+++ goci-interfaces/goci-curation/src/test/java/uk/ac/ebi/spot/goci/curation/ApplicationTests.java
@@ -5,9 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import uk.ac.ebi.spot.goci.CurationApplication;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+@SpringApplicationConfiguration(classes = CurationApplication.class)
 @WebAppConfiguration
 public class ApplicationTests {
 
