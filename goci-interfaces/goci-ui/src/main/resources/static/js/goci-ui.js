4fef0b2ec194ae5910be449b470019c6dd60cf2e
Dani Welter <dwelter@ebi.ac.uk>
2015-01-14 09:54:25 +0000

    Started to do some basic js stuff

--- goci-interfaces/goci-ui/src/main/resources/static/js/goci-ui.js
+++ goci-interfaces/goci-ui/src/main/resources/static/js/goci-ui.js
@@ -1,3 +1,41 @@
-/**
- * Created by dwelter on 09/01/15.
- */
+
+$(document).ready(function(){
+    $('#search-button').click(function(){
+        $('#lower_container').show();
+        $('#search-term').text($('#search-box').val());
+    })
+
+});
+
+function toggleSidebar(ts){
+   
+        if ($(ts).hasClass('panel-collapsed')) {
+            // expand the panel
+            $(ts).parents('.panel').find('.panel-body').slideDown();
+            $(ts).removeClass('panel-collapsed');
+            $(ts).find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+            $(ts).parents('#filter-bar').removeClass('col-md-1').addClass('col-md-3');
+            $(ts).parents('#filter-bar').siblings('#results-area').removeClass('col-md-11').addClass('col-md-9');
+
+        }
+        else {
+            // collapse the panel
+            $(ts).parents('.panel').find('.panel-body').slideUp();
+            $(ts).addClass('panel-collapsed');
+            $(ts).find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+            $(ts).parents('#filter-bar').removeClass('col-md-3').addClass('col-md-1');
+            $(ts).parents('#filter-bar').siblings('#results-area').removeClass('col-md-9').addClass('col-md-11');
+        }
+} ;
+
+
+//$(document).ready(function(){
+//    $('#search-button').click(function(){
+//        alert($('#search-box').val());
+//    })
+//
+//    //
+//    //$('#lower_container').show();
+//    $('#search-term').text($('#search-box').val())
+//
+//});
