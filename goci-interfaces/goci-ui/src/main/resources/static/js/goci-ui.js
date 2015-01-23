ec93ee430c431f018653f85d8f67c5fabde26ffb
Tony Burdett <tburdett@ebi.ac.uk>
2015-01-23 12:36:15 +0000

    index.html now themes up as required with pass 1 at colours

--- goci-interfaces/goci-ui/src/main/resources/static/js/goci-ui.js
+++ goci-interfaces/goci-ui/src/main/resources/static/js/goci-ui.js
@@ -14,6 +14,12 @@
                   doSearch()
 
           });
+
+        $('.toplevel-view').hover(function() {
+            $(this).addClass("background-color-complementary-accent", 300, "easeOutExpo");
+        }, function() {
+            $(this).removeClass("background-color-complementary-accent", 300, "easeOutExpo");
+        });
     });
 
 //};
