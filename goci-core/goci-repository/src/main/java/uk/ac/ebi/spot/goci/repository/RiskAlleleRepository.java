5b302d2c2c56713d2fe1e941f665fa05816c07a0
emma-ebi <emma@ebi.ac.uk>
2015-01-27 17:15:24 +0000

    Repository to allow access to risk allele table

---
+++ goci-core/goci-repository/src/main/java/uk/ac/ebi/spot/goci/repository/RiskAlleleRepository.java
@@ -0,0 +1,17 @@
+package uk.ac.ebi.spot.goci.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import uk.ac.ebi.spot.goci.model.RiskAllele;
+
+/**
+ * Created by emma on 27/01/15.
+ *
+ * @author emma
+ *         <p/>
+ *         Repository accessing RiskAllele entity object
+ */
+@RepositoryRestResource
+public interface RiskAlleleRepository extends JpaRepository<RiskAllele, Long> {
+    RiskAllele findByRiskAlleleName(String riskAlleleName);
+}
