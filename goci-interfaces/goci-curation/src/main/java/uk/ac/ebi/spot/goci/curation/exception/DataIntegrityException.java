1e881a3a2f765725871b712ed9f21886a497a04a
emma-ebi <emma@ebi.ac.uk>
2015-01-29 12:14:01 +0000

    New exception, for case where curator tries to assign a snp to a risk allele that already has a snp attached

---
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/exception/DataIntegrityException.java
@@ -0,0 +1,27 @@
+package uk.ac.ebi.spot.goci.curation.exception;
+
+/**
+ * Created by emma on 29/01/15.
+ */
+public class DataIntegrityException extends RuntimeException {
+
+    public DataIntegrityException() {
+        super();
+    }
+
+    public DataIntegrityException(String message) {
+        super(message);
+    }
+
+    public DataIntegrityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DataIntegrityException(Throwable cause) {
+        super(cause);
+    }
+
+    protected DataIntegrityException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
