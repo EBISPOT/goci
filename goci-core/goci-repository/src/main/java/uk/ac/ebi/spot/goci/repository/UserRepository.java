682f715b2221c65f50f78c6a3771c1af536629aa
emma-ebi <emma@ebi.ac.uk>
2015-02-10 11:46:26 +0000

    New Role repository, added some comments to User repository

--- goci-core/goci-repository/src/main/java/uk/ac/ebi/spot/goci/repository/UserRepository.java
+++ goci-core/goci-repository/src/main/java/uk/ac/ebi/spot/goci/repository/UserRepository.java
@@ -6,6 +6,9 @@ import uk.ac.ebi.spot.goci.model.User;
 
 /**
  * Created by emma on 09/02/15.
+ * @author emma
+ *
+ * Repository accessing User entity object
  */
 @RepositoryRestResource
 public interface UserRepository extends JpaRepository<User,Long> {
