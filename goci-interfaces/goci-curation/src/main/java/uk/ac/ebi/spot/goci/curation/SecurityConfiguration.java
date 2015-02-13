bfc91f45710ebd1f9f7888e730c1543479f39eae
emma-ebi <emma@ebi.ac.uk>
2015-02-13 12:52:50 +0000

    Disabled csrf

--- goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/SecurityConfiguration.java
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/SecurityConfiguration.java
@@ -26,7 +26,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                .csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/users/**").hasAuthority("admin")
                 .anyRequest().authenticated()
                 .and()
