5ebc7eb2f134e98cbc362dc41d2d76540a07b327
emma-ebi <emma@ebi.ac.uk>
2015-02-18 11:42:29 +0000

    Download catalog functionality

--- goci-tools/goci-catalog-data-processor/src/main/java/uk/ac/ebi/spot/goci/DownloadApplication.java
+++ goci-tools/goci-catalog-data-processor/src/main/java/uk/ac/ebi/spot/goci/DownloadApplication.java
@@ -8,25 +8,30 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import uk.ac.ebi.spot.goci.service.ProcessView;
 
+import java.util.List;
+
 /**
  * Created by emma on 17/02/15.
  */
 @SpringBootApplication
-public class Application {
+public class DownloadApplication {
 
     @Autowired ProcessView processView;
 
     public static void main(String[] args) {
         System.out.println("Starting catalog download service...");
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        ApplicationContext ctx = SpringApplication.run(DownloadApplication.class, args);
         System.out.println("Application executed successfully!");
         SpringApplication.exit(ctx);
     }
 
-    @Bean CommandLineRunner run() {
+    @Bean CommandLineRunner run(String... args) {
         return strings -> {
-            processView.createFileForNcbi();
-            System.out.print("Converting all GWAS database objects...");
+            System.out.print("Querying database for studies ready to send to NCBI...");
+            List<String> viewsAsStrings = processView.serialiseViews();
+            System.out.print("Querying database for studies ready to send to NCBI...");
+            String fileName = args.toString();
+            processView.createFileForNcbi(fileName, viewsAsStrings);
             System.out.println("done!\n");
         };
     }
--- goci-tools/goci-catalog-data-processor/src/main/java/uk/ac/ebi/spot/goci/service/ProcessView.java
+++ goci-tools/goci-catalog-data-processor/src/main/java/uk/ac/ebi/spot/goci/service/ProcessView.java
@@ -4,10 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.ac.ebi.spot.goci.model.CatalogSummaryView;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by emma on 17/02/15.
+ *
+ * @author emma
  */
 @Service
 public class ProcessView {
@@ -19,11 +27,89 @@ public class ProcessView {
         this.ncbiCatalogService = ncbiCatalogService;
     }
 
-    public void createFileForNcbi() {
+    public List<String> serialiseViews() {
 
         Collection<CatalogSummaryView> views = ncbiCatalogService.getCatalogSummaryViewsSendToNcbi();
+        List<String> viewAsStrings = new ArrayList<String>();
 
+        for (CatalogSummaryView view : views) {
+
+            String line = "";
+
+            String dateAddedToCatalog = view.getCatalogAddedDate().toString() + "\t";
+            String pubmedId = view.getPubmedId() + "\t";
+            String firstAuthor = view.getAuthor() + "\t";
+            String date = view.getPublicationDate().toString() + "\t";
+            String journal = view.getJournal() + "\t";
+            String link = view.getLink() + "\t";
+            String study = view.getStudy() + "\t";
+            String diseaseTrait = view.getDiseaseTrait() + "\t";
+            String initialSampleSize = view.getInitialSampleDescription() + "\t";
+            String replicateSampleSize = view.getReplicateSampleDescription() + "\t";
+            String region = view.getRegion() + "\t";
+            String reportedGenes = view.getReportedGene() + "\t";
+            String strongestSnpRiskAllele = view.getStrongestSnpRiskAllele() + "\t";
+            String snps = view.getSnpRsid() + "\t";
+            String riskAlleleFrequency = view.getRiskAlleleFrequency() + "\t";
+            String pValue = view.getpValue() + "\t";
+            String pValueText = view.getpValueQualifier() + "\t";
+            String orBeta = view.getOrBeta() + "\t";
+            String ciText = view.getCi() + " " + view.getCiQualifier() + "\t";
+            String platform = view.getPlatform() + "\t";
+
+            String cnv;
+            if (view.getCnv() == true) {
+                cnv = "Y" + "\t";
+            }
+            else {cnv = "N" + "\t";}
+
+            String associationId = view.getAssociationId().toString() + "\t";
+            String studyId = view.getStudyId().toString() + "\t";
+
+            String resultPublished;
+            if (view.getResultPublished() != null) {
+                resultPublished = "Y" + "\t";
+            }
+            else {resultPublished = "N" + "\t";}
+
+            line = dateAddedToCatalog + pubmedId + firstAuthor + date + journal + link + study + diseaseTrait +
+                    initialSampleSize + replicateSampleSize + region + reportedGenes + strongestSnpRiskAllele + snps +
+                    riskAlleleFrequency + pValue + pValueText + orBeta + ciText + platform + cnv + associationId +
+                    studyId + resultPublished + "\n";
+
+
+            viewAsStrings.add(line);
+        } // end for
+
+        return viewAsStrings;
     }
 
 
+    public void createFileForNcbi(String fileName, List<String> viewsAsStrings) {
+        File file = new File(fileName);
+        BufferedWriter output = null;
+        try {
+            output = new BufferedWriter(new FileWriter(file));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (String view: viewsAsStrings){
+
+            try {
+                output.write(view);
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        try {
+            output.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
