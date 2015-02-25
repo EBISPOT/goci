e647444c99bf15fa96f5ed51d5e7279856bcbc87
Tony Burdett <tburdett@ebi.ac.uk>
2015-02-25 13:56:48 +0000

    adding SpringBootServletInitializer

--- goci-interfaces/goci-ui/src/main/java/uk/ac/ebi/spot/goci/SearchApplication.java
+++ goci-interfaces/goci-ui/src/main/java/uk/ac/ebi/spot/goci/SearchApplication.java
@@ -2,9 +2,10 @@ package uk.ac.ebi.spot.goci;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SearchApplication {
+public class SearchApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(SearchApplication.class, args);
     }
