a2b120f0c8f1c71a404b9c1a588e0954ce9cbc82
Tony Burdett <tburdett@ebi.ac.uk>
2015-02-25 14:23:23 +0000

    curation also uses spring boot servlet initializer

--- goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/CurationApplication.java
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/CurationApplication.java
@@ -2,9 +2,10 @@ package uk.ac.ebi.spot.goci;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CurationApplication {
+public class CurationApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(CurationApplication.class, args);
     }
