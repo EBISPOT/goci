c4a5c94900c7d31f8aa0b2fefe25266af6e75b53
emma-ebi <emma@ebi.ac.uk>
2015-02-25 16:56:35 +0000

    Added method to read header and some placeholder methods

--- goci-tools/goci-catalog-io/src/main/java/uk/ac/ebi/spot/goci/repository/CatalogImportRepository.java
+++ goci-tools/goci-catalog-io/src/main/java/uk/ac/ebi/spot/goci/repository/CatalogImportRepository.java
@@ -1,6 +1,10 @@
 package uk.ac.ebi.spot.goci.repository;
 
 import org.springframework.stereotype.Repository;
+import uk.ac.ebi.spot.goci.model.CatalogHeaderBinding;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Javadocs go here!
@@ -11,6 +15,27 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class CatalogImportRepository {
     public void loadNCBIMappedData(String[][] data) {
+
+        // Create a map of col number to header
+        Map<Integer, String> colNumHeaderMap = new HashMap<>();
+        Integer colNum = 0;
+
+        for (String[] header : data) {
+            for (String cell : header) {
+                colNumHeaderMap.put(colNum, cell);
+                colNum++;
+            }
+            // Break after first line, as this is all we need to establish header
+            break;
+        }
+
+    }
+
+    private void mapHeader(Map<Integer, String> colNumHeaderMap){
+
+    }
+
+    private void mapData (Map<CatalogHeaderBinding, Integer>headerBindingIntegerMap, String[][] data){
 
     }
 }
