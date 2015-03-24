883a6f9e4204fbe0b7f55ba1b6ce28a5d58d233c
Dani Welter <dwelter@ebi.ac.uk>
2015-03-24 12:56:11 +0000

    New mappers for SNP_ID field and pvalue log in the download file added

--- goci-tools/goci-catalog-io/src/main/java/uk/ac/ebi/spot/goci/repository/CatalogExportRepository.java
+++ goci-tools/goci-catalog-io/src/main/java/uk/ac/ebi/spot/goci/repository/CatalogExportRepository.java
@@ -10,15 +10,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 /**
@@ -32,9 +32,9 @@ public class CatalogExportRepository {
     private static final String FROM_CLAUSE =
             " FROM CATALOG_SUMMARY_VIEW ";
     private static final String NCBI_WHERE_CLAUSE =
-            " WHERE CURATION_STATUS = 'Send to NCBI' ORDER BY STUDY_ID DESC ";
+            " WHERE CURATION_STATUS = 'Send to NCBI' ORDER BY PUBMED_ID DESC ";
     private static final String DOWNLOAD_WHERE_CLAUSE =
-            " WHERE RESULT_PUBLISHED IS NOT NULL ORDER BY STUDY_ID DESC ";
+            " WHERE RESULT_PUBLISHED IS NOT NULL ORDER BY PUBMED_ID DESC ";
 
     private final DateFormat df;
 
@@ -110,13 +110,14 @@ public class CatalogExportRepository {
     }
 
     public String[][] getDownloadSpreadsheet() {
-        List<String> downloadOutputHeaders = CatalogHeaderBindings.getDownloadHeaders()
+        List<String> downloadOutputHeaders = getOrderedDownloadHeaders()
                 .stream()
                 .filter(binding -> binding.getDownloadName() != null)
                 .map(CatalogHeaderBinding::getDownloadName)
                 .collect(Collectors.toList());
 
-        List<String> downloadQueryHeaders = CatalogHeaderBindings.getDownloadHeaders()
+
+        List<String> downloadQueryHeaders = getOrderedDownloadHeaders()
                 .stream()
                 .filter(binding -> binding.getDatabaseName() != null)
                 .map(CatalogHeaderBinding::getDatabaseName)
@@ -126,7 +127,7 @@ public class CatalogExportRepository {
         List<String[]> rows = jdbcTemplate.query(query, (resultSet, i) -> {
             Map<CatalogHeaderBinding, String> dataForMapping = new LinkedHashMap<>();
             Map<CatalogHeaderBinding, String> rowMap = new LinkedHashMap<>();
-            for (CatalogHeaderBinding binding : CatalogHeaderBindings.getDownloadHeaders()) {
+            for (CatalogHeaderBinding binding : getOrderedDownloadHeaders()) {
                 if (binding.getDownloadName() != null) {
                     // insert headings in declaration order (this is the correct order)
                     // which controls for reinsertion
@@ -165,9 +166,10 @@ public class CatalogExportRepository {
         return rows.toArray(new String[rows.size()][]);
     }
 
+
     private String buildSelectClause(List<String> requiredFields) {
         StringBuilder sb = new StringBuilder();
-        sb.append("SELECT ");
+        sb.append("SELECT DISTINCT ");
 
         Iterator<String> requiredFieldIterator = requiredFields.iterator();
         while (requiredFieldIterator.hasNext()) {
@@ -201,4 +203,64 @@ public class CatalogExportRepository {
             }
         }
     }
+
+     //put the CatalogHeaderBindings into the correct order for the download spreadsheet
+    private List<CatalogHeaderBinding> getOrderedDownloadHeaders() {
+        List<CatalogHeaderBinding> catalogHeaders = CatalogHeaderBindings.getDownloadHeaders();
+        List<CatalogHeaderBinding> orderedHeaders = new ArrayList<CatalogHeaderBinding>();
+
+        List<String> order = Arrays.asList("DATE ADDED TO CATALOG",
+                                           "PUBMEDID",
+                                           "FIRST AUTHOR",
+                                           "DATE",
+                                           "JOURNAL",
+                                           "LINK",
+                                           "STUDY",
+                                           "DISEASE/TRAIT",
+                                           "INITIAL SAMPLE DESCRIPTION",
+                                           "REPLICATION SAMPLE DESCRIPTION",
+                                           "REGION",
+                                           "CHR_ID",
+                                           "CHR_POS",
+                                           "REPORTED GENE(S)",
+                                           "MAPPED_GENE",
+                                           "UPSTREAM_GENE_ID",
+                                           "DOWNSTREAM_GENE_ID",
+                                           "SNP_GENE_IDS",
+                                           "UPSTREAM_GENE_DISTANCE",
+                                           "DOWNSTREAM_GENE_DISTANCE",
+                                           "STRONGEST SNP-RISK ALLELE",
+                                           "SNPS",
+                                           "MERGED",
+                                           "SNP_ID_CURRENT",
+                                           "CONTEXT",
+                                           "INTERGENIC",
+                                           "RISK ALLELE FREQUENCY",
+                                           "P-VALUE",
+                                           "PVALUE_MLOG",
+                                           "P-VALUE (TEXT)",
+                                           "OR or BETA",
+                                           "95% CI (TEXT)",
+                                           "PLATFORM [SNPS PASSING QC]",
+                                           "CNV"/*,
+                                           "MAPPED_TRAIT",
+                                           "MAPPED_TRAIT_URI"*/);
+
+        for(String header : order){
+            for(CatalogHeaderBinding binding : catalogHeaders){
+                if(binding.getDownloadName() != null){
+                    if(binding.getDownloadName().equals(header)){
+                        orderedHeaders.add(binding);
+                    }
+                }
+                else {
+                    if(!orderedHeaders.contains(binding)){
+                        orderedHeaders.add(binding);
+                    }
+                }
+            }
+        }
+        return orderedHeaders;
+    }
+
 }
