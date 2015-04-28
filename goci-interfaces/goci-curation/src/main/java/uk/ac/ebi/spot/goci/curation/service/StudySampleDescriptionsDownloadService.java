a86da641414074227c9dd2814a3b62b8cdc8a0ae
emma-ebi <emma@ebi.ac.uk>
2015-04-28 11:55:38 +0100

    Altered writing output logic and added further string tiding

--- goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/StudySampleDescriptionsDownloadService.java
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/StudySampleDescriptionsDownloadService.java
@@ -1,23 +1,36 @@
 package uk.ac.ebi.spot.goci.curation.service;
 
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.stereotype.Service;
 import uk.ac.ebi.spot.goci.curation.model.StudySampleDescription;
 
+import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
+import java.util.Date;
 
 /**
  * Created by emma on 27/04/2015.
  *
- * @author emma This is a service class to process a set of study sample descriptions and output the result to a tsv
- *         file. Based on AssociationDownloadService.java
+ * @author emma
+ *
+ * This is a service class to process a set of study sample descriptions and output the result to a tsv
+ * file. Based on AssociationDownloadService.java
  */
+
 @Service
 public class StudySampleDescriptionsDownloadService {
 
+    // Constructor
     public StudySampleDescriptionsDownloadService() {
     }
 
@@ -27,22 +40,17 @@ public class StudySampleDescriptionsDownloadService {
 
         String file = processStudySampleDescriptions(studySampleDescriptions);
 
-        InputStream in = new ByteArrayInputStream(file.getBytes("UTF-8"));
-
-        byte[] outputByte = new byte[4096];
-        //copy binary contect to output stream
-        while (in.read(outputByte, 0, 4096) != -1) {
-            outputStream.write(outputByte, 0, 4096);
-        }
-        in.close();
+        // Write file
+        outputStream.write(file.getBytes("UTF-8"));
         outputStream.flush();
+        outputStream.close();
 
     }
 
     private String processStudySampleDescriptions(Collection<StudySampleDescription> studySampleDescriptions) {
 
         String header =
-                "Author\tStudy Date\tInitial Sample Description\tReplication Sample Description\tEthnicty Checked Level One\tEthnicty Checked Level Two\tType\tNumber of Individuals\tEthnic Group\tCountry of Origin\tCountry of Recruitment\tAdditional Description\tSample Sizes Match\tNotes\r\n";
+                "Author\tStudy Date\tPubmed ID\tInitial Sample Description\tReplication Sample Description\tType\tNumber of Individuals\tEthnic Group\tCountry of Origin\tCountry of Recruitment\tAdditional Description\tSample Sizes Match\tEthnicty Checked Level One\tEthnicty Checked Level Two\tNotes\n";
 
 
         StringBuilder output = new StringBuilder();
@@ -51,6 +59,7 @@ public class StudySampleDescriptionsDownloadService {
         for (StudySampleDescription studySampleDescription : studySampleDescriptions) {
             StringBuilder line = new StringBuilder();
 
+            // Author
             if (studySampleDescription.getAuthor() == null) {
                 line.append("");
             }
@@ -60,15 +69,29 @@ public class StudySampleDescriptionsDownloadService {
             }
             line.append("\t");
 
+            // Study Date
             if (studySampleDescription.getStudyDate() == null) {
                 line.append("");
             }
 
             else {
-                line.append(studySampleDescription.getStudyDate());
+                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                String studyDate = dateFormat.format(studySampleDescription.getStudyDate());
+                line.append(studyDate);
             }
             line.append("\t");
 
+            // Pubmed ID
+            if (studySampleDescription.getPubmedId() == null) {
+                line.append("");
+            }
+
+            else {
+                line.append(studySampleDescription.getPubmedId());
+            }
+            line.append("\t");
+
+            // Initial sample size
             String initialSampleSize= studySampleDescription.getInitialSampleSize();
             if (initialSampleSize== null) {
                 line.append("");
@@ -79,15 +102,98 @@ public class StudySampleDescriptionsDownloadService {
             }
             line.append("\t");
 
-            if (studySampleDescription.getReplicateSampleSize() == null) {
+            // Replicate sample size
+            String replicateSampleSize = studySampleDescription.getReplicateSampleSize();
+            if (replicateSampleSize == null) {
                 line.append("");
             }
 
             else {
-                line.append(studySampleDescription.getReplicateSampleSize());
+                line.append(tidyStringForOutput(replicateSampleSize));
             }
             line.append("\t");
 
+            // Type
+            if (studySampleDescription.getType() == null) {
+                line.append("");
+            }
+
+            else {
+                line.append(studySampleDescription.getType());
+            }
+            line.append("\t");
+
+            // Number of individuals
+            if (studySampleDescription.getNumberOfIndividuals() == null) {
+                line.append("");
+            }
+
+            else {
+                line.append(studySampleDescription.getNumberOfIndividuals());
+            }
+            line.append("\t");
+
+            // Ethnic group
+            String ethnicGroup = studySampleDescription.getEthnicGroup();
+            if (ethnicGroup == null) {
+                line.append("");
+            }
+
+            else {
+                line.append(tidyStringForOutput(ethnicGroup));
+            }
+            line.append("\t");
+
+            // Origin
+            String countryOfOrigin = studySampleDescription.getCountryOfOrigin();
+            if (countryOfOrigin == null) {
+                line.append("");
+            }
+
+            else {
+                line.append(tidyStringForOutput(countryOfOrigin));
+            }
+            line.append("\t");
+
+            // Recruitment
+            String countryOfRecruitment = studySampleDescription.getCountryOfRecruitment();
+            if (countryOfRecruitment== null) {
+                line.append("");
+            }
+
+            else {
+                line.append(tidyStringForOutput(countryOfRecruitment));
+            }
+            line.append("\t");
+
+            // Description , this requires some tidying
+            String description = studySampleDescription.getDescription();
+            if (description == null) {
+                line.append("");
+            }
+
+            else {
+                String newline = System.getProperty("line.separator");
+                if (description.equals(newline)) {
+                    line.append("");
+                }
+                else {
+                    line.append(tidyStringForOutput(description));
+                }
+            }
+            line.append("\t");
+
+            // Sample size
+            if (studySampleDescription.getSampleSizesMatch() == null) {
+                line.append("");
+            }
+
+            else {
+                line.append(studySampleDescription.getSampleSizesMatch());
+            }
+            line.append("\t");
+
+            // Housekeeping information
             if (studySampleDescription.isEthnicityCheckedLevelOne() == null) {
                 line.append("");
             }
@@ -114,77 +220,8 @@ public class StudySampleDescriptionsDownloadService {
             }
             line.append("\t");
 
-            if (studySampleDescription.getType() == null) {
-                line.append("");
-            }
 
-            else {
-                line.append(studySampleDescription.getType());
-            }
-            line.append("\t");
-
-            if (studySampleDescription.getNumberOfIndividuals() == null) {
-                line.append("");
-            }
-
-            else {
-                line.append(studySampleDescription.getNumberOfIndividuals());
-            }
-            line.append("\t");
-
-            if (studySampleDescription.getEthnicGroup() == null) {
-                line.append("");
-            }
-
-            else {
-                line.append(studySampleDescription.getEthnicGroup());
-            }
-            line.append("\t");
-
-            if (studySampleDescription.getCountryOfOrigin() == null) {
-                line.append("");
-            }
-
-            else {
-                line.append(studySampleDescription.getCountryOfOrigin());
-            }
-            line.append("\t");
-
-            if (studySampleDescription.getCountryOfRecruitment() == null) {
-                line.append("");
-            }
-
-            else {
-                line.append(studySampleDescription.getCountryOfRecruitment());
-            }
-            line.append("\t");
-
-            if (studySampleDescription.getDescription() == null) {
-                line.append("");
-            }
-
-            else {
-
-                String newline = System.getProperty("line.separator");
-                if (studySampleDescription.getDescription().equals(newline)) {
-                    line.append("");
-                }
-                else {
-                    line.append(studySampleDescription.getDescription());
-                }
-            }
-            line.append("\t");
-
-            if (studySampleDescription.getSampleSizesMatch() == null) {
-                line.append("");
-            }
-
-            else {
-                line.append(studySampleDescription.getSampleSizesMatch());
-            }
-            line.append("\t");
-
-            // Tidy notes field
+            // Notes, this requires some tidying
             String notes = studySampleDescription.getNotes();
             if (notes == null) {
                 line.append("");
@@ -192,7 +229,9 @@ public class StudySampleDescriptionsDownloadService {
             else {
                 line.append(tidyStringForOutput(notes));
             }
-            line.append("\r\n");
+
+            // Add new line
+            line.append("\n");
             output.append(line.toString());
 
         }
