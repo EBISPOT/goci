8be6b9b0007903ea6bc435dc6682ce992e3a00b5
emma-ebi <emma@ebi.ac.uk>
2015-05-19 16:46:33 +0100

    Added proxies attribute

--- goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/model/SnpFormColumn.java
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/model/SnpFormColumn.java
@@ -6,7 +6,7 @@ import java.util.Collection;
  * Created by emma on 12/02/15.
  *
  * @author emma
- *         <p/>
+ *         <p>
  *         Service class to deal with columns used by curators to enter snp/association details for interaction studies
  */
 public class SnpFormColumn {
@@ -19,46 +19,65 @@ public class SnpFormColumn {
 
     private Collection<String> authorReportedGenes;
 
+    private Collection<String> proxies;
+
     // Constructors
     public SnpFormColumn() {
     }
 
-    public SnpFormColumn(String snp, String strongestRiskAllele, String riskFrequency, Collection<String> authorReportedGenes) {
+    public SnpFormColumn(String snp,
+                         String strongestRiskAllele,
+                         String riskFrequency,
+                         Collection<String> authorReportedGenes, Collection<String> proxies) {
         this.snp = snp;
         this.strongestRiskAllele = strongestRiskAllele;
         this.riskFrequency = riskFrequency;
         this.authorReportedGenes = authorReportedGenes;
+        this.proxies = proxies;
     }
 
     public String getSnp() {
         return snp;
     }
 
-    public void setSnp(String snp) {
+    public SnpFormColumn setSnp(String snp) {
         this.snp = snp;
+        return this;
     }
 
     public String getStrongestRiskAllele() {
         return strongestRiskAllele;
     }
 
-    public void setStrongestRiskAllele(String strongestRiskAllele) {
+    public SnpFormColumn setStrongestRiskAllele(String strongestRiskAllele) {
         this.strongestRiskAllele = strongestRiskAllele;
+        return this;
     }
 
     public String getRiskFrequency() {
         return riskFrequency;
     }
 
-    public void setRiskFrequency(String riskFrequency) {
+    public SnpFormColumn setRiskFrequency(String riskFrequency) {
         this.riskFrequency = riskFrequency;
+        return this;
     }
 
     public Collection<String> getAuthorReportedGenes() {
         return authorReportedGenes;
     }
 
-    public void setAuthorReportedGenes(Collection<String> authorReportedGenes) {
+    public SnpFormColumn setAuthorReportedGenes(Collection<String> authorReportedGenes) {
         this.authorReportedGenes = authorReportedGenes;
+        return this;
+    }
+
+    public Collection<String> getProxies() {
+        return proxies;
+    }
+
+    public SnpFormColumn setProxies(Collection<String> proxies) {
+        this.proxies = proxies;
+        return this;
     }
 }
