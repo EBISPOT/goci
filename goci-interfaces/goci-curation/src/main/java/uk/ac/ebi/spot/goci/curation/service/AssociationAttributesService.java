33000da89f4612a40a948fe7bf8023ec38f59de8
emma-ebi <emma@ebi.ac.uk>
2015-05-26 17:45:32 +0100

    New service classes used when handling association attributes

---
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/AssociationAttributesService.java
@@ -0,0 +1,68 @@
+package uk.ac.ebi.spot.goci.curation.service;
+
+import org.springframework.stereotype.Service;
+import uk.ac.ebi.spot.goci.model.AssociationReport;
+import uk.ac.ebi.spot.goci.model.Locus;
+import uk.ac.ebi.spot.goci.model.RiskAllele;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by emma on 26/05/2015.
+ * <p>
+ * Service class that holds methods used when interacting with the various association types (snp snp interaction,
+ * multi-snp..) and their associated forms
+ */
+@Service
+public class AssociationAttributesService {
+
+    // Services
+    private LociAttributesService lociAttributesService;
+
+    // Delete a locus and its risk alleles
+    public void deleteLocusAndLinkedRiskAllele(Collection<Locus> associationLoci) {
+        Collection<RiskAllele> existingRiskAlleles = new ArrayList<>();
+        for (Locus locus : associationLoci) {
+            existingRiskAlleles.addAll(locus.getStrongestRiskAlleles());
+        }
+        for (Locus locus : associationLoci) {
+            lociAttributesService.deleteLocus(locus);
+        }
+        for (RiskAllele existingRiskAllele : existingRiskAlleles) {
+            lociAttributesService.deleteRiskAllele(existingRiskAllele);
+        }
+    }
+
+    // Create map of all errors linked to an association
+    public Map<String, String> createAssociationErrorMap(AssociationReport associationReport) {
+
+        Map<String, String> associationErrorMap = new HashMap<>();
+
+        //Create map of errors
+        if (associationReport != null) {
+            if (associationReport.getSnpError() != null && !associationReport.getSnpError().isEmpty()) {
+                associationErrorMap.put("SNP Error: ", associationReport.getSnpError());
+            }
+
+            if (associationReport.getGeneNotOnGenome() != null &&
+                    !associationReport.getGeneNotOnGenome().isEmpty()) {
+                associationErrorMap.put("Gene Not On Genome Error: ", associationReport.getGeneNotOnGenome());
+            }
+
+            if (associationReport.getSnpGeneOnDiffChr() != null &&
+                    !associationReport.getSnpGeneOnDiffChr().isEmpty()) {
+                associationErrorMap.put("Snp Gene On Diff Chr: ", associationReport.getSnpGeneOnDiffChr());
+            }
+
+            if (associationReport.getNoGeneForSymbol() != null &&
+                    !associationReport.getNoGeneForSymbol().isEmpty()) {
+                associationErrorMap.put("No Gene For Symbol: ", associationReport.getNoGeneForSymbol());
+            }
+        }
+
+        return associationErrorMap;
+    }
+}
