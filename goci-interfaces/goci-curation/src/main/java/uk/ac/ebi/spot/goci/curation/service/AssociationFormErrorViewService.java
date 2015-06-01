8ee643ab3850ac66b459a3663c9d2ddd9b198271
emma-ebi <emma@ebi.ac.uk>
2015-06-01 15:49:50 +0100

    Updated service to deal with processing of associations

--- goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/AssociationFormErrorViewService.java
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/AssociationFormErrorViewService.java
@@ -2,11 +2,19 @@ package uk.ac.ebi.spot.goci.curation.service;
 
 import org.springframework.stereotype.Service;
 import uk.ac.ebi.spot.goci.curation.model.AssociationFormErrorView;
-import uk.ac.ebi.spot.goci.curation.model.SnpAssociationForm;
-import uk.ac.ebi.spot.goci.curation.model.SnpFormRow;
+import uk.ac.ebi.spot.goci.model.Association;
+import uk.ac.ebi.spot.goci.model.Locus;
+import uk.ac.ebi.spot.goci.model.RiskAllele;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * Created by emma on 29/05/2015.
+ *
+ * @author emma
+ *         <p>
+ *         Service class that determines possible errors in curator entered fields
  */
 @Service
 public class AssociationFormErrorViewService {
@@ -14,28 +22,60 @@ public class AssociationFormErrorViewService {
     public AssociationFormErrorViewService() {
     }
 
-    public AssociationFormErrorView checkFormForErrors(SnpAssociationForm snpAssociationForm) {
+    // Determine error prone attributes of association and then return them via controller to view
+    public AssociationFormErrorView checkAssociationForErrors(Association association) {
+
         AssociationFormErrorView associationErrorView = new AssociationFormErrorView();
-        String snpError = "SNP errors: ";
+        Collection<String> associationRiskAlleles = new ArrayList<String>();
+        Collection<String> associationSnps = new ArrayList<String>();
+        Collection<String> associationProxies = new ArrayList<String>();
 
-        for (SnpFormRow snpFormRow : snpAssociationForm.getSnpFormRows()) {
+        String riskAlleleErrors = "Risk Allele errors: ";
+        String snpErrors = "SNP errors: ";
+        String proxyErrors = "Proxy errors: ";
 
-            // Check SNPs
-            if (snpFormRow.getSnp() != null && !snpFormRow.getSnp().isEmpty()) {
-                String error = checkSnpOrRiskAllele(snpFormRow.getSnp());
-                snpError = snpError+error;
+        for (Locus locus : association.getLoci()) {
+
+            if (locus != null) {
+                for (RiskAllele riskAllele : locus.getStrongestRiskAlleles()) {
+                    associationRiskAlleles.add(riskAllele.getRiskAlleleName());
+                    associationSnps.add(riskAllele.getSnp().getRsId());
+                    associationProxies.add(riskAllele.getProxySnp().getRsId());
+
+                }
             }
-
-            associationErrorView.setSnpError(snpError);
         }
+        String error = "";
+        for (String riskAlleleName : associationRiskAlleles) {
+            error = error + checkSnpOrRiskAllele(riskAlleleName);
+
+        }
+        riskAlleleErrors = riskAlleleErrors + error;
+
+        error = "";
+        for (String snpName : associationSnps) {
+            error = error + checkSnpOrRiskAllele(snpName);
+        }
+        snpErrors = snpErrors + error;
+
+        error = "";
+        for (String proxyName : associationProxies) {
+            error = error + checkSnpOrRiskAllele(proxyName);
+        }
+        proxyErrors = proxyErrors + error;
+
+        associationErrorView.setRiskAlleleErrors(riskAlleleErrors);
+        associationErrorView.setSnpErrors(snpErrors);
+        associationErrorView.setProxyErrors(proxyErrors);
         return associationErrorView;
     }
 
+    // Check for common errors in sno and risk allele names
     public String checkSnpOrRiskAllele(String snpValue) {
 
         String error = "";
         if (snpValue.contains(",")) {
-            error = "SNP " + snpValue + " contains a comma\n";
+            error = "SNP " + snpValue + " contains a ','\n";
         }
         if (snpValue.contains("x")) {
             error = error + "SNP " + snpValue + " contains an x character\n";
@@ -43,12 +83,14 @@ public class AssociationFormErrorViewService {
         if (snpValue.contains("X")) {
             error = error + "SNP " + snpValue + " contains an X character\n";
         }
-
         if (snpValue.contains(":")) {
-            error = error + "SNP " + snpValue + "contains a :\n";
+            error = error + "SNP " + snpValue + " contains a ':'\n";
         }
-        else {
-            error = "No errors\n";
+        if (snpValue.contains(";")) {
+            error = error + "SNP " + snpValue + " contains a ';'\n";
+        }
+        if (!snpValue.startsWith("rs")) {
+            error = error + "SNP " + snpValue + " does not start with RS :\n";
         }
 
         return error;
