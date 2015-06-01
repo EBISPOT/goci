cb7b47a80d23f7b5f63f244e8d7ebc63c76a4d10
emma-ebi <emma@ebi.ac.uk>
2015-06-01 15:49:09 +0100

    Updated model to store proxy, snp and risk allele errors

--- goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/model/AssociationFormErrorView.java
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/model/AssociationFormErrorView.java
@@ -2,25 +2,45 @@ package uk.ac.ebi.spot.goci.curation.model;
 
 /**
  * Created by emma on 29/05/2015.
+ *
+ * @author emma
+ *         <p>
+ *         Model object used to store errors in an assoication
  */
 public class AssociationFormErrorView {
 
-    private String snpError;
+    private String snpErrors;
+
+    private String riskAlleleErrors;
+
+    private String proxyErrors;
 
     // Constructor
     public AssociationFormErrorView() {
 
     }
 
-    public AssociationFormErrorView(String snpError) {
-        this.snpError = snpError;
+    public String getSnpErrors() {
+        return snpErrors;
     }
 
-    public String getSnpError() {
-        return snpError;
+    public void setSnpErrors(String snpErrors) {
+        this.snpErrors = snpErrors;
     }
 
-    public void setSnpError(String snpError) {
-        this.snpError = snpError;
+    public String getRiskAlleleErrors() {
+        return riskAlleleErrors;
+    }
+
+    public void setRiskAlleleErrors(String riskAlleleErrors) {
+        this.riskAlleleErrors = riskAlleleErrors;
+    }
+
+    public String getProxyErrors() {
+        return proxyErrors;
+    }
+
+    public void setProxyErrors(String proxyErrors) {
+        this.proxyErrors = proxyErrors;
     }
 }
