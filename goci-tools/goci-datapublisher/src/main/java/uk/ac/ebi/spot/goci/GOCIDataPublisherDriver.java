03e05c8a733814152446b07ac47b3962b0fcd4b2
Catherine Leroy <cleroy@ebi.ac.uk>
2015-06-01 17:10:58 +0100

    update publisher to use new schema

--- goci-tools/goci-datapublisher/src/main/java/uk/ac/ebi/spot/goci/GOCIDataPublisherDriver.java
+++ goci-tools/goci-datapublisher/src/main/java/uk/ac/ebi/spot/goci/GOCIDataPublisherDriver.java
@@ -1,20 +1,29 @@
 package uk.ac.ebi.spot.goci;
 
 import org.apache.commons.cli.*;
-import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
 import uk.ac.ebi.spot.goci.exception.OWLConversionException;
-import uk.ac.ebi.spot.goci.lang.FilterProperties;
-import uk.ac.ebi.spot.goci.lang.OntologyConfiguration;
-import uk.ac.ebi.spot.goci.service.GWASOWLPublisher;
+import uk.ac.ebi.spot.goci.owl.ReasonedOntologyLoader;
+import uk.ac.ebi.spot.goci.utils.FilterProperties;
+import uk.ac.ebi.spot.goci.owl.OntologyLoader;
+import uk.ac.ebi.spot.goci.service.GwasOwlPublisher;
+
+
 
 import java.io.File;
 import java.net.URI;
+import java.util.Arrays;
 
 /**
  * A driver class, containing a {@link #main(String[])} method, that can be used to run the datapublisher to convert the
@@ -22,32 +31,80 @@ import java.net.URI;
  *
  * @author Tony Burdett Date 26/01/12
  */
+@SpringBootApplication
 public class GOCIDataPublisherDriver {
     private static File assertedOntologyFile;
     private static File inferredOntologyFile;
 
-    public static void main(String[] args) {
-        try {
-            // parse arguments
-            int parseArgs = parseArguments(args);
-            if (parseArgs == 0) {
-                // execute publisher
-                GOCIDataPublisherDriver driver = new GOCIDataPublisherDriver();
-                driver.publishAndSave(assertedOntologyFile, inferredOntologyFile);
-//                driver.loadAndPrintStats(assertedOntologyFile); // AJCB - This just dumps out some stats about the knowledgebase provided
-            }
-            else {
-                // could not parse arguments, exit with exit code >1 (depending on parsing problem)
-                System.err.println("Failed to parse supplied arguments");
-                System.exit(1 + parseArgs);
-            }
-        }
-        catch (Exception e) {
-            // failed to execute, exit with exit code 1
-            System.err.println("An unexpected error occurred\n\t(" + e.getMessage() + ")");
-            System.exit(1);
-        }
+    @Autowired
+    private GwasOwlPublisher gwasOwlPublisher;
+
+    public GwasOwlPublisher getGwasOwlPublisher(){
+        return gwasOwlPublisher;
     }
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    //ReasonedOntologyLoader or AssertedOntologyLoader
+    private OntologyLoader ontologyLoader = new ReasonedOntologyLoader();
+
+
+    public static void main(String[] args) {
+        System.out.println("Starting Goci data publisher...");
+        ApplicationContext ctx = SpringApplication.run(GOCIDataPublisherDriver.class, args);
+        System.out.println("Application executed successfully!");
+        SpringApplication.exit(ctx);
+    }
+
+    @Bean CommandLineRunner run() {
+        return strings -> {
+            long start_time = System.currentTimeMillis();
+            System.out.println("Building indexes with supplied params: " + Arrays.toString(strings));
+            int parseArgs = parseArguments(strings);
+                        if (parseArgs == 0) {
+                            // execute publisher
+
+                            this.publishAndSave(assertedOntologyFile, inferredOntologyFile);
+            //                driver.loadAndPrintStats(assertedOntologyFile); // AJCB - This just dumps out some stats about the knowledgebase provided
+                        }
+                        else {
+                            // could not parse arguments, exit with exit code >1 (depending on parsing problem)
+                            System.err.println("Failed to parse supplied arguments");
+                            System.exit(1 + parseArgs);
+                        }
+            long end_time = System.currentTimeMillis();
+            String time = String.format("%.1f", ((double) (end_time - start_time)) / 1000);
+            System.out.println("Indexing building complete in " + time + " s. - application will now exit");
+        };
+    }
+
+    // JDBCSingleNucleotidePolymorphsm to be replaced by SingleNucleotidePolymorphismRepository
+//    public static void main(String[] args) {
+//        try {
+//
+////            ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"SpringBeans.xml"});
+//
+//            // parse arguments
+//            int parseArgs = parseArguments(args);
+//            if (parseArgs == 0) {
+//                // execute publisher
+//
+//                GOCIDataPublisherDriver driver = new GOCIDataPublisherDriver();
+//                driver.publishAndSave(assertedOntologyFile, inferredOntologyFile);
+////                driver.loadAndPrintStats(assertedOntologyFile); // AJCB - This just dumps out some stats about the knowledgebase provided
+//            }
+//            else {
+//                // could not parse arguments, exit with exit code >1 (depending on parsing problem)
+//                System.err.println("Failed to parse supplied arguments");
+//                System.exit(1 + parseArgs);
+//            }
+//        }
+//        catch (Exception e) {
+//            // failed to execute, exit with exit code 1
+//            System.err.println("An unexpected error occurred\n\t(" + e.getMessage() + ")");
+//            System.exit(1);
+//        }
+//    }
 
     private static int parseArguments(String[] args) {
         CommandLineParser parser = new GnuParser();
@@ -128,40 +185,39 @@ public class GOCIDataPublisherDriver {
         return options;
     }
 
-    private GWASOWLPublisher publisher;
-    private OntologyConfiguration config;
 
-    private Logger log = LoggerFactory.getLogger(getClass());
 
     private Logger getLog() {
         return log;
     }
 
-    public GOCIDataPublisherDriver() {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("goci-datapublisher.xml");
-        publisher = ctx.getBean("publisher", GWASOWLPublisher.class);
-        config = ctx.getBean("config", OntologyConfiguration.class);
-    }
 
     public void publishAndSave(File assertedOntologyFile, File inferredOntologyFile) throws RuntimeException {
         try {
             // publishAndSave the data
             System.out.println("Attempting to convert and publish GWAS data as OWL...");
-            OWLOntology ontology = publisher.publishGWASData();
+
+            if(getGwasOwlPublisher() == null){
+                System.out.println("getGwasOwlPublisher is NULL!!!!!!!!!");
+            }else{
+                System.out.println("getGwasOwlPublisher is NOT NULL!!!!!!!!!");
+
+            }
+            OWLOntology ontology = getGwasOwlPublisher().publishGWASData();
 
             // and save the result
             System.out.print("Ontology converted, saving asserted results...");
-            publisher.saveGWASData(ontology, assertedOntologyFile);
+            getGwasOwlPublisher().saveGWASData(ontology, assertedOntologyFile);
             System.out.println("..done!");
 
             if (inferredOntologyFile != null) {
                 // now get the inferred view
                System.out.println("Evaluating inferred view...");
-                OWLReasoner reasoner = publisher.publishGWASDataInferredView(ontology);
+                OWLReasoner reasoner = getGwasOwlPublisher().publishGWASDataInferredView(ontology);
 
                 // now save inferred view
                 System.out.print("Ontology fully classified, saving inferred results...");
-                publisher.saveGWASDataInferredView(reasoner, inferredOntologyFile);
+                getGwasOwlPublisher().saveGWASDataInferredView(reasoner, inferredOntologyFile);
                 System.out.println("..done!");
             }
         }
@@ -182,7 +238,8 @@ public class GOCIDataPublisherDriver {
             // load ontology
             URI gwasDataURI = assertedOntologyFile.toURI();
             getLog().info("Loading GWAS data from " + gwasDataURI);
-            OWLOntology ontology = config.getOWLOntologyManager().loadOntology(IRI.create(gwasDataURI));
+
+            OWLOntology ontology = ontologyLoader.getOntology();
 
             // print some stats
             int individualCount = ontology.getIndividualsInSignature().size();
