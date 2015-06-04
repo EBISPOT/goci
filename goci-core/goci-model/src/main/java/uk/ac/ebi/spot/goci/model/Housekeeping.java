fe3690605f85b2b7a75d81e39f39e20481fccc92
Dani Welter <dwelter@ebi.ac.uk>
2015-06-04 10:51:12 +0100

    Unpublish study button is now available in the housekeeping tab for studies that have been published. No functionality available yet

--- goci-core/goci-model/src/main/java/uk/ac/ebi/spot/goci/model/Housekeeping.java
+++ goci-core/goci-model/src/main/java/uk/ac/ebi/spot/goci/model/Housekeeping.java
@@ -46,6 +46,9 @@ public class Housekeeping {
     private Date studyAddedDate;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date catalogUnpublishDate;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date lastUpdateDate;
 
     private String fileName;
@@ -62,7 +65,7 @@ public class Housekeeping {
     public Housekeeping() {
     }
 
-    public Housekeeping(Boolean studySnpCheckedLevelOne, Boolean studySnpCheckedLevelTwo, Boolean ethnicityCheckedLevelOne, Boolean ethnicityCheckedLevelTwo, Boolean ethnicityBackFilled, Boolean checkedNCBIError, Boolean snpsRechecked, Date catalogPublishDate, Date sendToNCBIDate, Date studyAddedDate, Date lastUpdateDate, String fileName, String notes, Curator curator, CurationStatus curationStatus) {
+    public Housekeeping(Boolean studySnpCheckedLevelOne, Boolean studySnpCheckedLevelTwo, Boolean ethnicityCheckedLevelOne, Boolean ethnicityCheckedLevelTwo, Boolean ethnicityBackFilled, Boolean checkedNCBIError, Boolean snpsRechecked, Date catalogPublishDate, Date sendToNCBIDate, Date studyAddedDate, Date catalogUnpublishDate, Date lastUpdateDate, String fileName, String notes, Curator curator, CurationStatus curationStatus) {
         this.studySnpCheckedLevelOne = studySnpCheckedLevelOne;
         this.studySnpCheckedLevelTwo = studySnpCheckedLevelTwo;
         this.ethnicityCheckedLevelOne = ethnicityCheckedLevelOne;
@@ -73,6 +76,7 @@ public class Housekeeping {
         this.catalogPublishDate = catalogPublishDate;
         this.sendToNCBIDate = sendToNCBIDate;
         this.studyAddedDate = studyAddedDate;
+        this.catalogUnpublishDate = catalogUnpublishDate;
         this.lastUpdateDate = lastUpdateDate;
         this.fileName = fileName;
         this.notes = notes;
@@ -168,6 +172,14 @@ public class Housekeeping {
         this.studyAddedDate = studyAddedDate;
     }
 
+    public Date getCatalogUnpublishDate() {
+        return catalogUnpublishDate;
+    }
+
+    public void setCatalogUnpublishDate(Date catalogUnpublishDate) {
+        this.catalogUnpublishDate = catalogUnpublishDate;
+    }
+
     public Date getLastUpdateDate() {
         return lastUpdateDate;
     }
@@ -222,6 +234,7 @@ public class Housekeeping {
                 ", catalogPublishDate=" + catalogPublishDate +
                 ", sendToNCBIDate=" + sendToNCBIDate +
                 ", studyAddedDate=" + studyAddedDate +
+                ", catalogUnpublishDate=" + catalogUnpublishDate +
                 ", lastUpdateDate=" + lastUpdateDate +
                 ", fileName='" + fileName + '\'' +
                 ", notes='" + notes + '\'' +
@@ -229,4 +242,5 @@ public class Housekeeping {
                 ", curationStatus=" + curationStatus +
                 '}';
     }
+
 }
