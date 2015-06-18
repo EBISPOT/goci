824c538f397a9d25d1dee84aec17a5543f9f9018
emma-ebi <emma@ebi.ac.uk>
2015-06-18 11:57:46 +0100

    Tidied up some issues that occured during merging

--- goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/SingleSnpMultiSnpAssociationService.java
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/SingleSnpMultiSnpAssociationService.java
@@ -59,7 +59,6 @@ public class SingleSnpMultiSnpAssociationService {
 
     }
 
-
     public Association createAssociation(SnpAssociationForm snpAssociationForm) {
 
         Association association = new Association();
