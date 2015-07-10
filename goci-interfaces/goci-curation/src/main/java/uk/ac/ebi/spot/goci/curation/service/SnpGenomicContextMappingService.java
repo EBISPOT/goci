5cc27d984f52d44f7cb9a2c136b936287b8598ad
emma-ebi <emma@ebi.ac.uk>
2015-07-10 16:11:54 +0100

    Shell for class that will store genomic context data

---
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/SnpGenomicContextMappingService.java
@@ -0,0 +1,11 @@
+package uk.ac.ebi.spot.goci.curation.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by emma on 10/07/2015.
+ * @author emma
+ */
+@Service
+public class SnpGenomicContextMappingService {
+}
