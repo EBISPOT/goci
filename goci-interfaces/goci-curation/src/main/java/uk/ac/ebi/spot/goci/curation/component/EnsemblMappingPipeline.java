8a88ab82e1e8726d42e244dc368b037585c8e086
Laurent Gil <lgil@ebi.ac.uk>
2015-07-16 10:08:59 +0100

    Add a new contructor with only 1 argument

---
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/component/EnsemblMappingPipeline.java
@@ -0,0 +1,7 @@
+package uk.ac.ebi.spot.goci.curation.component;
+
+/**
+ * Created by Laurent on 15/07/15.
+ */
+public class EnsemblMappingPipeline {
+}
---
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/EnsemblRestService.java
@@ -0,0 +1,7 @@
+package uk.ac.ebi.spot.goci.curation.service;
+
+/**
+ * Created by Laurent on 15/07/15.
+ */
+public class EnsemblRestService {
+}
