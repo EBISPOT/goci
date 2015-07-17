64e81493ffe19648f83b0cc504b64116a9d9effe
emma-ebi <emma@ebi.ac.uk>
2015-07-17 12:50:32 +0100

    Updated genomic context constructor to take account of isClosestGene attribute

--- goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/component/EnsemblMappingPipeline.java
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/component/EnsemblMappingPipeline.java
@@ -360,8 +360,12 @@ public class EnsemblMappingPipeline {
             }
             Long dist = new Long(distance);
             Gene gene_object = new Gene(gene_name,ncbi_id,ensembl_id);
-            GenomicContext gc = new GenomicContext(intergenic,upstream,downstream,dist,snp_tmp,gene_object,source,mapping_method);
+
             // TODO add the closest upstream/downstream info
+            // TODO (Emma) I added this so could get interface to build as I have added isClosestGene to GenomicContext model object
+            Boolean isClosestGene = false;
+            GenomicContext gc = new GenomicContext(intergenic,upstream,downstream,dist,snp_tmp,gene_object,source,mapping_method, isClosestGene);
+
             genomic_contexts.add(gc);
         }
         return (closest_gene != "") ? true : false;
