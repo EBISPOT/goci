3b470e220e54d11bc5315d047dd81cb744c9cf61
emma-ebi <emma@ebi.ac.uk>
2015-07-21 14:04:50 +0100

    Update to gene model so each gene can have multiple ensembl or entrez ids

---
+++ goci-core/goci-model/src/main/java/uk/ac/ebi/spot/goci/model/EnsemblGene.java
@@ -0,0 +1,64 @@
+package uk.ac.ebi.spot.goci.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+
+/**
+ * Created by emma on 21/07/2015.
+ *
+ * @author emma
+ * <p>
+ * A model object representing a gene from Ensembl database
+ */
+@Entity
+public class EnsemblGene {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String ensemblGeneId;
+
+    @ManyToOne
+    @JoinTable(name = "GENE_ENSEMBL_GENE",
+               joinColumns = @JoinColumn(name = "ENSEMBL_GENE_ID"),
+               inverseJoinColumns = @JoinColumn(name = "GENE_ID"))
+    private Gene gene;
+
+    // JPA no-args constructor
+    public EnsemblGene() {
+    }
+
+    public EnsemblGene(String ensemblGeneId, Gene gene) {
+        this.ensemblGeneId = ensemblGeneId;
+        this.gene = gene;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEnsemblGeneId() {
+        return ensemblGeneId;
+    }
+
+    public void setEnsemblGeneId(String ensemblGeneId) {
+        this.ensemblGeneId = ensemblGeneId;
+    }
+
+    public Gene getGene() {
+        return gene;
+    }
+
+    public void setGene(Gene gene) {
+        this.gene = gene;
+    }
+}
