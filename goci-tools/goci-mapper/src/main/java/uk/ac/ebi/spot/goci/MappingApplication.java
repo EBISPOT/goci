1bd08cb9f07fb79cbc41c2436234520a1d5d3efd
emma-ebi <emma@ebi.ac.uk>
2015-08-14 09:06:01 +0100

    New application that haandles mapping

---
+++ goci-tools/goci-mapper/src/main/java/uk/ac/ebi/spot/goci/MappingApplication.java
@@ -0,0 +1,152 @@
+package uk.ac.ebi.spot.goci;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionGroup;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import uk.ac.ebi.spot.goci.service.MappingService;
+
+/**
+ * Created by emma on 13/08/2015.
+ *
+ * @author emma
+ *         <p>
+ *         Application to map all associations in the GWAS database.
+ */
+@SpringBootApplication
+public class MappingApplication {
+
+    @Autowired
+    private MappingService mappingService;
+
+    private OperationMode opMode;
+
+    private static int exitCode;
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    protected Logger getLog() {
+        return log;
+    }
+
+    public static void main(String... args) {
+        System.out.println("Starting catalog I/O service...");
+        ApplicationContext ctx = SpringApplication.run(MappingApplication.class, args);
+        int code = SpringApplication.exit(ctx, () -> exitCode);
+        if (code > 0) {
+            System.exit(code);
+        }
+    }
+
+
+    @Bean CommandLineRunner run() {
+        return strings -> {
+            bindOptions();
+            exitCode = parseArguments(strings);
+
+            if (exitCode == 0) {
+                switch (opMode) {
+                    case MAPPING:
+                        try {
+                            doMapping();
+                        }
+                        catch (Exception e) {
+                            System.err.println("Mapping failed(" + e.getMessage() + ")");
+                            getLog().error("Mapping failed", e);
+                            exitCode += 2;
+                        }
+                        break;
+                    default:
+                        System.err.println("No mapping argument supplied");
+                        exitCode += 1;
+                }
+            }
+        };
+    }
+
+    private void doMapping() {
+        mappingService.mapCatalogContents();
+    }
+
+    private Options bindOptions() {
+        Options options = new Options();
+
+        // help
+        Option helpOption = new Option("h", "help", false, "Print the help");
+        options.addOption(helpOption);
+
+        // options are...
+        // -m do mapping
+
+        OptionGroup modeGroup = new OptionGroup();
+        modeGroup.setRequired(true);
+
+        Option mappingOption = new Option(
+                "m",
+                "mapping",
+                false,
+                "Map current association data in database");
+        mappingOption.setRequired(true);
+        modeGroup.addOption(mappingOption);
+        options.addOptionGroup(modeGroup);
+
+        return options;
+    }
+
+    private int parseArguments(String[] arguments) {
+        CommandLineParser parser = new GnuParser();
+        HelpFormatter help = new HelpFormatter();
+        Options options = bindOptions();
+
+        int parseArgs = 0;
+        try {
+            CommandLine cl = parser.parse(options, arguments, true);
+
+            // check for mode help option
+            if (cl.hasOption("")) {
+                // print out mode help
+                help.printHelp("goci-mapper", options, true);
+                parseArgs += 1;
+            }
+            else {
+                System.out.println("Running mapping with the following options...");
+                for (Option opt : cl.getOptions()) {
+                    System.out.println("\t" + opt.getLongOpt() +
+                                               (opt.hasArg() ? ": " + opt.getValue() : "") +
+                                               " (" + opt.getArgName() + ")");
+                }
+
+                // options are...
+                // -m do mapping
+                // required options
+                if (cl.hasOption("m")) {
+                    this.opMode = OperationMode.MAPPING;
+                }
+
+            }
+        }
+        catch (ParseException e) {
+            System.err.println("Failed to read supplied arguments (" + e.getMessage() + ")");
+            help.printHelp("goci-mapper", options, true);
+            parseArgs += 1;
+        }
+        return parseArgs;
+    }
+
+    private enum OperationMode {
+        MAPPING
+    }
+
+}
---
+++ goci-tools/goci-mapper/src/main/java/uk/ac/ebi/spot/goci/service/MappingService.java
@@ -0,0 +1,198 @@
+package uk.ac.ebi.spot.goci.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.ac.ebi.spot.goci.component.EnsemblMappingPipeline;
+import uk.ac.ebi.spot.goci.model.Association;
+import uk.ac.ebi.spot.goci.model.Curator;
+import uk.ac.ebi.spot.goci.model.Gene;
+import uk.ac.ebi.spot.goci.model.GenomicContext;
+import uk.ac.ebi.spot.goci.model.Location;
+import uk.ac.ebi.spot.goci.model.Locus;
+import uk.ac.ebi.spot.goci.model.SingleNucleotidePolymorphism;
+import uk.ac.ebi.spot.goci.model.Study;
+import uk.ac.ebi.spot.goci.repository.AssociationRepository;
+import uk.ac.ebi.spot.goci.repository.SingleNucleotidePolymorphismRepository;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by emma on 13/08/2015.
+ *
+ * @author emma
+ */
+@Service
+public class MappingService {
+
+    // Repositories
+    private AssociationRepository associationRepository;
+    private SingleNucleotidePolymorphismRepository singleNucleotidePolymorphismRepository;
+
+    // Services
+    private SnpLocationMappingService snpLocationMappingService;
+    private SnpGenomicContextMappingService snpGenomicContextMappingService;
+    private AssociationReportService associationReportService;
+    private MappingRecordService mappingRecordService;
+
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    protected Logger getLog() {
+        return log;
+    }
+
+    @Autowired
+    public MappingService(AssociationRepository associationRepository,
+                          SingleNucleotidePolymorphismRepository singleNucleotidePolymorphismRepository,
+                          SnpLocationMappingService snpLocationMappingService,
+                          SnpGenomicContextMappingService snpGenomicContextMappingService,
+                          AssociationReportService associationReportService,
+                          MappingRecordService mappingRecordService) {
+        this.associationRepository = associationRepository;
+        this.singleNucleotidePolymorphismRepository = singleNucleotidePolymorphismRepository;
+        this.snpLocationMappingService = snpLocationMappingService;
+        this.snpGenomicContextMappingService = snpGenomicContextMappingService;
+        this.associationReportService = associationReportService;
+        this.mappingRecordService = mappingRecordService;
+    }
+
+    /**
+     * Perform validation and mapping of association
+     */
+    public void mapCatalogContents() {
+
+        // Get all associations
+        Collection<Association> associations = associationRepository.findAll();
+        validateAndMapSnps(associations);
+    }
+
+    /**
+     * Perform validation and mapping of association
+     *
+     * @param associations Collection of associations to map
+     */
+    public void validateAndMapSnps(Collection<Association> associations) {
+
+        // Map to store returned location data, this is used as
+        // snpLocationMappingService process all locations linked
+        // to a single snp in one go
+        Map<String, Set<Location>> snpToLocationsMap = new HashMap<>();
+
+        // Collection to store all genomic contexts
+        Collection<GenomicContext> allGenomicContexts = new ArrayList<>();
+
+        // For each association get the loci
+        for (Association association : associations) {
+
+            // Collection to store all errors for one association
+            Collection<String> associationPipelineErrors = new ArrayList<>();
+
+            Collection<Locus> studyAssociationLoci = association.getLoci();
+
+            // For each loci get the get the SNP and author reported genes
+            for (Locus associationLocus : studyAssociationLoci) {
+                Long locusId = associationLocus.getId();
+
+                Collection<SingleNucleotidePolymorphism> snpsLinkedToLocus =
+                        singleNucleotidePolymorphismRepository.findByRiskAllelesLociId(locusId);
+
+                Collection<Gene> authorReportedGenesLinkedToSnp = associationLocus.getAuthorReportedGenes();
+
+                // Get gene names
+                Collection<String> authorReportedGeneNamesLinkedToSnp = new ArrayList<>();
+                for (Gene authorReportedGeneLinkedToSnp : authorReportedGenesLinkedToSnp) {
+                    authorReportedGeneNamesLinkedToSnp.add(authorReportedGeneLinkedToSnp.getGeneName());
+                }
+
+                // Pass rs_id and author reported genes to mapping component
+                for (SingleNucleotidePolymorphism snpLinkedToLocus : snpsLinkedToLocus) {
+                    String snpRsId = snpLinkedToLocus.getRsId();
+                    EnsemblMappingPipeline ensemblMappingPipeline =
+                            new EnsemblMappingPipeline(snpRsId, authorReportedGeneNamesLinkedToSnp);
+                    ensemblMappingPipeline.run_pipeline();
+
+                    Collection<Location> locations = ensemblMappingPipeline.getLocations();
+                    Collection<GenomicContext> snpGenomicContexts = ensemblMappingPipeline.getGenomicContexts();
+                    ArrayList<String> pipelineErrors = ensemblMappingPipeline.getPipelineErrors();
+
+                    // Store location information for SNP
+                    if (!locations.isEmpty()) {
+                        for (Location location : locations) {
+
+                            // Next time we see SNP, add location to set
+                            // This would only occur is SNP has multiple locations
+                            if (snpToLocationsMap.containsKey(snpRsId)) {
+                                snpToLocationsMap.get(snpRsId).add(location);
+                            }
+
+                            // First time we see a SNP store the location
+                            else {
+                                Set<Location> snpLocation = new HashSet<>();
+                                snpLocation.add(location);
+                                snpToLocationsMap.put(snpRsId, snpLocation);
+                            }
+                        }
+                    }
+                    else {
+                        getLog().warn("Attempt to map SNP: " + snpRsId + " returned no location details");
+                    }
+
+                    // Store genomic context data for snp
+                    if (!snpGenomicContexts.isEmpty()) {
+                        allGenomicContexts.addAll(snpGenomicContexts);
+                    }
+                    else {
+                        getLog().warn("Attempt to map SNP: " + snpRsId + " returned no mapped genes");
+                    }
+
+                    if (!pipelineErrors.isEmpty()) {
+                        associationPipelineErrors.addAll(pipelineErrors);
+                    }
+                }
+            }
+
+            // Create association report based on whether there is errors or not
+            if (!associationPipelineErrors.isEmpty()) {
+                associationReportService.processAssociationErrors(association, associationPipelineErrors);
+            }
+            else {
+                associationReportService.updateAssociationReportDetails(association);
+            }
+        }
+
+        // Save data
+        if (!snpToLocationsMap.isEmpty()) {
+            getLog().info("Adding/updating location details for SNPs" + snpToLocationsMap.keySet().toString());
+            snpLocationMappingService.storeSnpLocation(snpToLocationsMap);
+        }
+        if (!allGenomicContexts.isEmpty()) {
+            getLog().info(
+                    "Adding/updating genomic context for SNPs" + snpToLocationsMap.keySet().toString());
+            snpGenomicContextMappingService.processGenomicContext(allGenomicContexts);
+        }
+
+        // Once mapping is complete, update mapping record
+        for (Association association : associations) {
+
+            // Get current curator of study
+            Study associationStudy = association.getStudy();
+            Curator curator = associationStudy.getHousekeeping().getCurator();
+            String mappedBy = curator.getLastName();
+
+            mappingRecordService.updateAssociationMappingRecord(association, new Date(), mappedBy);
+        }
+
+    }
+
+}
+
+
+
