6e3e45be6539caa50b8f8fd89cacc1a8a3ae02e0
emma-ebi <emma@ebi.ac.uk>
2015-08-14 16:20:38 +0100

    Removed code that sends daily audit email and removed code that handles import of NCBI mapping data

--- goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/MailService.java
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/MailService.java
@@ -7,12 +7,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-import uk.ac.ebi.spot.goci.curation.model.StudyAuditView;
 import uk.ac.ebi.spot.goci.model.Study;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Collection;
 import java.util.Date;
 
 /**
@@ -87,151 +84,6 @@ public class MailService {
 
     }
 
-    // Send single email with all study errors
-    public void sendDailyAuditEmail(Collection<StudyAuditView> studiesWithNcbiErrors,
-                                    Integer totalStudiesWithNcbiErrors,
-                                    Integer totalStudiesWithImportErrors,
-                                    Integer totalNumberOfStudiesSentToNcbi,
-                                    Collection<StudyAuditView> studiesSentToNcbi) {
-
-        // Create email body
-        String emailBody = "";
-
-        // If we have errors, construct body of email
-        if (!studiesWithNcbiErrors.isEmpty()) {
-            for (StudyAuditView studyWithNcbiError : studiesWithNcbiErrors) {
-
-                // General information
-                String title = "Title: " + studyWithNcbiError.getTitle() + "\n";
-                String author = "Author: " + studyWithNcbiError.getAuthor() + "\n";
-                String pubmedId = "Pubmed Id: " + studyWithNcbiError.getPubmedId() + "\n";
-
-                // Dates
-                DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-                String sendToNCBIDate = "";
-                if (studyWithNcbiError.getSendToNCBIDate() != null) {
-                    sendToNCBIDate = df.format(studyWithNcbiError.getSendToNCBIDate());
-                }
-                String sendToNCBIDateBody = "Send To NCBI date: " + sendToNCBIDate + "\n";
-
-                String studyDate = "";
-                if (studyWithNcbiError.getPublicationDate() != null) {
-                    studyDate = df.format(studyWithNcbiError.getPublicationDate());
-                }
-                String studyDateBody = "Study Date: " + studyDate + "\n";
-
-                // Pubmed error
-                String pubmedIdErrorFound = "No";
-                if (studyWithNcbiError.getPubmedIdError() != null) {
-                    pubmedIdErrorFound = "Yes";
-                }
-                String pubmedErrorBody = "Pubmed ID error: " + pubmedIdErrorFound + "\n";
-
-                // SNP error
-                String snpErrors = "";
-                if (!studyWithNcbiError.getSnpErrors().isEmpty()) {
-                    snpErrors = studyWithNcbiError.getSnpErrors().toString();
-                    snpErrors = createErrorForEmail(snpErrors);
-                }
-                else {snpErrors = "none";}
-                String snpErrorBody = "SNP Error(s): " + snpErrors + "\n";
-
-                // Gene Not On Genome Error
-                String geneNotOnGenomeErrors = "";
-                if (!studyWithNcbiError.getGeneNotOnGenomeErrors().isEmpty()) {
-                    geneNotOnGenomeErrors = studyWithNcbiError.getGeneNotOnGenomeErrors().toString();
-                    geneNotOnGenomeErrors = createErrorForEmail(geneNotOnGenomeErrors);
-                }
-                else {geneNotOnGenomeErrors = "none";}
-                String geneNotOnGenomeErrorsBody = "Gene Not On Genome Error(s): " + geneNotOnGenomeErrors + "\n";
-
-                // SNP Gene On Different Chromosome Error
-                String snpGeneOnDiffChrErrors = "";
-                if (!studyWithNcbiError.getSnpGeneOnDiffChrErrors().isEmpty()) {
-                    snpGeneOnDiffChrErrors = studyWithNcbiError.getSnpGeneOnDiffChrErrors().toString();
-                    snpGeneOnDiffChrErrors = createErrorForEmail(snpGeneOnDiffChrErrors);
-                }
-                else {snpGeneOnDiffChrErrors = "none";}
-                String snpGeneOnDiffChrErrorsBody =
-                        "SNP Gene On Different Chromosome Error(s): " + snpGeneOnDiffChrErrors + "\n";
-
-                // No Gene For Symbol Error
-                String noGeneForSymbolErrors = "";
-                if (!studyWithNcbiError.getNoGeneForSymbolErrors().isEmpty()) {
-                    noGeneForSymbolErrors = studyWithNcbiError.getNoGeneForSymbolErrors().toString();
-                    noGeneForSymbolErrors = createErrorForEmail(noGeneForSymbolErrors);
-                }
-                else {noGeneForSymbolErrors = "none";}
-                String noGeneForSymbolErrorsBody = "No Gene For Symbol Error(s): " + noGeneForSymbolErrors + "\n";
-
-                // Edit link
-                Long studyId = studyWithNcbiError.getStudyId();
-                String editStudyLink = "Edit link: http://garfield.ebi.ac.uk:8080/gwas/curation/studies/" + studyId;
-
-                // Create email body
-                emailBody = emailBody + "\n" + title + author + studyDateBody + sendToNCBIDateBody + pubmedId +
-                        pubmedErrorBody +
-                        snpErrorBody +
-                        geneNotOnGenomeErrorsBody + snpGeneOnDiffChrErrorsBody + noGeneForSymbolErrorsBody +
-                        editStudyLink +
-                        "\n";
-            }
-        }
-
-        else {
-            emailBody = "\nNo errors found\n";
-        }
-
-        // Create summary view of studies sent to NCBI
-        String sentToNcbiSummary = "";
-        if (!studiesSentToNcbi.isEmpty()) {
-            for (StudyAuditView studySentToNcbi : studiesSentToNcbi) {
-                String title = studySentToNcbi.getTitle();
-                String author = studySentToNcbi.getAuthor();
-                String pubmedId = studySentToNcbi.getPubmedId();
-
-                // Edit link
-                Long studyId = studySentToNcbi.getStudyId();
-                String editStudyLink = "Edit link: http://garfield.ebi.ac.uk:8080/gwas/curation/studies/" + studyId;
-
-                sentToNcbiSummary =
-                        sentToNcbiSummary + "\n" + title + "\t" + author + "\t" + pubmedId + "\t" + editStudyLink +
-                                "\n";
-
-            }
-        }
-
-        else {sentToNcbiSummary = "No studies sent to NCBI";}
-
-
-        // Format mail message
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(getTo());
-        mailMessage.setFrom(getFrom());
-        mailMessage.setSubject("GWAS Curation daily audit report");
-        mailMessage.setText(
-                "\nSummary of studies sent to NCBI for mapping: " + "\n" +
-                        "Total number of studies with status 'Send To NCBI' before pipeline ran (see details below): " +
-                        totalNumberOfStudiesSentToNcbi + "\n"
-                        + sentToNcbiSummary
-                        + "\n\nSummary of errors:\n" +
-                        "Total number of studies with data import errors: " + totalStudiesWithImportErrors +
-                        "\n" + "Total number of studies with NCBI pipeline errors (see details below): " +
-                        totalStudiesWithNcbiErrors + "\n" + emailBody);
-
-        getLog().info("Sending daily audit email");
-        javaMailSender.send(mailMessage);
-    }
-
-
-    // Format text for email
-    private String createErrorForEmail(String errorString) {
-        String emailString = errorString;
-        emailString = emailString.replaceAll("\\[", "");
-        emailString = emailString.replaceAll("]", "");
-        emailString = emailString.replaceAll(", ", "\n");
-        return emailString;
-    }
 
     // Getter and setters
     public String getFrom() {
