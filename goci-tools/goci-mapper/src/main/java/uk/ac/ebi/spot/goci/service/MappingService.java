6398e1565a8d1cf1f289e6ed17db76d2e00789b8
emma-ebi <emma@ebi.ac.uk>
2015-08-18 13:28:55 +0100

    Added trim to code that retrieves aouthor reported gene name

--- goci-tools/goci-mapper/src/main/java/uk/ac/ebi/spot/goci/service/MappingService.java
+++ goci-tools/goci-mapper/src/main/java/uk/ac/ebi/spot/goci/service/MappingService.java
@@ -110,7 +110,7 @@ public class MappingService {
                 // Get gene names
                 Collection<String> authorReportedGeneNamesLinkedToSnp = new ArrayList<>();
                 for (Gene authorReportedGeneLinkedToSnp : authorReportedGenesLinkedToSnp) {
-                    authorReportedGeneNamesLinkedToSnp.add(authorReportedGeneLinkedToSnp.getGeneName());
+                    authorReportedGeneNamesLinkedToSnp.add(authorReportedGeneLinkedToSnp.getGeneName().trim());
                 }
 
                 // Pass rs_id and author reported genes to mapping component
