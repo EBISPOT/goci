6fd0ee54547df6d5919f1438366b3db0b8b3782b
emma-ebi <emma@ebi.ac.uk>
2015-08-27 09:18:33 +0100

    Two new components to handle including Ensembl upstream and downstream genes

---
+++ goci-tools/goci-utils/src/main/java/uk/ac/ebi/spot/goci/repository/mapper/DownloadEnsemblUpstreamGeneId.java
@@ -0,0 +1,39 @@
+package uk.ac.ebi.spot.goci.repository.mapper;
+
+import org.springframework.stereotype.Component;
+import uk.ac.ebi.spot.goci.model.CatalogDataMapper;
+import uk.ac.ebi.spot.goci.model.CatalogHeaderBinding;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by emma on 27/08/2015. * @author emma
+ * <p>
+ * Mapper component to handle scenario where a mapped Ensembl gene is present. In this case the download should not
+ * include details of upstream gene IDs.
+ */
+@Component
+public class DownloadEnsemblUpstreamGeneId implements CatalogDataMapper {
+    @Override public List<CatalogHeaderBinding> getRequiredDatabaseFields() {
+        return Arrays.asList(CatalogHeaderBinding.ENSEMBL_MAPPED_GENE,
+                             CatalogHeaderBinding.ENSEMBL_UPSTREAM_GENE_ID);
+    }
+
+    @Override public CatalogHeaderBinding getOutputField() {
+        return CatalogHeaderBinding.DOWNLOAD_ENSEMBL_UPSTREAM_GENE_ID;
+    }
+
+    @Override public String produceOutput(Map<CatalogHeaderBinding, String> databaseValues) {
+        String output;
+        String mapped = databaseValues.get(CatalogHeaderBinding.ENSEMBL_MAPPED_GENE);
+        if (mapped.isEmpty()) {
+            output = databaseValues.get(CatalogHeaderBinding.ENSEMBL_UPSTREAM_GENE_ID);
+        }
+        else {
+            output = "";
+        }
+        return output;
+    }
+}
