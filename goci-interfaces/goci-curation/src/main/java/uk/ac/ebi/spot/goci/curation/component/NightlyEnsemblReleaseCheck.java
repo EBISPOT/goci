0249b69dac291c6694c5e421f6fb1555afb21b46
emma-ebi <emma@ebi.ac.uk>
2015-09-28 12:52:11 +0100

    First pass at component that runs nightly check of Ensembl release

---
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/component/NightlyEnsemblReleaseCheck.java
@@ -0,0 +1,12 @@
+package uk.ac.ebi.spot.goci.curation.component;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by emma on 28/09/2015.
+ * <p>
+ * Scheduled component that runs nightly and pings Ensembl REST API to check for changes in release number.
+ */
+@Component
+public class NightlyEnsemblReleaseCheck {
+}
