4414e65e9bd0f41b9fb09ffa45c42c842e0033aa
emma-ebi <emma@ebi.ac.uk>
2015-10-05 12:53:23 +0100

    Added selection of query methods

--- goci-core/goci-repository/src/main/java/uk/ac/ebi/spot/goci/repository/MonthlyTotalsSummaryViewRepository.java
+++ goci-core/goci-repository/src/main/java/uk/ac/ebi/spot/goci/repository/MonthlyTotalsSummaryViewRepository.java
@@ -1,8 +1,11 @@
 package uk.ac.ebi.spot.goci.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import uk.ac.ebi.spot.goci.model.MonthlyTotalsSummaryView;
+
+import java.util.List;
 
 /**
  * Created by emma on 22/09/2015.
@@ -13,6 +16,22 @@ import uk.ac.ebi.spot.goci.model.MonthlyTotalsSummaryView;
  */
 @RepositoryRestResource
 public interface MonthlyTotalsSummaryViewRepository extends JpaRepository<MonthlyTotalsSummaryView, Long> {
+
+    List<MonthlyTotalsSummaryView> findByYearOrderByYearDesc(Integer year);
+
+    List<MonthlyTotalsSummaryView> findByYearAndMonthOrderByYearDesc(Integer year, Integer month);
+
+    List<MonthlyTotalsSummaryView> findByCurator(String curator);
+
+    List<MonthlyTotalsSummaryView> findByCurationStatus(String curationStatus);
+
+    List<MonthlyTotalsSummaryView> findByCuratorAndCurationStatus(String curator, String curationStatus);
+
+    @Query("select distinct month from MonthlyTotalsSummaryView order by month desc")
+    List<Integer> getAllMonths();
+
+    @Query("select distinct year from MonthlyTotalsSummaryView order by year desc")
+    List<Integer> getAllYears();
 
 
 }
