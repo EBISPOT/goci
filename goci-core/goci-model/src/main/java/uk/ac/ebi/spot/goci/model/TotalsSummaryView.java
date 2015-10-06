b2fee7001a8314e26422d90eda837b6bde5158cc
emma-ebi <emma@ebi.ac.uk>
2015-10-06 14:37:35 +0100

    Added getter/setter for ID

--- goci-core/goci-model/src/main/java/uk/ac/ebi/spot/goci/model/TotalsSummaryView.java
+++ goci-core/goci-model/src/main/java/uk/ac/ebi/spot/goci/model/TotalsSummaryView.java
@@ -31,6 +31,14 @@ public class TotalsSummaryView {
         this.curationStatus = curationStatus;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public Integer getYear() {
         return year;
     }
