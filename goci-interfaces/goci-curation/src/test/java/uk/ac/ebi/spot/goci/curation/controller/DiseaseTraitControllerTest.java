1d5661148b10b848a00219a68dda38fb8772baac
emma-ebi <emma@ebi.ac.uk>
2015-10-19 17:19:19 +0100

    Further updates

--- goci-interfaces/goci-curation/src/test/java/uk/ac/ebi/spot/goci/curation/controller/DiseaseTraitControllerTest.java
+++ goci-interfaces/goci-curation/src/test/java/uk/ac/ebi/spot/goci/curation/controller/DiseaseTraitControllerTest.java
@@ -4,82 +4,85 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
-import uk.ac.ebi.spot.goci.CurationApplication;
 import uk.ac.ebi.spot.goci.model.DiseaseTrait;
 import uk.ac.ebi.spot.goci.repository.DiseaseTraitRepository;
 import uk.ac.ebi.spot.goci.repository.StudyRepository;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 /**
  * Created by emma on 15/10/2015.
+ *
  * @author emma
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = CurationApplication.class)
+@SpringApplicationConfiguration(classes = MockServletContext.class)
 @WebAppConfiguration
 public class DiseaseTraitControllerTest {
 
-    @Autowired
-    private DiseaseTraitController diseaseTraitController;
+    private StudyRepository studyRepositoryMock;
 
-    private MockMvc mvc;
+    private DiseaseTraitRepository diseaseTraitRepositoryMock;
+
+    private DiseaseTraitController diseaseTraitControllerMock;
 
     @Before
-    public void setUp() throws Exception {
-        mvc = MockMvcBuilders.standaloneSetup(diseaseTraitController).build();
+    public void setupMocks() {
+        diseaseTraitRepositoryMock = Mockito.mock(DiseaseTraitRepository.class);
+        studyRepositoryMock = Mockito.mock(StudyRepository.class);
+        diseaseTraitControllerMock = new DiseaseTraitController(diseaseTraitRepositoryMock, studyRepositoryMock);
     }
 
     @Test
-    public void getDiseaseTraits() throws Exception {
+    public void testEndpoint() throws Exception {
+
+        // Mock disease trait object
+        DiseaseTrait diseaseTrait = Mockito.mock(DiseaseTrait.class);
+        List<DiseaseTrait> diseaseTraits = Collections.singletonList(diseaseTrait);
+        Mockito.when(diseaseTraitRepositoryMock.findAll(Mockito.any(Sort.class))).thenReturn(diseaseTraits);
+
+        MockMvc mvc = MockMvcBuilders.standaloneSetup(diseaseTraitControllerMock).build();
+
         mvc.perform(MockMvcRequestBuilders.get("/diseasetraits").accept(MediaType.TEXT_HTML_VALUE))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(view().name("disease_traits"));
     }
 
     @Test
     public void testAllDiseaseTraits() {
 
+        // Mock disease trait object
         DiseaseTrait diseaseTrait = Mockito.mock(DiseaseTrait.class);
-        List <DiseaseTrait> diseaseTraits = Collections.singletonList(diseaseTrait);
-        DiseaseTraitRepository diseaseTraitRepository = Mockito.mock(DiseaseTraitRepository.class);
-        Mockito.when(diseaseTraitRepository.findAll(Mockito.any(Sort.class))).thenReturn(diseaseTraits);
+        List<DiseaseTrait> diseaseTraits = Collections.singletonList(diseaseTrait);
+        Integer totalDiseaseTraits = diseaseTraits.size();
 
-        StudyRepository studyRepository = Mockito.mock(StudyRepository.class);
-        DiseaseTraitController diseaseTraitControllerMock = new DiseaseTraitController(diseaseTraitRepository, studyRepository);
-
-
+        // Stubbing
+        Mockito.when(diseaseTraitRepositoryMock.findAll(Mockito.any(Sort.class))).thenReturn(diseaseTraits);
 
         Model mockModel = Mockito.mock(Model.class);
-        String s = diseaseTraitController.allDiseaseTraits(mockModel);
+        String s = diseaseTraitControllerMock.allDiseaseTraits(mockModel);
+        assertEquals("disease_traits", s);
 
-        // Check the view name
-        // Check model attributes
+        Mockito.verify(diseaseTraitRepositoryMock, Mockito.times(2)).findAll(Mockito.any(Sort.class));
+        Mockito.verify(mockModel).addAttribute("diseaseTraits", diseaseTraits);
+        Mockito.verify(mockModel).addAttribute("totaldiseaseTraits", totalDiseaseTraits);
 
-
-        Mockito.verify(mockModel).addAttribute("diseaseTraits",diseaseTraits);
-
-//        model.addAttribute("diseaseTraits", diseaseTraitRepository.findAll(sortByTraitAsc()));
-//        model.addAttribute("totaldiseaseTraits" , diseaseTraitRepository.findAll(sortByTraitAsc()).size());
-//
-//        // Return an empty DiseaseTrait object so user can add a new one
-//        model.addAttribute("diseaseTrait", new DiseaseTrait());
-//
-//        return "disease_traits";
+        // TODO DOES NOT WORK
+        Mockito.verify(mockModel).addAttribute("diseaseTrait", diseaseTrait);
     }
-
-
 }
