2b643f085f4314f4613b7de8a2a19d381d50858b
emma-ebi <emma@ebi.ac.uk>
2015-10-21 17:06:07 +0100

    New dto to store information returned from thymeleaf form to controller

---
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/model/Assignee.java
@@ -0,0 +1,28 @@
+package uk.ac.ebi.spot.goci.curation.model;
+
+/**
+ * Created by emma on 21/10/2015.
+ *
+ * @author emma
+ *         <p>
+ *         DTO used to store the curator that user wishes to assign a study to
+ */
+public class Assignee {
+
+    private Long curatorId;
+
+    public Assignee() {
+    }
+
+    public Assignee(Long curatorId) {
+        this.curatorId = curatorId;
+    }
+
+    public Long getCuratorId() {
+        return curatorId;
+    }
+
+    public void setCuratorId(Long curatorId) {
+        this.curatorId = curatorId;
+    }
+}
