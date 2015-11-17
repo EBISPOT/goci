5bb735d11991b468b18c1744aefd841883b38743
emma-ebi <emma@ebi.ac.uk>
2015-11-17 13:04:02 +0000

    Added new attribute for genome build and dbsnp version

--- goci-core/goci-model/src/main/java/uk/ac/ebi/spot/goci/model/MappingMetadata.java
+++ goci-core/goci-model/src/main/java/uk/ac/ebi/spot/goci/model/MappingMetadata.java
@@ -21,6 +21,10 @@ public class MappingMetadata {
 
     private Integer ensemblReleaseNumber;
 
+    private String genomeBuildVersion;
+
+    private Integer dbsnpVersion;
+
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date usageStartDate;
 
@@ -29,8 +33,13 @@ public class MappingMetadata {
     public MappingMetadata() {
     }
 
-    public MappingMetadata(Integer ensemblReleaseNumber, Date usageStartDate) {
+    public MappingMetadata(Integer ensemblReleaseNumber,
+                           String genomeBuildVersion,
+                           Integer dbsnpVersion,
+                           Date usageStartDate) {
         this.ensemblReleaseNumber = ensemblReleaseNumber;
+        this.genomeBuildVersion = genomeBuildVersion;
+        this.dbsnpVersion = dbsnpVersion;
         this.usageStartDate = usageStartDate;
     }
 
@@ -56,5 +65,21 @@ public class MappingMetadata {
 
     public void setUsageStartDate(Date usageStartDate) {
         this.usageStartDate = usageStartDate;
+    }
+
+    public String getGenomeBuildVersion() {
+        return genomeBuildVersion;
+    }
+
+    public void setGenomeBuildVersion(String genomeBuildVersion) {
+        this.genomeBuildVersion = genomeBuildVersion;
+    }
+
+    public Integer getDbsnpVersion() {
+        return dbsnpVersion;
+    }
+
+    public void setDbsnpVersion(Integer dbsnpVersion) {
+        this.dbsnpVersion = dbsnpVersion;
     }
 }
