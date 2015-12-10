d4e7b6b9a1f72f6347aba9134f153e146f741f9f
emma-ebi <emma@ebi.ac.uk>
2015-12-10 10:53:52 +0000

    Added ability to unapprove checked SNPs

--- goci-interfaces/goci-curation/src/main/resources/static/js/goci-study-association.js
+++ goci-interfaces/goci-curation/src/main/resources/static/js/goci-study-association.js
@@ -30,6 +30,39 @@ function updateAssociations(studyId, associationIds) {
               });
 }
 
+
+// Unpprove individual SNP associations
+$(document).ready(function() {
+    $("#unapprove-button").click(function() {
+        // Passes study id to method
+        unapproveSelectedAssociations();
+    });
+});
+
+function unapproveSelectedAssociations() {
+    var associationIds = [];
+    //create an array of association ids
+    $('input.association-selector:checked').each(
+            function() {
+                associationIds.push($(this).attr("value"))
+            }
+    );
+    unapproveAssociations(associationIds);
+}
+
+function unapproveAssociations(studyId, associationIds) {
+    // Pass details to method in controller which handles database changes
+    $.getJSON("associations/unapprove_checked",
+              {"associationIds[]": associationIds},
+              //Response
+              function(data) {
+                  alert(data.message);
+                  //Reload page
+                  location.reload();
+              });
+}
+
+
 // Delete individual SNP associations
 $(document).ready(function() {
     $("#delete-button").click(function() {
