17dcce6c7ed84247287bdeb7d55b052d42a74f73
emma-ebi <emma@ebi.ac.uk>
2015-12-14 16:37:48 +0000

    Refactored this so common error checks are in a single method

--- goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/AssociationComponentsSyntaxChecks.java
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/AssociationComponentsSyntaxChecks.java
@@ -15,25 +15,16 @@ public class AssociationComponentsSyntaxChecks {
     public AssociationComponentsSyntaxChecks() {
     }
 
-    // Check for common errors in snp and risk allele names
-    public String checkSnpOrProxy(String snpValue) {
+    /**
+     * Check value for common syntax errors
+     *
+     * @param snpValue SNP RS_ID
+     */
+    public String checkSnp(String snpValue) {
 
         String error = "";
-        if (snpValue.contains(",")) {
-            error = "SNP " + snpValue + " contains a ',' character.";
-        }
-        if (snpValue.contains("x")) {
-            error = error + "SNP " + snpValue + " contains an 'x' character.";
-        }
-        if (snpValue.contains("X")) {
-            error = error + "SNP " + snpValue + " contains an 'X' character.";
-        }
-        if (snpValue.contains(":")) {
-            error = error + "SNP " + snpValue + " contains a ':' character.";
-        }
-        if (snpValue.contains(";")) {
-            error = error + "SNP " + snpValue + " contains a ';' character.";
-        }
+        error = runCommonChecks(snpValue, "SNP");
+
         if (snpValue.contains("-")) {
             error = error + "SNP " + snpValue + " contains a '-' character.";
         }
@@ -44,26 +35,69 @@ public class AssociationComponentsSyntaxChecks {
         return error;
     }
 
+    /**
+     * Check value for common syntax errors
+     *
+     * @param snpValue Proxy SNP RS_ID
+     */
+    public String checkProxy(String snpValue) {
+
+        String error = "";
+        error = runCommonChecks(snpValue, "Proxy SNP");
+
+        if (snpValue.contains("-")) {
+            error = error + "SNP " + snpValue + " contains a '-' character.";
+        }
+        if (!snpValue.equals("NR")) {
+            if (!snpValue.startsWith("rs")) {
+                error = error + "SNP " + snpValue + " does not start with rs.";
+            }
+        }
+
+        return error;
+    }
+
+    /**
+     * Check value for common syntax errors
+     *
+     * @param riskAllele Risk allele name
+     */
     public String checkRiskAllele(String riskAllele) {
 
         String error = "";
-        if (riskAllele.contains(",")) {
-            error = "Risk Allele " + riskAllele + " contains a ',' character.";
-        }
-        if (riskAllele.contains("x")) {
-            error = error + "Risk Allele " + riskAllele + " contains an 'x' character.";
-        }
-        if (riskAllele.contains("X")) {
-            error = error + "Risk Allele " + riskAllele + " contains an 'X' character.";
-        }
-        if (riskAllele.contains(":")) {
-            error = error + "Risk Allele " + riskAllele + " contains a ':' character.";
-        }
-        if (riskAllele.contains(";")) {
-            error = error + "Risk Allele " + riskAllele + " contains a ';' character.";
-        }
+        error = runCommonChecks(riskAllele, "Risk Allele");
+
         if (!riskAllele.startsWith("rs")) {
             error = error + "Risk Allele " + riskAllele + " does not start with rs.";
+        }
+
+        return error;
+    }
+
+    /**
+     * Check value for common syntax errors
+     *
+     * @param value     value to check
+     * @param valueType Type of value i.e. SNP, Proxy SNP, Risk Allele
+     */
+
+    public String runCommonChecks(String value, String valueType) {
+
+        String error = "";
+        if (value.contains(",")) {
+            error = valueType + " " + value + " contains a ',' character.";
+        }
+        if (value.contains("x")) {
+            error = error + valueType + " " + value + " contains an 'x' character.";
+        }
+        if (value.contains("X")) {
+            error = error + valueType + " " + value + " contains an 'X' character.";
+        }
+        if (value.contains(":")) {
+            error = error + valueType + " " + value + " contains a ':' character.";
+        }
+        if (value.contains(";")) {
+            error = error + valueType + " " + value + " contains a ';' character.";
         }
 
         return error;
