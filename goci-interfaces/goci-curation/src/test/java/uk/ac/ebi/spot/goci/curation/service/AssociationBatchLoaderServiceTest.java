09289e06d22c89f8d08355c9c2a1418882da640d
emma-ebi <emma@ebi.ac.uk>
2015-12-18 13:46:35 +0000

    New unit test

---
+++ goci-interfaces/goci-curation/src/test/java/uk/ac/ebi/spot/goci/curation/service/AssociationBatchLoaderServiceTest.java
@@ -0,0 +1,64 @@
+package uk.ac.ebi.spot.goci.curation.service;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import uk.ac.ebi.spot.goci.model.Association;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by emma on 18/12/2015.
+ *
+ * @author emma
+ *         <p>
+ *         Unit test for AssociationBatchLoaderService
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class AssociationBatchLoaderServiceTest {
+
+    @InjectMocks
+    private AssociationBatchLoaderService associationBatchLoaderService;
+
+    @Mock
+    private AssociationSheetProcessor associationSheetProcessor;
+
+    @Mock
+    private XSSFSheet sheet;
+
+    @Rule
+    public TemporaryFolder folder= new TemporaryFolder();
+
+    @Test
+    public void testProcessData() throws IOException, InvalidFormatException {
+
+        Collection<Association> testAssociations = new ArrayList<>();
+
+        // Checking return type
+/*     File temp= new File();
+        Collection<Association> returnedCollection = associationBatchLoaderService.processData(temp.getPath());
+        assertEquals(returnedCollection, testAssociations);*/
+
+        // Stubbing behaviour of Association Sheet Processor
+        when(associationSheetProcessor.readSnpAssociations(sheet)).thenReturn(testAssociations);
+        assertEquals(testAssociations, associationSheetProcessor.readSnpAssociations(sheet));
+        verify(associationSheetProcessor, atLeastOnce()).readSnpAssociations(sheet);
+
+    }
+
+    // TODO TEST EXCEPTIONS ARE THROWN
+}
