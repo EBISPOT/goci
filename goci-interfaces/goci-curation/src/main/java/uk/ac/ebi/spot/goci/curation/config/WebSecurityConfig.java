acfa0086e65bbcde474e946b490dcb8e48f7e879
emma-ebi <emma@ebi.ac.uk>
2016-01-05 17:27:57 +0000

    Moved location of this file and refactored some code

--- goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/config/WebSecurityConfig.java
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/config/WebSecurityConfig.java
@@ -1,15 +1,14 @@
 package uk.ac.ebi.spot.goci.curation.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import uk.ac.ebi.spot.goci.curation.service.CurrentUserDetailsService;
 
 
 /**
@@ -18,20 +17,19 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
  * @author emma
  */
 
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private UserDetailsService userDetailsService;
+    private CurrentUserDetailsService currentUserDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/users/**").hasAuthority("admin")
+                .antMatchers("/").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -44,14 +42,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/")
-                .permitAll().and().rememberMe();
+                .permitAll();
     }
 
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .userDetailsService(userDetailsService)
+                .userDetailsService(currentUserDetailsService)
                 .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+        return currentUserDetailsService;
     }
 }
