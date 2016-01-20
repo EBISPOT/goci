7f02a509de3d44253432f55e56cb72c42785f446
emma-ebi <emma@ebi.ac.uk>
2016-01-20 17:41:05 +0000

    New object to hold results of mapping

---
+++ goci-tools/goci-mapper/src/main/java/uk/ac/ebi/spot/goci/model/EnsemblMappingResult.java
@@ -0,0 +1,71 @@
+package uk.ac.ebi.spot.goci.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by emma on 20/01/2016.
+ *
+ * @author emma
+ *         <p>
+ *         DTO that holds results of Ensembl REST API call
+ */
+
+
+public class EnsemblMappingResult {
+
+    private Integer merged;
+
+    private Collection<Location> locations = new ArrayList<>();
+
+    private Collection<GenomicContext> genomic_contexts = new ArrayList<>();
+
+    private ArrayList<String> pipeline_errors = new ArrayList<>();
+
+    private String functionalClass;
+
+
+    public EnsemblMappingResult() {
+    }
+
+
+    public ArrayList<String> getPipeline_errors() {
+        return pipeline_errors;
+    }
+
+    public void setPipeline_errors(ArrayList<String> pipeline_errors) {
+        this.pipeline_errors = pipeline_errors;
+    }
+
+    public Collection<GenomicContext> getGenomic_contexts() {
+        return genomic_contexts;
+    }
+
+    public void setGenomic_contexts(Collection<GenomicContext> genomic_contexts) {
+        this.genomic_contexts = genomic_contexts;
+    }
+
+    public Collection<Location> getLocations() {
+        return locations;
+    }
+
+    public void setLocations(Collection<Location> locations) {
+        this.locations = locations;
+    }
+
+    public Integer getMerged() {
+        return merged;
+    }
+
+    public void setMerged(Integer merged) {
+        this.merged = merged;
+    }
+
+    public String getFunctionalClass() {
+        return functionalClass;
+    }
+
+    public void setFunctionalClass(String functionalClass) {
+        this.functionalClass = functionalClass;
+    }
+}
