315c6a81c9e34a6b91541a5b05896c4f9396d6a1
emma-ebi <emma@ebi.ac.uk>
2016-01-21 14:24:22 +0000

    Re-wrote this service to use standard spring method for accessing REST data

---
+++ goci-tools/goci-mapper/src/main/java/uk/ac/ebi/spot/goci/model/EnsemblDbsnpVersionJson.java
@@ -0,0 +1,24 @@
+package uk.ac.ebi.spot.goci.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * Created by emma on 21/01/2016.
+ * @author emma
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class EnsemblDbsnpVersionJson {
+
+    private String version;
+
+    public EnsemblDbsnpVersionJson() {
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
