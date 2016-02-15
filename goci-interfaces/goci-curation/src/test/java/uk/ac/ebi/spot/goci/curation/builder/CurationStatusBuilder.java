5ee0010ba4c7218ec5c6e510f74e205ddbabcecf
emma-ebi <emma@ebi.ac.uk>
2016-02-15 13:57:57 +0000

    Create curation status builder for testing

---
+++ goci-interfaces/goci-curation/src/test/java/uk/ac/ebi/spot/goci/curation/builder/CurationStatusBuilder.java
@@ -0,0 +1,30 @@
+package uk.ac.ebi.spot.goci.curation.builder;
+
+
+import uk.ac.ebi.spot.goci.model.CurationStatus;
+
+/**
+ * Created by emma on 15/02/2016.
+ *
+ * @author emma
+ *         <p>
+ *         Status builder used in testing
+ */
+public class CurationStatusBuilder {
+
+    private CurationStatus curationStatus;
+
+    public CurationStatusBuilder setId(Long id) {
+        curationStatus.setId(id);
+        return this;
+    }
+
+    public CurationStatusBuilder setStatus(String status) {
+        curationStatus.setStatus(status);
+        return this;
+    }
+
+    public CurationStatus build() {
+        return curationStatus;
+    }
+}
