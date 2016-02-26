39780820744c15806bde83f04f2f66129b45ee73
emma-ebi <emma@ebi.ac.uk>
2016-02-26 12:51:18 +0000

    Update to interface to remove method that creates an association

--- goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/SnpAssociationFormService.java
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/SnpAssociationFormService.java
@@ -11,12 +11,6 @@ import uk.ac.ebi.spot.goci.model.Association;
  *         A component that takes a form containing association details and creates an associationobject or vice versa
  */
 public interface SnpAssociationFormService {
-    /**
-     * Create association from values returned in form
-     *
-     * @return Association, which is normally saved in database
-     */
-    Association createAssociation(SnpAssociationForm form);
 
     /**
      * Create form from association
