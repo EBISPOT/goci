aef5e12ef0726a0c467721f05a4d5470c283f1dc
emma-ebi <emma@ebi.ac.uk>
2016-03-07 13:22:09 +0000

    Add default method to set common association parameters

--- goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/SnpAssociationFormService.java
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/SnpAssociationFormService.java
@@ -18,4 +18,39 @@ public interface SnpAssociationFormService {
      * @return Form, which is presented via controller to view
      */
     SnpAssociationForm createForm(Association association);
+
+    /**
+     * Set common association attributes
+     *
+     * @return Form, contains details of association to create
+     */
+    default Association setCommonAssociationElements(SnpAssociationForm form) {
+
+        Association association = new Association();
+
+        // Set common string, boolean and float association attributes
+        association.setPvalueText(form.getPvalueText());
+        association.setSnpType(form.getSnpType());
+        association.setSnpApproved(form.getSnpApproved());
+        association.setRange(form.getRange());
+        association.setStandardError(form.getStandardError());
+        association.setDescription(form.getDescription());
+        association.setRiskFrequency(form.getRiskFrequency());
+
+        // Add collection of EFO traits
+        association.setEfoTraits(form.getEfoTraits());
+
+        // Set mantissa and exponent
+        association.setPvalueMantissa(form.getPvalueMantissa());
+        association.setPvalueExponent(form.getPvalueExponent());
+
+        // Set OR/Beta values
+        association.setOrPerCopyNum(form.getOrPerCopyNum());
+        association.setOrPerCopyRecip(form.getOrPerCopyRecip());
+        association.setOrPerCopyRecipRange(form.getOrPerCopyRecipRange());
+        association.setBetaNum(form.getBetaNum());
+        association.setBetaDirection(form.getBetaDirection());
+        association.setBetaUnit(form.getBetaUnit());
+        return association;
+    }
 }
