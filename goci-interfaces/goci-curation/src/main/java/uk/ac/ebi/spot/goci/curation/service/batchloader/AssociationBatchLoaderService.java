44f32bab38b3172fea423de15b63fc786e5d27cb
emma-ebi <emma@ebi.ac.uk>
2016-03-21 15:26:29 +0000

    New method added to check errors

--- goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/batchloader/AssociationBatchLoaderService.java
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/batchloader/AssociationBatchLoaderService.java
@@ -1,6 +1,7 @@
 package uk.ac.ebi.spot.goci.curation.service.batchloader;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import uk.ac.ebi.spot.goci.curation.model.BatchUploadError;
 import uk.ac.ebi.spot.goci.model.Association;
 
 import java.io.IOException;
@@ -11,12 +12,15 @@ import java.util.Collection;
  *
  * @author emma
  *         <p>
- *         This is a small program to upload a batch of SNPs from a .xlsx spreadsheet. Note that the spreadsheet must be
- *         of .xlsx format!
+ *         Upload, error check and create persistance obejects that can be saved in DB from a batch of SNPs from a .xlsx
+ *         spreadsheet. Note that the spreadsheet must be of .xlsx format!
  *         <p>
  *         Created from code originally written by Dani. Adapted to fit with new curation system.
  */
 public interface AssociationBatchLoaderService {
 
+    Collection<BatchUploadError> checkUploadForErrors(String fileName) throws InvalidFormatException, IOException;
+
     Collection<Association> processData(String fileName) throws InvalidFormatException, IOException;
+
 }
