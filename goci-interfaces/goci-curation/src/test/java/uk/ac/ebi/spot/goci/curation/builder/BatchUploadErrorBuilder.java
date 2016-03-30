08d4578cc8ed563a4aa4b32842346bf3074c02ea
emma-ebi <emma@ebi.ac.uk>
2016-03-30 17:07:19 +0100

    New builder

---
+++ goci-interfaces/goci-curation/src/test/java/uk/ac/ebi/spot/goci/curation/builder/BatchUploadErrorBuilder.java
@@ -0,0 +1,36 @@
+package uk.ac.ebi.spot.goci.curation.builder;
+
+import uk.ac.ebi.spot.goci.curation.model.batchloader.BatchUploadError;
+
+/**
+ * Created by emma on 30/03/2016.
+ *
+ * @author emma
+ *         <p>
+ *         Builder for BatchUploadError
+ */
+public class BatchUploadErrorBuilder {
+
+    private BatchUploadError batchUploadError = new BatchUploadError();
+    
+    public BatchUploadErrorBuilder setRow(Integer row) {
+        batchUploadError.setRow(row);
+        return this;
+    }
+
+    public BatchUploadErrorBuilder setColumnName(String columnName) {
+        batchUploadError.setColumnName(columnName);
+        return this;
+    }
+
+    public BatchUploadErrorBuilder setError(String error) {
+        batchUploadError.setError(error);
+        return this;
+    }
+
+    public BatchUploadError build() {
+        return batchUploadError;
+    }
+
+
+}
