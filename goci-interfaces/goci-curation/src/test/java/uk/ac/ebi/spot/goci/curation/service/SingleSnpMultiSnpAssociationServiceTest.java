f9ce535c09cbd8f81294f635b9bf585e9de3d9bb
emma-ebi <emma@ebi.ac.uk>
2016-04-08 15:30:32 +0100

    Separated out elements for interaction association

--- goci-interfaces/goci-curation/src/test/java/uk/ac/ebi/spot/goci/curation/service/SingleSnpMultiSnpAssociationServiceTest.java
+++ goci-interfaces/goci-curation/src/test/java/uk/ac/ebi/spot/goci/curation/service/SingleSnpMultiSnpAssociationServiceTest.java
@@ -61,7 +61,7 @@ public class SingleSnpMultiSnpAssociationServiceTest {
     @Mock
     private LociAttributesService lociAttributesService;
 
-    private SingleSnpMultiSnpAssociationService singleSnpMultiSnpAssociationService;
+    private SnpAssociationFormService snpAssociationFormService;
 
     // Entity objects
     private static final EfoTrait EFO_01 = new EfoTraitBuilder()
@@ -81,10 +81,7 @@ public class SingleSnpMultiSnpAssociationServiceTest {
     private static final Gene GENE_02 = new GeneBuilder().setId(113L).setGeneName("FRS2").build();
 
     private static final Gene GENE_03 = new GeneBuilder().setId(113L).setGeneName("ELF1").build();
-
-    private static final Gene GENE_04 = new GeneBuilder().setId(113L).setGeneName("WBP4").build();
-
-    private static final Gene GENE_05 = new GeneBuilder().setId(113L).setGeneName("SLC25A15").build();
+    ;
 
     private static final Region REGION_01 = new RegionBuilder().setId(897L).setName("9q33.1").build();
 
@@ -124,17 +121,6 @@ public class SingleSnpMultiSnpAssociationServiceTest {
             .setRsId("rs114205691")
             .build();
 
-    private static final SingleNucleotidePolymorphism SNP_04 = new SingleNucleotidePolymorphismBuilder().setId(311L)
-            .setLastUpdateDate(new Date())
-            .setRsId("rs9788721")
-            .build();
-
-    private static final SingleNucleotidePolymorphism SNP_05 = new SingleNucleotidePolymorphismBuilder().setId(311L)
-            .setLastUpdateDate(new Date())
-            .setRsId("rs8042374")
-            .build();
-
-
     private static final RiskAllele RISK_ALLELE_01 = new RiskAlleleBuilder().setId(411L)
             .setRiskAlleleName("rs579459-?")
             .build();
@@ -147,15 +133,6 @@ public class SingleSnpMultiSnpAssociationServiceTest {
             .setRiskAlleleName("rs114205691-?")
             .build();
 
-    private static final RiskAllele RISK_ALLELE_04 = new RiskAlleleBuilder().setId(414L)
-            .setRiskAlleleName("rs9788721-?")
-            .build();
-
-    private static final RiskAllele RISK_ALLELE_05 = new RiskAlleleBuilder().setId(451L)
-            .setRiskAlleleName("rs8042374-?")
-            .build();
-
-
     private static final Locus LOCUS_01 =
             new LocusBuilder().setId(111L)
                     .setDescription("Single variant")
@@ -164,16 +141,7 @@ public class SingleSnpMultiSnpAssociationServiceTest {
     private static final Locus LOCUS_02 =
             new LocusBuilder().setId(111L)
                     .setDescription("2-SNP haplotype")
-                    .build();
-
-    private static final Locus LOCUS_03 =
-            new LocusBuilder().setId(111L)
-                    .setDescription("SNP x SNP interaction")
-                    .build();
-
-    private static final Locus LOCUS_04 =
-            new LocusBuilder().setId(111L)
-                    .setDescription("SNP x SNP interaction")
+                    .setHaplotypeSnpCount(2)
                     .build();
 
     private static final Association BETA_SINGLE_ASSOCIATION =
@@ -214,26 +182,6 @@ public class SingleSnpMultiSnpAssociationServiceTest {
                     .setDescription("this is a test")
                     .build();
 
-    private static final Association OR_INTERACTION_ASSOCIATION =
-            new AssociationBuilder().setId((long) 103)
-                    .setSnpType("novel")
-                    .setMultiSnpHaplotype(false)
-                    .setSnpInteraction(true)
-                    .setSnpApproved(false)
-                    .setPvalueExponent(-8)
-                    .setPvalueMantissa(1)
-                    .setStandardError((float) 6.24)
-                    .setRange("[14.1-38.56]")
-                    .setOrPerCopyNum((float) 1.89)
-                    .setOrPerCopyRecip((float) 0.99)
-                    .setOrPerCopyRecipRange("[1.0-8.0]")
-                    .setPvalueDescription("(ferritin)")
-                    .setRiskFrequency(String.valueOf(0.12))
-                    .setEfoTraits(Arrays.asList(EFO_01, EFO_02))
-                    .setDescription("this is a test")
-                    .build();
-
-
     @BeforeClass
     public static void setUpModelObjects() throws Exception {
 
@@ -250,15 +198,11 @@ public class SingleSnpMultiSnpAssociationServiceTest {
         SNP_01.setRiskAlleles(Collections.singletonList(RISK_ALLELE_01));
         SNP_02.setRiskAlleles(Collections.singletonList(RISK_ALLELE_02));
         SNP_03.setRiskAlleles(Collections.singletonList(RISK_ALLELE_03));
-        SNP_04.setRiskAlleles(Collections.singletonList(RISK_ALLELE_04));
-        SNP_05.setRiskAlleles(Collections.singletonList(RISK_ALLELE_05));
 
         // Set risk allele snp
         RISK_ALLELE_01.setSnp(SNP_01);
         RISK_ALLELE_02.setSnp(SNP_02);
         RISK_ALLELE_03.setSnp(SNP_03);
-        RISK_ALLELE_04.setSnp(SNP_04);
-        RISK_ALLELE_05.setSnp(SNP_05);
 
         // Set risk allele proxy snp
         RISK_ALLELE_01.setProxySnps(Collections.singletonList(PROXY_SNP_01));
@@ -267,38 +211,32 @@ public class SingleSnpMultiSnpAssociationServiceTest {
 
         // Set locus risk allele
         LOCUS_01.setStrongestRiskAlleles(Collections.singletonList(RISK_ALLELE_01));
-        LOCUS_02.setStrongestRiskAlleles(Collections.singletonList(RISK_ALLELE_02));
-        LOCUS_02.setStrongestRiskAlleles(Collections.singletonList(RISK_ALLELE_03));
-        LOCUS_03.setStrongestRiskAlleles(Collections.singletonList(RISK_ALLELE_04));
-        LOCUS_04.setStrongestRiskAlleles(Collections.singletonList(RISK_ALLELE_05));
+        LOCUS_02.setStrongestRiskAlleles(Arrays.asList(RISK_ALLELE_02, RISK_ALLELE_03));
 
         // Set Locus genes
         LOCUS_01.setAuthorReportedGenes(Arrays.asList(GENE_01, GENE_02));
         LOCUS_02.setAuthorReportedGenes(Collections.singletonList(GENE_03));
-        LOCUS_03.setAuthorReportedGenes(Collections.singletonList(GENE_04));
-        LOCUS_04.setAuthorReportedGenes(Collections.singletonList(GENE_05));
 
         // Build association links
         BETA_SINGLE_ASSOCIATION.setLoci(Collections.singletonList(LOCUS_01));
         OR_MULTI_ASSOCIATION.setLoci(Collections.singletonList(LOCUS_02));
-        OR_INTERACTION_ASSOCIATION.setLoci(Arrays.asList(LOCUS_03, LOCUS_04));
     }
 
     @Before
     public void setUp() throws Exception {
-        singleSnpMultiSnpAssociationService = new SingleSnpMultiSnpAssociationService(associationRepository,
-                                                                                      locusRepository,
-                                                                                      genomicContextRepository,
-                                                                                      lociAttributesService);
+        snpAssociationFormService = new SingleSnpMultiSnpAssociationService(associationRepository,
+                                                                            locusRepository,
+                                                                            genomicContextRepository,
+                                                                            lociAttributesService);
     }
 
     @Test
     public void testCreateSingleForm() throws Exception {
-        assertThat(singleSnpMultiSnpAssociationService.createForm(BETA_SINGLE_ASSOCIATION)).isInstanceOf(
+        assertThat(snpAssociationFormService.createForm(BETA_SINGLE_ASSOCIATION)).isInstanceOf(
                 SnpAssociationStandardMultiForm.class);
 
         SnpAssociationStandardMultiForm form =
-                (SnpAssociationStandardMultiForm) singleSnpMultiSnpAssociationService.createForm(BETA_SINGLE_ASSOCIATION);
+                (SnpAssociationStandardMultiForm) snpAssociationFormService.createForm(BETA_SINGLE_ASSOCIATION);
 
         // Check values we would expect in form
         assertThat(form.getAssociationId()).as("Check form ID").isEqualTo(BETA_SINGLE_ASSOCIATION.getId());
@@ -356,5 +294,69 @@ public class SingleSnpMultiSnpAssociationServiceTest {
         }
 
         assertThat(proxyNames).containsOnlyOnce("rs6538678");
+    }
+
+    @Test
+    public void testCreateMultiForm() throws Exception {
+        assertThat(snpAssociationFormService.createForm(OR_MULTI_ASSOCIATION)).isInstanceOf(
+                SnpAssociationStandardMultiForm.class);
+
+        SnpAssociationStandardMultiForm form =
+                (SnpAssociationStandardMultiForm) snpAssociationFormService.createForm(OR_MULTI_ASSOCIATION);
+
+        // Check values we would expect in form
+        assertThat(form.getAssociationId()).as("Check form ID").isEqualTo(OR_MULTI_ASSOCIATION.getId());
+        assertThat(form.getSnpType()).as("Check form SNP TYPE").isEqualTo(OR_MULTI_ASSOCIATION.getSnpType());
+        assertThat(form.getMultiSnpHaplotype()).as("Check form MULTI SNP HAPLOTYPE")
+                .isEqualTo(OR_MULTI_ASSOCIATION.getMultiSnpHaplotype());
+        assertThat(form.getSnpApproved()).as("Check form SNP APPROVED")
+                .isEqualTo(OR_MULTI_ASSOCIATION.getSnpApproved());
+        assertThat(form.getPvalueExponent()).as("Check form PVALUE EXPONENT")
+                .isEqualTo(OR_MULTI_ASSOCIATION.getPvalueExponent());
+        assertThat(form.getPvalueMantissa()).as("Check form PVALUE MANTISSA")
+                .isEqualTo(OR_MULTI_ASSOCIATION.getPvalueMantissa());
+        assertThat(form.getStandardError()).as("Check form STANDARD ERROR")
+                .isEqualTo(OR_MULTI_ASSOCIATION.getStandardError());
+        assertThat(form.getRange()).as("Check form RANGE").isEqualTo(OR_MULTI_ASSOCIATION.getRange());
+        assertThat(form.getPvalueDescription()).as("Check form PVALUE DESCRIPTION")
+                .isEqualTo(OR_MULTI_ASSOCIATION.getPvalueDescription());
+        assertThat(form.getRiskFrequency()).as("Check form RISK FREQUENCY")
+                .isEqualTo(OR_MULTI_ASSOCIATION.getRiskFrequency());
+        assertThat(form.getDescription()).as("Check form DESCRIPTION")
+                .isEqualTo(OR_MULTI_ASSOCIATION.getDescription());
+
+        // Check EFO traits
+        assertThat(form.getEfoTraits()).extracting("id", "trait", "uri")
+                .contains(tuple(988L, "atrophic rhinitis", "http://www.ebi.ac.uk/efo/EFO_0007159"),
+                          tuple(989L, "HeLa", "http://www.ebi.ac.uk/efo/EFO_0001185"));
+
+        // Check null values
+        assertNull(form.getBetaDirection());
+        assertNull(form.getBetaNum());
+        assertNull(form.getBetaUnit());
+
+
+        // Test locus attributes
+        assertThat(form.getMultiSnpHaplotypeDescr()).as("Check form MULTI HAPLOTYPE DESCRIPTION")
+                .isEqualTo("2-SNP haplotype");
+        assertThat(form.getMultiSnpHaplotypeNum()).as("Check form MULTI HAPLOTYPE NUMBER")
+                .isEqualTo(2);
+        assertThat(form.getAuthorReportedGenes()).isInstanceOf(Collection.class);
+        assertThat(form.getAuthorReportedGenes()).containsOnly("ELF1");
+
+        // Test the row values
+        Collection<SnpFormRow> rows = form.getSnpFormRows();
+        assertThat(rows).hasSize(2);
+        assertThat(rows).extracting("proxySnps").isNotEmpty();
+        assertThat(rows).extracting("snp").isNotEmpty();
+        assertThat(rows).extracting("strongestRiskAllele").isNotEmpty();
+        assertThat(rows).extracting("snp").containsExactly("rs9533090", "rs114205691");
+        assertThat(rows).extracting("strongestRiskAllele").containsExactly("rs9533090-?", "rs114205691-?");
+
+        List<String> proxyNames = new ArrayList<String>();
+        for (SnpFormRow row : rows) {
+            proxyNames.addAll(row.getProxySnps());
+        }
+        assertThat(proxyNames).containsExactly("rs7329174", "rs1234567");
     }
 }
