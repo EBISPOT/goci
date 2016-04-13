bf4b0caedc0be399e8a3e3e93b06a2a5ed0517e6
emma-ebi <emma@ebi.ac.uk>
2016-04-13 15:54:22 +0100

    Add method to process headers

--- goci-data-services/goci-association-upload-services/src/main/java/uk/ac/ebi/spot/goci/service/UploadSheetProcessor.java
+++ goci-data-services/goci-association-upload-services/src/main/java/uk/ac/ebi/spot/goci/service/UploadSheetProcessor.java
@@ -1,17 +1,23 @@
 package uk.ac.ebi.spot.goci.service;
 
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import uk.ac.ebi.spot.goci.model.AssociationUploadRow;
 
 import java.util.Collection;
+import java.util.Map;
 
 /**
  * Created by emma on 13/04/2016.
  *
  * @author emma
  *         <p>
- *         Service to read an spreadsheet of associations
+ *         Service to read an spreadsheet of associations, create a map of headers in the file and process all further
+ *         rows
  */
 public interface UploadSheetProcessor {
+
+    Map<Integer, String> createHeaderMap(XSSFRow row);
+
     Collection<AssociationUploadRow> readSheetRows(XSSFSheet sheet);
 }
