cd4e50e0f8c1be09840b5ea3331398e62b60f4af
emma-ebi <emma@ebi.ac.uk>
2016-04-13 09:08:16 +0100

    Move string tidy method to simple utility class

---
+++ goci-data-services/goci-association-upload-services/src/main/java/utils/StringProcessingService.java
@@ -0,0 +1,21 @@
+package utils;
+
+/**
+ * Created by emma on 13/04/2016.
+ *
+ * @author emma
+ *         <p>
+ *         Simple utility class to tidy up user entered strings before saving to database
+ */
+public class StringProcessingService {
+
+    public static String tidy_curator_entered_string(String string) {
+        String newString = string.trim();
+        String newline = System.getProperty("line.separator");
+
+        if (newString.contains(newline)) {
+            newString = newString.replace(newline, "");
+        }
+        return newString;
+    }
+}
