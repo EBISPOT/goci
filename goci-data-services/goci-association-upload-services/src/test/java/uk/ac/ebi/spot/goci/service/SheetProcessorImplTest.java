79872a90bcbfadd4637508ed4cab9ba6b21c19ee
emma-ebi <emma@ebi.ac.uk>
2016-04-14 10:43:22 +0100

    Sheet processor test

--- goci-data-services/goci-association-upload-services/src/test/java/uk/ac/ebi/spot/goci/service/SheetProcessorImplTest.java
+++ goci-data-services/goci-association-upload-services/src/test/java/uk/ac/ebi/spot/goci/service/SheetProcessorImplTest.java
@@ -6,14 +6,15 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import uk.ac.ebi.spot.goci.model.AssociationUploadRow;
 
+import java.util.Collection;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 import static org.assertj.core.data.MapEntry.entry;
-import static org.mockito.Mockito.verifyZeroInteractions;
 
 /**
  * Created by emma on 13/04/2016.
@@ -29,13 +30,9 @@ public class SheetProcessorImplTest {
 
     private XSSFSheet sheet;
 
-    @Mock
-    private AssociationCalculationService associationCalculationService;
-
     @Before
     public void setUp() throws Exception {
-        uploadSheetProcessor = new SheetProcessorImpl(associationCalculationService);
-
+        uploadSheetProcessor = new SheetProcessorImpl();
 
         // Create spreadsheet for testing
         XSSFWorkbook workbook = new XSSFWorkbook();
@@ -57,39 +54,70 @@ public class SheetProcessorImplTest {
         row1.createCell(11).setCellValue("OR/beta SE");
         row1.createCell(12).setCellValue("OR/beta range");
 
-
         //Create second row
         XSSFRow row2 = sheet.createRow(1);
-        row2.createCell(0).setCellValue("TEST, SFRP1"); // Gene(s)
+        row2.createCell(0).setCellValue("rs123"); // SNP
         row2.createCell(1).setCellValue("rs123-?"); // Strongest SNP-Risk Allele
-        row2.createCell(2).setCellValue("rs123"); // SNP
-        row2.createCell(3).setCellValue("rs222, rs111, rs333"); // Proxy SNP
-        row2.createCell(4); // Independent SNP risk allele frequency in controls (ONLY REQUIRED FOR SNP INTERACTION ASSOCIATIONS)
-        row2.createCell(5)
-                .setCellValue("0.6"); // Risk element (allele,haplotype or SNPxSNP interaction) frequency in controls
-        row2.createCell(6).setCellValue(1); // P-value mantissa
-        row2.createCell(7).setCellValue(-9); // P-value exponent
-        row2.createCell(8); // P-value description
-        row2.createCell(9).setCellValue("OR"); // Effect type
-        row2.createCell(10).setCellValue(2.48); //  OR
-        row2.createCell(11); //  OR reciprocal
-        row2.createCell(12); // Beta
-        row2.createCell(13); // Beta unit
-        row2.createCell(14); // Beta direction
-        row2.createCell(15); // Range
-        row2.createCell(16); // OR reciprocal range
-        row2.createCell(17).setCellValue(0.01); // Standard Error
-        row2.createCell(18); // OR/Beta description
-        row2.createCell(19).setCellValue("N"); // Multi-SNP Haplotype?
-        row2.createCell(20).setCellValue("N"); // SNP:SNP interaction?
-        row2.createCell(21); // SNP Status  (ONLY REQUIRED FOR SNP INTERACTION ASSOCIATIONS)
-        row2.createCell(22).setCellValue("novel"); // SNP type (novel/known)
-        row2.createCell(23).setCellValue("EFO_0003894"); // SNP type (novel/known)
-
+        row2.createCell(2).setCellValue("rs222-T"); // Other Allele
+        row2.createCell(3).setCellValue("0.6"); // Effect allele frequency in controls
+        row2.createCell(4).setCellValue("TEST, SFRP1"); // Gene(s)
+        row2.createCell(5).setCellValue(1); // P-value mantissa
+        row2.createCell(6).setCellValue(-9); // P-value exponent
+        row2.createCell(7).setCellValue(2.48); //  OR
+        row2.createCell(8); // Beta
+        row2.createCell(9); // Beta unit
+        row2.createCell(10); // Beta direction
+        row2.createCell(11).setCellValue(0.01); // Standard Error
+        row2.createCell(12).setCellValue("[1.22-1.43]"); // Range
     }
 
     @Test
     public void testReadSheetRows() throws Exception {
+
+        Collection<AssociationUploadRow> rows = uploadSheetProcessor.readSheetRows(sheet);
+        assertThat(rows).isInstanceOf(Collection.class);
+        assertThat(rows).hasOnlyElementsOfType(AssociationUploadRow.class);
+        assertThat(rows).hasSize(1);
+
+        // Check first row
+        assertThat(rows).extracting("rowNumber",
+                                    "authorReportedGene",
+                                    "strongestAllele",
+                                    "snp",
+                                    "proxy",
+                                    "riskFrequency",
+                                    "associationRiskFrequency",
+                                    "pvalueMantissa",
+                                    "pvalueExponent",
+                                    "pvalueDescription",
+                                    "effectType",
+                                    "orPerCopyNum",
+                                    "orPerCopyRecip",
+                                    "betaNum",
+                                    "betaUnit",
+                                    "betaDirection",
+                                    "range",
+                                    "orPerCopyRecipRange",
+                                    "standardError",
+                                    "description",
+                                    "multiSnpHaplotype",
+                                    "snpInteraction",
+                                    "snpStatus",
+                                    "snpType",
+                                    "efoTrait")
+                .containsExactly(tuple(1,
+                                       "TEST, SFRP1",
+                                       "rs123-?",
+                                       "rs123",
+                                       null,
+                                       null,
+                                       "0.6",
+                                       1,
+                                       -9,
+                                       null,
+                                       null, (float) 2.48, null, null, null, null, "[1.22-1.43]",
+                                       null, (float) 0.01, null, null, null, null, null, null));
+
 
     }
 
@@ -97,7 +125,6 @@ public class SheetProcessorImplTest {
     public void testCreateHeaderMap() throws Exception {
 
         Map<Integer, String> headerRowMap = uploadSheetProcessor.createHeaderMap(sheet.getRow(0));
-        verifyZeroInteractions(associationCalculationService);
         assertThat(headerRowMap).hasSize(13);
         assertThat(headerRowMap).contains(entry(0, "SNP ID"),
                                           entry(1, "effect allele"),
