c26ea3d94215dcb0a5010f124ba7f52edef3711b
emma-ebi <emma@ebi.ac.uk>
2016-04-14 16:21:05 +0100

    Updated poms to reflect application to upload association files

---
+++ goci-tools/goci-association-file-upload/src/main/java/uk/ac/ebi/spot/goci/UploadApplication.java
@@ -0,0 +1,12 @@
+package uk.ac.ebi.spot.goci;
+
+/**
+ * Created by emma on 14/04/2016.
+ * @author emma
+ *
+ * Application that can be used to upload a spreadsheet and validate it
+ *
+ */
+@SpringBootApplication
+public class UploadApplication {
+}
