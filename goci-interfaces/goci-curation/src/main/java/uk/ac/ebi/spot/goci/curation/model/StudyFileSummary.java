0b9c49410e6f0b4d360bac02afeadd7c1bfaecd2
emma-ebi <emma@ebi.ac.uk>
2016-04-19 10:45:14 +0100

    Add file link as attribute

--- goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/model/StudyFileSummary.java
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/model/StudyFileSummary.java
@@ -11,8 +11,11 @@ public class StudyFileSummary {
 
     private String fileName;
 
-    public StudyFileSummary(String fileName) {
+    private String link;
+    
+    public StudyFileSummary(String fileName, String link) {
         this.fileName = fileName;
+        this.link = link;
     }
 
     public String getFileName() {
@@ -21,5 +24,13 @@ public class StudyFileSummary {
 
     public void setFileName(String fileName) {
         this.fileName = fileName;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
     }
 }
