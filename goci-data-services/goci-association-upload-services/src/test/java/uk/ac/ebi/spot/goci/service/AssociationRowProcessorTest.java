fd9d62e2efca8230666a2d8f4e68e8aa7ed4cf66
emma-ebi <emma@ebi.ac.uk>
2016-04-21 16:16:38 +0100

    Add test for row with no locus attributes

--- goci-data-services/goci-association-upload-services/src/test/java/uk/ac/ebi/spot/goci/service/AssociationRowProcessorTest.java
+++ goci-data-services/goci-association-upload-services/src/test/java/uk/ac/ebi/spot/goci/service/AssociationRowProcessorTest.java
@@ -22,7 +22,9 @@ import java.util.Collections;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 /**
@@ -55,7 +57,6 @@ public class AssociationRowProcessorTest {
             .setStandardError((float) 0.6)
             .build();
 
-
     private static final Gene GENE_01 = new GeneBuilder().setGeneName("SFRP1").build();
 
     private static final Gene GENE_02 = new GeneBuilder().setGeneName("ELF1").build();
@@ -64,6 +65,19 @@ public class AssociationRowProcessorTest {
             new SingleNucleotidePolymorphismBuilder().setRsId("rs123").build();
 
     private static final RiskAllele RA_01 = new RiskAlleleBuilder().setRiskAlleleName("rs123-?").build();
+
+    private static final AssociationUploadRow ROW_2 = new AssociationUploadRowBuilder().setRowNumber(1)
+            .setSnp("rs456")
+            .setAssociationRiskFrequency("0.52")
+            .setPvalueMantissa(2)
+            .setPvalueExponent(-7)
+            .setOrPerCopyRecip((float) 0.32)
+            .setOrPerCopyRecipRange("[1.0-2.0]")
+            .setStandardError((float) 0.6)
+            .build();
+
+    private static final SingleNucleotidePolymorphism SNP_02 =
+            new SingleNucleotidePolymorphismBuilder().setRsId("rs456").build();
 
     @Before
     public void setUp() throws Exception {
@@ -130,6 +144,64 @@ public class AssociationRowProcessorTest {
                                  (float) 1.22,
                                  null,
                                  null,
+                                 null,
+                                 null,
+                                 null,
+                                 null,
+                                 null,
+                                 null,
+                                 null, null);
+    }
+
+    @Test
+    public void testCreateAssociationFromUploadRowWithNoLocusFields() throws Exception {
+
+        // Stubbing mock object behaviour
+        when(associationAttributeService.createSnp(ROW_2.getSnp())).thenReturn(SNP_02);
+
+        Association association = associationRowProcessor.createAssociationFromUploadRow(ROW_2);
+
+        verify(associationCalculationService, times(1)).reverseCI(ROW_2.getOrPerCopyRecipRange());
+        verify(associationCalculationService, never()).setRange(ROW_2.getStandardError(), 0.00);
+        verifyZeroInteractions(associationAttributeService);
+
+        assertThat(association).extracting("id", "riskFrequency",
+                                           "pvalueDescription",
+                                           "pvalueMantissa",
+                                           "pvalueExponent",
+                                           "multiSnpHaplotype",
+                                           "snpInteraction",
+                                           "snpApproved",
+                                           "snpType",
+                                           "standardError",
+                                           "range",
+                                           "description",
+                                           "orPerCopyNum",
+                                           "orPerCopyRecip",
+                                           "orPerCopyRecipRange",
+                                           "betaNum",
+                                           "betaUnit",
+                                           "betaDirection",
+                                           "study",
+                                           "associationReport",
+                                           "lastMappingDate",
+                                           "lastMappingPerformedBy",
+                                           "lastUpdateDate")
+                .containsExactly(null,
+                                 "0.52",
+                                 null,
+                                 2,
+                                 -7,
+                                 false,
+                                 false,
+                                 false,
+                                 null,
+                                 (float) 0.6,
+                                 null,
+                                 null,
+                                 (float) 3.125,
+                                 (float) 0.32,
+                                 "[1.0-2.0]",
                                  null,
                                  null,
                                  null,
