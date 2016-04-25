950167e331aa9cb73794a2849e031637e4e88af5
emma-ebi <emma@ebi.ac.uk>
2016-04-25 14:14:03 +0100

    New component to run snp checks

---
+++ goci-data-services/goci-data-validation-services/src/main/java/uk/ac/ebi/spot/goci/component/SnpValidationChecks.java
@@ -0,0 +1,70 @@
+package uk.ac.ebi.spot.goci.component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by Emma on 22/04/16.
+ *
+ * @author emma
+ *         <p>
+ *         Checks a SNP identifier is valid using standard Spring mechanism to consume a RESTful service
+ */
+@Service
+public class SnpValidationChecks {
+
+    @NotNull @Value("${mapping.snp_lookup_endpoint}")
+    private String endpoint;
+
+    @NotNull @Value("${ensembl.server}")
+    private String server;
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    protected Logger getLog() {
+        return log;
+    }
+
+    /**
+     * Check gene name returns a response
+     *
+     * @param snp Snp identifier to check
+     * @return Error message
+     */
+    public String checkSnpIdentifierIsValid(String snp) {
+
+        String error = null;
+        String url = getServer().concat(getEndpoint()).concat(snp);
+        getLog().info("Checking url: " + url);
+
+        // Create a new RestTemplate instance
+        RestTemplate restTemplate = new RestTemplate();
+
+        // Add the Jackson message converter
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+
+        try {
+            String response = restTemplate.getForObject(url, String.class);
+        }
+        // The query returns a 400 error if response returns an error
+        catch (Exception e) {
+            error = "SNP identifier ".concat(snp).concat(" is not valid");
+            getLog().error("Checking SNP identifier failed", e);
+        }
+        return error;
+    }
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public String getServer() {
+        return server;
+    }
+}
