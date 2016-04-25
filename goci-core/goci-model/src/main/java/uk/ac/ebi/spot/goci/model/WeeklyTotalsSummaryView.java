1a7470b88e5950deb68daf24e807e8ccb822662b
Dani Welter <dwelter@ebi.ac.uk>
2016-04-25 17:20:56 +0100

    Full first pass done

--- goci-core/goci-model/src/main/java/uk/ac/ebi/spot/goci/model/WeeklyTotalsSummaryView.java
+++ goci-core/goci-model/src/main/java/uk/ac/ebi/spot/goci/model/WeeklyTotalsSummaryView.java
@@ -1,46 +1,59 @@
 package uk.ac.ebi.spot.goci.model;
 
 import javax.persistence.Entity;
+import java.sql.Date;
 
 /**
  * Created by dwelter on 15/04/16.
  */
 
 @Entity
-public class WeeklyTotalsSummaryView extends TotalsSummaryView{
+public class WeeklyTotalsSummaryView{// extends TotalsSummaryView{
 
-    private Integer week;
+    private Date week;
 
-    private Integer weeklyTotal;
+    private Integer weeklyStudies;
+
+    private Integer weeklyEntries;
 
     // JPA no-args constructor
     public WeeklyTotalsSummaryView() {
     }
 
-    public WeeklyTotalsSummaryView(Integer year,
-                                    String curator,
-                                    Integer curatorTotal,
-                                    String curationStatus,
-                                    Integer month,
-                                    Integer weeklyTotal) {
-        super(year, curator, curatorTotal, curationStatus);
+    public WeeklyTotalsSummaryView(Date week,
+//                                    String curator,
+//                                    Integer curatorTotal,
+//                                    String curationStatus,
+//                                    Integer month,
+                                    Integer weeklyStudies,
+                                    Integer weeklyEntries) {
+//        super(year, curator, curatorTotal, curationStatus);
         this.week = week;
-        this.weeklyTotal = weeklyTotal;
+        this.weeklyStudies = weeklyStudies;
+        this.weeklyEntries = weeklyEntries;
     }
 
-    public Integer getweek() {
+    public Date getweek() {
         return week;
     }
 
-    public void setweek(Integer week) {
+    public void setweek(Date week) {
         this.week = week;
     }
 
-    public Integer getWeeklyTotal() {
-        return weeklyTotal;
+    public Integer getWeeklyStudies() {
+        return weeklyStudies;
     }
 
-    public void setWeeklyTotal(Integer weeklyTotal) {
-        this.weeklyTotal = weeklyTotal;
+    public void setWeeklyStudies(Integer weeklyStudies) {
+        this.weeklyStudies = weeklyStudies;
+    }
+
+    public Integer getWeeklyEntries() {
+        return weeklyEntries;
+    }
+
+    public void setWeeklyEntries(Integer weeklyEntries) {
+        this.weeklyEntries = weeklyEntries;
     }
 }
--- goci-core/goci-repository/src/main/java/uk/ac/ebi/spot/goci/repository/WeeklyTotalsSummaryViewRepository.java
+++ goci-core/goci-repository/src/main/java/uk/ac/ebi/spot/goci/repository/WeeklyTotalsSummaryViewRepository.java
@@ -1,8 +1,11 @@
 package uk.ac.ebi.spot.goci.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import uk.ac.ebi.spot.goci.model.WeeklyTotalsSummaryView;
+
+import java.util.List;
 
 /**
  * Created by dwelter on 15/04/16.
@@ -10,17 +13,7 @@ import uk.ac.ebi.spot.goci.model.WeeklyTotalsSummaryView;
 @RepositoryRestResource
 public interface WeeklyTotalsSummaryViewRepository extends JpaRepository<WeeklyTotalsSummaryView, Long> {
 
-    //query to get studies grouped by start day of the week
-//    select trunc(study_added_date, 'D'), count(*) from housekeeping
-////    group by trunc(study_added_date, 'D')
-//
-//
-//    select trunc(h.study_added_date, 'D'), count(*) as entries, count(distinct s.pubmed_id) as studies
-//    from housekeeping h
-//    join study s on s.housekeeping_id = h.id
-//    where h.study_added_date is not null
-//    group by trunc(h.study_added_date, 'D')
-//    order by trunc(h.study_added_date, 'D') desc
+    @Query("select week, studies, entries from WeeklyTotalsSummaryView where rownum < 9") List<WeeklyTotalsSummaryView> getLastEightWeeks();
 
 
 
