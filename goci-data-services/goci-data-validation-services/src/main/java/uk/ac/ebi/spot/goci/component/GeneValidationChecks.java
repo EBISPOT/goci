8646043e92de4df28ba8613f5573eb895e656f00
emma-ebi <emma@ebi.ac.uk>
2016-04-25 10:58:05 +0100

    New component to run gene checks

--- goci-data-services/goci-data-validation-services/src/main/java/uk/ac/ebi/spot/goci/component/GeneValidationChecks.java
+++ goci-data-services/goci-data-validation-services/src/main/java/uk/ac/ebi/spot/goci/component/GeneValidationChecks.java
@@ -2,27 +2,29 @@ package uk.ac.ebi.spot.goci.component;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import uk.ac.ebi.spot.goci.model.GeneLookupJson;
 
+import javax.validation.constraints.NotNull;
+
 /**
- * Created by Laurent on 28/09/15.
+ * Created by Emma on 22/04/16.
  *
- * @author Laurent
+ * @author emma
  *         <p>
- *         Class getting the dbSNP version from the Ensembl REST API
- *         <p>
- *         Refactored by Emma to use standard Spring mechanism to consume a RESTful service
+ *         Checks a gene symbol is valid using standard Spring mechanism to consume a RESTful service
  */
 @Service
 public class GeneValidationChecks {
 
-/*    @NotNull @Value("${mapping.gene_lookup_endpoint}")
+    @NotNull @Value("${mapping.gene_lookup_endpoint}")
     private String endpoint;
 
     @NotNull @Value("${ensembl.server}")
-    private String server;*/
+    private String server;
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
@@ -33,39 +35,42 @@ public class GeneValidationChecks {
     /**
      * Check gene name returns a response
      *
-     * @return the dbSNP version
+     * @param gene Gene name to check
+     * @return Error message
      */
     public String checkGeneSymbolIsValid(String gene) {
 
         String error = null;
+        String url = getServer().concat(getEndpoint()).concat(gene);
+        getLog().info("Checking gene: " + url);
 
+        // Create a new RestTemplate instance
         RestTemplate restTemplate = new RestTemplate();
-        String url = "http://rest.ensembl.org/lookup/symbol/homo_sapiens/SFRP1?content-type=application/json";
+
+        // Add the Jackson message converter
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
         GeneLookupJson geneLookupJson = new GeneLookupJson();
 
         try {
             geneLookupJson = restTemplate.getForObject(url, GeneLookupJson.class);
-            String objectType = geneLookupJson.getObject_type();
-            getLog().info("Checking gene: " + url);
 
-            if (!objectType.equalsIgnoreCase("gene")) {
-                error = "Gene symbol not valid";
+            if (!geneLookupJson.getObject_type().equalsIgnoreCase("gene")) {
+                error = "Gene symbol is not valid";
             }
         }
-
+        // The query returns a 400 error if response returns an error
         catch (Exception e) {
+            error = "Gene symbol is not valid";
             getLog().error("Checking gene symbol failed", e);
         }
-
         return error;
     }
 
-//    public String getEndpoint() {
-//        return endpoint;
-//    }
-//
-//    public String getServer() {
-//        return server;
-//    }
+    public String getEndpoint() {
+        return endpoint;
+    }
 
+    public String getServer() {
+        return server;
+    }
 }
