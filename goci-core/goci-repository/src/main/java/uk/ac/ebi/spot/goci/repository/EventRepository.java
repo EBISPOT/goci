08bd2aa060fd2403fc15ee0716366084a4e67d21
emma-ebi <emma@ebi.ac.uk>
2016-05-06 15:38:32 +0100

    Repository for new event table

---
+++ goci-core/goci-repository/src/main/java/uk/ac/ebi/spot/goci/repository/EventRepository.java
@@ -0,0 +1,18 @@
+package uk.ac.ebi.spot.goci.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import uk.ac.ebi.spot.goci.model.Event;
+
+/**
+ * Created by emma on 06/05/16.
+ *
+ * @author emma
+ *         <p>
+ *         Repository accessing Evnet entity object
+ */
+@RepositoryRestResource
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+}
+
