d9e70e6e70c624ea17b26d140536c06093a5a081
emma-ebi <emma@ebi.ac.uk>
2016-05-09 09:20:59 +0100

    Add method to find by user id

--- goci-core/goci-repository/src/main/java/uk/ac/ebi/spot/goci/repository/EventRepository.java
+++ goci-core/goci-repository/src/main/java/uk/ac/ebi/spot/goci/repository/EventRepository.java
@@ -4,15 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import uk.ac.ebi.spot.goci.model.Event;
 
+import java.util.List;
+
 /**
  * Created by emma on 06/05/16.
  *
  * @author emma
  *         <p>
- *         Repository accessing Evnet entity object
+ *         Repository accessing Event entity object
  */
 @RepositoryRestResource
 public interface EventRepository extends JpaRepository<Event, Long> {
-
+    List<Event> findByUserId(Long id);
 }
 
