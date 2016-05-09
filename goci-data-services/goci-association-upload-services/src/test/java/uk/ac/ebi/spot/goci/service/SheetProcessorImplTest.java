e9debfe65671580f58cac870a06469d66972ff69
emma-ebi <emma@ebi.ac.uk>
2016-05-09 14:35:00 +0100

    Update to test to reflect change in column headings

--- goci-data-services/goci-association-upload-services/src/test/java/uk/ac/ebi/spot/goci/service/SheetProcessorImplTest.java
+++ goci-data-services/goci-association-upload-services/src/test/java/uk/ac/ebi/spot/goci/service/SheetProcessorImplTest.java
@@ -6,15 +6,18 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import uk.ac.ebi.spot.goci.model.AssociationUploadRow;
+import uk.ac.ebi.spot.goci.utils.TranslateUploadHeaders;
+import uk.ac.ebi.spot.goci.utils.UploadFileHeader;
 
 import java.util.Collection;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
-import static org.assertj.core.data.MapEntry.entry;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by emma on 13/04/2016.
@@ -30,9 +33,12 @@ public class SheetProcessorImplTest {
 
     private XSSFSheet sheet;
 
+    @Mock
+    private TranslateUploadHeaders translateUploadHeaders;
+
     @Before
     public void setUp() throws Exception {
-        uploadSheetProcessor = new SheetProcessorImpl();
+        uploadSheetProcessor = new SheetProcessorImpl(translateUploadHeaders);
 
         // Create spreadsheet for testing
         XSSFWorkbook workbook = new XSSFWorkbook();
@@ -40,35 +46,64 @@ public class SheetProcessorImplTest {
 
         // Create header row
         XSSFRow row1 = sheet.createRow(0);
-        row1.createCell(0).setCellValue("SNP ID");
-        row1.createCell(1).setCellValue("effect allele");
-        row1.createCell(2).setCellValue("other allele");
-        row1.createCell(3).setCellValue("effect allele frequency in controls");
-        row1.createCell(4).setCellValue("gene");
-        row1.createCell(5).setCellValue("p-value mantissa");
-        row1.createCell(6).setCellValue("p-value exponent");
-        row1.createCell(7).setCellValue("OR");
-        row1.createCell(8).setCellValue("beta");
-        row1.createCell(9).setCellValue("beta unit");
-        row1.createCell(10).setCellValue("beta direction");
-        row1.createCell(11).setCellValue("OR/beta SE");
-        row1.createCell(12).setCellValue("OR/beta range");
+        row1.createCell(0).setCellValue("SNP ID (ideally rsID) (see below) (mandatory)");
+        row1.createCell(1).setCellValue("Chr(Optional)");
+        row1.createCell(2).setCellValue("Bp(Optional)");
+        row1.createCell(3).setCellValue("Genome Build(Optional)");
+        row1.createCell(4).setCellValue("Effect Allele(Optional)");
+        row1.createCell(5).setCellValue("Other Alleles(Optional)");
+        row1.createCell(6).setCellValue("Effect Allele Frequency in Controls(Optional)");
+        row1.createCell(7).setCellValue("p-value mantissa(Mandatory)");
+        row1.createCell(8).setCellValue("p-value exponent(Mandatory)");
+        row1.createCell(9).setCellValue("OR(Optional)");
+        row1.createCell(10).setCellValue("Beta(Optional)");
+        row1.createCell(11).setCellValue("Beta Unit(mandatory if beta is entered)");
+        row1.createCell(12).setCellValue("Beta Direction(mandatory if beta is entered)");
+        row1.createCell(13).setCellValue("OR/Beta SE(Optional)");
+        row1.createCell(14).setCellValue("OR/Beta Range(95% confidence intervals)(Optional)");
+        row1.createCell(15).setCellValue("Association Description(Optional)");
 
         //Create second row
         XSSFRow row2 = sheet.createRow(1);
         row2.createCell(0).setCellValue("rs123"); // SNP
-        row2.createCell(1).setCellValue("rs123-?"); // Strongest SNP-Risk Allele
-        row2.createCell(2).setCellValue("rs222-T"); // Other Allele
-        row2.createCell(3).setCellValue("0.6"); // Effect allele frequency in controls
-        row2.createCell(4).setCellValue("TEST, SFRP1"); // Gene(s)
-        row2.createCell(5).setCellValue(1); // P-value mantissa
-        row2.createCell(6).setCellValue(-9); // P-value exponent
-        row2.createCell(7).setCellValue(2.48); //  OR
-        row2.createCell(8); // Beta
-        row2.createCell(9); // Beta unit
-        row2.createCell(10); // Beta direction
-        row2.createCell(11).setCellValue(0.01); // Standard Error
-        row2.createCell(12).setCellValue("[1.22-1.43]"); // Range
+        row2.createCell(1);
+        row2.createCell(2);
+        row2.createCell(3);
+        row2.createCell(4).setCellValue("rs123-?"); // Strongest SNP-Risk Allele
+        row2.createCell(5).setCellValue("rs222-T"); // Other Allele
+        row2.createCell(6).setCellValue("0.6"); // Effect allele frequency in controls
+        row2.createCell(7).setCellValue(1); // P-value mantissa
+        row2.createCell(8).setCellValue(-9); // P-value exponent
+        row2.createCell(9).setCellValue(2.48); //  OR
+        row2.createCell(10); // Beta
+        row2.createCell(11); // Beta unit
+        row2.createCell(12); // Beta direction
+        row2.createCell(13).setCellValue(0.01); // Standard Error
+        row2.createCell(14).setCellValue("[1.22-1.43]"); // Range
+        row2.createCell(15).setCellValue("This is a description"); // Description
+
+        when(translateUploadHeaders.translateToEnumValue("SNP ID (ideally rsID) (see below) (mandatory)")).thenReturn(
+                UploadFileHeader.SNP_ID);
+        when(translateUploadHeaders.translateToEnumValue("Chr(Optional)")).thenReturn(UploadFileHeader.CHR);
+        when(translateUploadHeaders.translateToEnumValue("Bp(Optional)")).thenReturn(UploadFileHeader.BP);
+        when(translateUploadHeaders.translateToEnumValue("Genome Build(Optional)")).thenReturn(UploadFileHeader.GENOME_BUILD);
+        when(translateUploadHeaders.translateToEnumValue("Effect Allele(Optional)")).thenReturn(UploadFileHeader.EFFECT_ALLELE);
+        when(translateUploadHeaders.translateToEnumValue("Other Alleles(Optional)")).thenReturn(UploadFileHeader.OTHER_ALLELES);
+        when(translateUploadHeaders.translateToEnumValue("Effect Allele Frequency in Controls(Optional)")).thenReturn(
+                UploadFileHeader.EFFECT_ALLELE_FREQUENCY_IN_CONTROLS);
+        when(translateUploadHeaders.translateToEnumValue("p-value mantissa(Mandatory)")).thenReturn(UploadFileHeader.PVALUE_MANTISSA);
+        when(translateUploadHeaders.translateToEnumValue("p-value exponent(Mandatory)")).thenReturn(UploadFileHeader.PVALUE_EXPONENT);
+        when(translateUploadHeaders.translateToEnumValue("OR(Optional)")).thenReturn(UploadFileHeader.OR);
+        when(translateUploadHeaders.translateToEnumValue("Beta(Optional)")).thenReturn(UploadFileHeader.BETA);
+        when(translateUploadHeaders.translateToEnumValue("Beta Unit(mandatory if beta is entered)")).thenReturn(
+                UploadFileHeader.BETA_UNIT);
+        when(translateUploadHeaders.translateToEnumValue("Beta Direction(mandatory if beta is entered)")).thenReturn(
+                UploadFileHeader.BETA_DIRECTION);
+        when(translateUploadHeaders.translateToEnumValue("OR/Beta SE(Optional)")).thenReturn(UploadFileHeader.STANDARD_ERROR);
+        when(translateUploadHeaders.translateToEnumValue("OR/Beta Range(95% confidence intervals)(Optional)")).thenReturn(
+                UploadFileHeader.RANGE);
+        when(translateUploadHeaders.translateToEnumValue("Association Description(Optional)")).thenReturn(
+                UploadFileHeader.PVALUE_DESCRIPTION);
     }
 
     @Test
@@ -105,7 +140,7 @@ public class SheetProcessorImplTest {
                                     "snpType",
                                     "efoTrait")
                 .containsExactly(tuple(1,
-                                       "TEST, SFRP1",
+                                       null,
                                        "rs123-?",
                                        "rs123",
                                        null,
@@ -113,30 +148,31 @@ public class SheetProcessorImplTest {
                                        "0.6",
                                        1,
                                        -9,
-                                       null, (float) 2.48, null, null, null, null, "[1.22-1.43]",
+                                       "This is a description", (float) 2.48, null, null, null, null, "[1.22-1.43]",
                                        null, (float) 0.01, null, null, null, null, null, null));
-
-
     }
 
     @Test
     public void testCreateHeaderMap() throws Exception {
 
-        Map<Integer, String> headerRowMap = uploadSheetProcessor.createHeaderMap(sheet.getRow(0));
-        assertThat(headerRowMap).hasSize(13);
-        assertThat(headerRowMap).contains(entry(0, "SNP ID"),
-                                          entry(1, "effect allele"),
-                                          entry(2, "other allele"),
-                                          entry(3, "effect allele frequency in controls"),
-                                          entry(4, "gene"),
-                                          entry(5, "p-value mantissa"),
-                                          entry(6, "p-value exponent"),
-                                          entry(7, "OR"),
-                                          entry(8, "beta"),
-                                          entry(9, "beta unit"),
-                                          entry(10, "beta direction"),
-                                          entry(11, "OR/beta SE"),
-                                          entry(12, "OR/beta range")
-        );
+        Map<Integer, UploadFileHeader> headerRowMap = uploadSheetProcessor.createHeaderMap(sheet.getRow(0));
+        assertThat(headerRowMap).isNotEmpty().hasSize(16);
+        assertThat(headerRowMap).containsValues(UploadFileHeader.SNP_ID,
+                                                UploadFileHeader.CHR,
+                                                UploadFileHeader.BP,
+                                                UploadFileHeader.GENOME_BUILD,
+                                                UploadFileHeader.EFFECT_ALLELE,
+                                                UploadFileHeader.OTHER_ALLELES,
+                                                UploadFileHeader.EFFECT_ALLELE_FREQUENCY_IN_CONTROLS,
+                                                UploadFileHeader.PVALUE_MANTISSA,
+                                                UploadFileHeader.PVALUE_EXPONENT,
+                                                UploadFileHeader.OR,
+                                                UploadFileHeader.BETA,
+                                                UploadFileHeader.BETA_UNIT,
+                                                UploadFileHeader.BETA_DIRECTION,
+                                                UploadFileHeader.STANDARD_ERROR,
+                                                UploadFileHeader.RANGE,
+                                                UploadFileHeader.PVALUE_DESCRIPTION);
+
     }
 }
