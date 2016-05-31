032cf893f94d60e4181e2a6dce28ecd525aa820f
emma-ebi <emma@ebi.ac.uk>
2016-05-31 13:38:25 +0100

    Add event variable

--- goci-interfaces/goci-curation/src/test/java/uk/ac/ebi/spot/goci/curation/builder/DeletedStudyBuilder.java
+++ goci-interfaces/goci-curation/src/test/java/uk/ac/ebi/spot/goci/curation/builder/DeletedStudyBuilder.java
@@ -1,6 +1,9 @@
 package uk.ac.ebi.spot.goci.curation.builder;
 
 import uk.ac.ebi.spot.goci.model.DeletedStudy;
+import uk.ac.ebi.spot.goci.model.Event;
+
+import java.util.Collection;
 
 /**
  * Created by emma on 31/05/2016.
@@ -25,6 +28,11 @@ public class DeletedStudyBuilder {
 
     public DeletedStudyBuilder setPubmedId(String pubmedId) {
         deletedStudy.setPubmedId(pubmedId);
+        return this;
+    }
+
+    public DeletedStudyBuilder setEvents(Collection<Event> events) {
+        deletedStudy.setEvents(events);
         return this;
     }
 
