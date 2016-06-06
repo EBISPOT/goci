101abd02e89193973c20ecc3f8fc69829e4365ba
emma-ebi <emma@ebi.ac.uk>
2016-06-06 15:36:45 +0100

    Update test to use mockito runner

--- goci-interfaces/goci-curation/src/test/java/uk/ac/ebi/spot/goci/curation/controller/HomeControllerTest.java
+++ goci-interfaces/goci-curation/src/test/java/uk/ac/ebi/spot/goci/curation/controller/HomeControllerTest.java
@@ -1,53 +1,41 @@
-/*
 package uk.ac.ebi.spot.goci.curation.controller;
-
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import uk.ac.ebi.spot.goci.CurationApplication;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-*/
 /**
  * Created by emma on 14/10/2015.
  *
  * @author emma
  *         <p>
  *         Unit test for HomeController based on http://spring.io/guides/gs/spring-boot/
- *//*
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = CurationApplication.class)
-@WebAppConfiguration
+ */
+@RunWith(MockitoJUnitRunner.class)
 public class HomeControllerTest {
 
-    @Autowired
     private HomeController homeController;
 
-    private MockMvc mvc;
+    private MockMvc mockMvc;
 
     @Before
     public void setUp() throws Exception {
-        mvc = MockMvcBuilders.standaloneSetup(homeController).build();
+        homeController = new HomeController();
+        mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
     }
 
     @Test
     public void getHome() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.TEXT_HTML_VALUE))
+        mockMvc.perform(get("/").accept(MediaType.TEXT_HTML_VALUE))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/login"));
     }
 }
-
-*/
