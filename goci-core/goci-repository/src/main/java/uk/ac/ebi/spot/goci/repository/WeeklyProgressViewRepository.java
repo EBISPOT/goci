65bad89a8726660c22cf80c3906bc182d9247fa5
emma-ebi <emma@ebi.ac.uk>
2016-06-09 11:44:29 +0100

    Removed unused method

--- goci-core/goci-repository/src/main/java/uk/ac/ebi/spot/goci/repository/WeeklyProgressViewRepository.java
+++ goci-core/goci-repository/src/main/java/uk/ac/ebi/spot/goci/repository/WeeklyProgressViewRepository.java
@@ -2,10 +2,7 @@ package uk.ac.ebi.spot.goci.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import uk.ac.ebi.spot.goci.model.EventType;
 import uk.ac.ebi.spot.goci.model.WeeklyProgressView;
-
-import java.util.List;
 
 /**
  * Created by emma on 08/06/2016.
@@ -16,6 +13,4 @@ import java.util.List;
  */
 @RepositoryRestResource
 public interface WeeklyProgressViewRepository extends JpaRepository<WeeklyProgressView, Long> {
-
-    List<WeeklyProgressView> findByEventType(EventType eventType);
 }
