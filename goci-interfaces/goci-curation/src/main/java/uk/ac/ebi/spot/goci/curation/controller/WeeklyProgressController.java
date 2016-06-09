61314afc2191c4e6208b4e73187a29d79848bd22
emma-ebi <emma@ebi.ac.uk>
2016-06-09 12:42:44 +0100

    Controller to display weekly_reports view

--- goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/controller/WeeklyProgressController.java
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/controller/WeeklyProgressController.java
@@ -1,31 +1,40 @@
 package uk.ac.ebi.spot.goci.curation.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import uk.ac.ebi.spot.goci.curation.model.reports.ReportsWeeklyProgressView;
+import uk.ac.ebi.spot.goci.curation.service.reports.WeeklyProgressService;
+
+import java.util.List;
 
 /**
  * Created by emma on 08/06/2016.
  *
  * @author emma
+ *         <p>
+ *         Controller used to handle requests to display weekly curator progress
  */
 
 @Controller
 @RequestMapping("/reports/weekly_progress")
 public class WeeklyProgressController {
 
+    private WeeklyProgressService weeklyProgressService;
+
+    @Autowired
+    public WeeklyProgressController(WeeklyProgressService weeklyProgressService) {
+        this.weeklyProgressService = weeklyProgressService;
+    }
+
     @RequestMapping(produces = MediaType.TEXT_HTML_VALUE, method = RequestMethod.GET)
     public String getWeeklyProgressTable(Model model) {
 
-        //TODO DETERMINE WHAT THIS WILL RETURN
-        processWeeklyView();
-
-
-        // TODO CREATE VIEW
-        return "view";
-
+        List<ReportsWeeklyProgressView> weeklyProgressViews = weeklyProgressService.processWeeklyView();
+        model.addAttribute("weeklyProgressViews", weeklyProgressViews);
+        return "reports_weekly_progress";
     }
-
 }
