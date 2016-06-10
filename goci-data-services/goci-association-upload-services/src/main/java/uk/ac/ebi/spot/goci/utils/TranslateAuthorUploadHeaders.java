9ae145a80d10a1472807da167c21345ce9cbbbf3
emma-ebi <emma@ebi.ac.uk>
2016-06-10 13:53:29 +0100

    Add new service to translate headers in curator uploaded spreadsheet

---
+++ goci-data-services/goci-association-upload-services/src/main/java/uk/ac/ebi/spot/goci/utils/TranslateAuthorUploadHeaders.java
@@ -0,0 +1,75 @@
+package uk.ac.ebi.spot.goci.utils;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by emma on 09/05/2016.
+ *
+ * @author emma
+ *         <p>
+ *         Service class to translate headers in author upload file to enum value
+ */
+@Service
+@Lazy
+public class TranslateAuthorUploadHeaders implements TranslateUploadHeaders {
+
+    @Override public UploadFileHeader translateToEnumValue(String value) {
+
+        value = value.toLowerCase();
+        UploadFileHeader enumValue;
+
+        if (value.equals("snp id(ideally rsid)(mandatory)")) {
+            enumValue = UploadFileHeader.SNP_ID;
+        }
+        else if (value.equals("chr(optional)")) {
+            enumValue = UploadFileHeader.CHR;
+        }
+        else if (value.equals("bp(optional)")) {
+            enumValue = UploadFileHeader.BP;
+        }
+        else if (value.equals("genome build(optional)")) {
+            enumValue = UploadFileHeader.GENOME_BUILD;
+        }
+        else if (value.equals("effect allele(optional)")) {
+            enumValue = UploadFileHeader.EFFECT_ALLELE;
+        }
+        else if (value.equals("other alleles(optional)")) {
+            enumValue = UploadFileHeader.OTHER_ALLELES;
+        }
+        else if (value.startsWith("effect allele frequency")) {
+            enumValue = UploadFileHeader.EFFECT_ALLELE_FREQUENCY_IN_CONTROLS;
+        }
+        else if (value.equals("p-value mantissa(mandatory)")) {
+            enumValue = UploadFileHeader.PVALUE_MANTISSA;
+        }
+        else if (value.equals("p-value exponent(mandatory)")) {
+            enumValue = UploadFileHeader.PVALUE_EXPONENT;
+        }
+        else if (value.equals("or(optional)")) {
+            enumValue = UploadFileHeader.OR;
+        }
+        else if (value.equals("beta(optional)")) {
+            enumValue = UploadFileHeader.BETA;
+        }
+        else if (value.startsWith("beta unit")) {
+            enumValue = UploadFileHeader.BETA_UNIT;
+        }
+        else if (value.startsWith("beta direction")) {
+            enumValue = UploadFileHeader.BETA_DIRECTION;
+        }
+        else if (value.equals("or/beta se(optional)")) {
+            enumValue = UploadFileHeader.STANDARD_ERROR;
+        }
+        else if (value.startsWith("or/beta range")) {
+            enumValue = UploadFileHeader.RANGE;
+        }
+        else if (value.equals("association description(optional)")) {
+            enumValue = UploadFileHeader.PVALUE_DESCRIPTION;
+        }
+        else {
+            enumValue = UploadFileHeader.UNKNOWN;
+        }
+        return enumValue;
+    }
+}
---
+++ goci-data-services/goci-association-upload-services/src/main/java/uk/ac/ebi/spot/goci/utils/TranslateCuratorUploadHeaders.java
@@ -0,0 +1,88 @@
+package uk.ac.ebi.spot.goci.utils;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by emma on 10/06/2016.
+ *
+ * @author emma
+ *         <p>
+ *         Service class to translate headers in curator upload file to enum value
+ */
+@Service
+@Lazy
+public class TranslateCuratorUploadHeaders implements TranslateUploadHeaders {
+    @Override public UploadFileHeader translateToEnumValue(String value) {
+
+        UploadFileHeader enumValue;
+
+        if (value.equals("Gene(s)")) {
+            enumValue = UploadFileHeader.GENES;
+        }
+        else if (value.equals("Strongest SNP-Risk Allele")) {
+            enumValue = UploadFileHeader.EFFECT_ALLELE;
+        }
+        else if (value.equals("SNP")) {
+            enumValue = UploadFileHeader.SNP_ID;
+        }
+        else if (value.equals("Proxy SNP")) {
+            enumValue = UploadFileHeader.PROXY_SNP;
+        }
+        else if (value.equals("P-value mantissa")) {
+            enumValue = UploadFileHeader.PVALUE_MANTISSA;
+        }
+        else if (value.equals("P-value exponent")) {
+            enumValue = UploadFileHeader.PVALUE_EXPONENT;
+        }
+        else if (value.equals("P-value description")) {
+            enumValue = UploadFileHeader.PVALUE_DESCRIPTION;
+        }
+        else if (value.equals("OR")) {
+            enumValue = UploadFileHeader.OR;
+        }
+        else if (value.equals("OR reciprocal")) {
+            enumValue = UploadFileHeader.OR_RECIPROCAL;
+        }
+        else if (value.equals("Beta")) {
+            enumValue = UploadFileHeader.BETA;
+        }
+        else if (value.equals("Beta Unit")) {
+            enumValue = UploadFileHeader.BETA_UNIT;
+        }
+        else if (value.equals("Beta Direction")) {
+            enumValue = UploadFileHeader.BETA_DIRECTION;
+        }
+        else if (value.equals("Range")) {
+            enumValue = UploadFileHeader.RANGE;
+        }
+        else if (value.equals("OR reciprocal range")) {
+            enumValue = UploadFileHeader.OR_RECIPROCAL_RANGE;
+        }
+        else if (value.equals("Standard Error")) {
+            enumValue = UploadFileHeader.STANDARD_ERROR;
+        }
+        else if (value.equals("OR/Beta description")) {
+            enumValue = UploadFileHeader.DESCRIPTION;
+        }
+        else if (value.startsWith("Multi-SNP Haplotype")) {
+            enumValue = UploadFileHeader.MULTI_SNP_HAPLOTYPE;
+        }
+        else if (value.startsWith("SNP:SNP interaction")) {
+            enumValue = UploadFileHeader.SNP_INTERACTION;
+        }
+        else if (value.equals("SNP Status")) {
+            enumValue = UploadFileHeader.SNP_STATUS;
+        }
+        else if (value.startsWith("SNP type")) {
+            enumValue = UploadFileHeader.SNP_TYPE;
+        }
+        else if (value.equals("EFO traits")) {
+            enumValue = UploadFileHeader.EFO_TRAITS;
+        }
+        else {
+            enumValue = UploadFileHeader.UNKNOWN;
+        }
+        return enumValue;
+    }
+}
