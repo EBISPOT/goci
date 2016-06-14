9c2e3b18a8fb5e1a1c7f29d484b84367442ed247
emma-ebi <emma@ebi.ac.uk>
2016-06-14 13:28:02 +0100

    Add exception to method signature, and add additional annotation

--- goci-data-services/goci-association-upload-services/src/main/java/uk/ac/ebi/spot/goci/service/SheetCreationService.java
+++ goci-data-services/goci-association-upload-services/src/main/java/uk/ac/ebi/spot/goci/service/SheetCreationService.java
@@ -1,9 +1,11 @@
 package uk.ac.ebi.spot.goci.service;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.exceptions.InvalidOperationException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -15,10 +17,12 @@ import java.io.IOException;
  *         <p>
  *         Create sheet from file, this is then used to read through each row
  */
+@Lazy
 @Service
 public class SheetCreationService {
 
-    public XSSFSheet createSheet(String fileName) throws InvalidFormatException, IOException {
+    public XSSFSheet createSheet(String fileName) throws InvalidFormatException, IOException,
+                                                         InvalidOperationException {
         // Open file
         OPCPackage pkg = OPCPackage.open(fileName);
         XSSFWorkbook current = new XSSFWorkbook(pkg);
