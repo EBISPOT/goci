2435d983ded2ad7cf612ce085e547f94d6eeae12
emma-ebi <emma@ebi.ac.uk>
2016-06-20 09:53:58 +0100

    New builder used for testing

---
+++ goci-interfaces/goci-curation/src/test/java/uk/ac/ebi/spot/goci/curation/builder/WeeklyProgressViewBuilder.java
@@ -0,0 +1,43 @@
+package uk.ac.ebi.spot.goci.curation.builder;
+
+import uk.ac.ebi.spot.goci.model.EventType;
+import uk.ac.ebi.spot.goci.model.WeeklyProgressView;
+
+import java.util.Date;
+
+/**
+ * Created by emma on 20/06/2016.
+ *
+ * @author emma
+ *         <p>
+ *         Builder for WeeklyProgressView used in testing
+ */
+public class WeeklyProgressViewBuilder {
+
+    private WeeklyProgressView weeklyProgressView = new WeeklyProgressView();
+
+    public WeeklyProgressViewBuilder setId(Long id) {
+        weeklyProgressView.setId(id);
+        return this;
+    }
+
+    public WeeklyProgressViewBuilder setWeekStartDay(Date weekStartDay) {
+        weeklyProgressView.setWeekStartDay(weekStartDay);
+        return this;
+    }
+
+    public WeeklyProgressViewBuilder setEventType(EventType eventType) {
+        weeklyProgressView.setEventType(eventType);
+        return this;
+    }
+
+    public WeeklyProgressViewBuilder setStudyId(Long studyId) {
+        weeklyProgressView.setStudyId(studyId);
+        return this;
+    }
+
+    private WeeklyProgressView build() {
+        return weeklyProgressView;
+    }
+
+}
