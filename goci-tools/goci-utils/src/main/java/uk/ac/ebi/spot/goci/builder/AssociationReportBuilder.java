f096dd70557b2b9765ec3dab7971786969443af2
emma-ebi <emma@ebi.ac.uk>
2016-07-18 10:41:28 +0100

    Added further setter methods

--- goci-tools/goci-utils/src/main/java/uk/ac/ebi/spot/goci/builder/AssociationReportBuilder.java
+++ goci-tools/goci-utils/src/main/java/uk/ac/ebi/spot/goci/builder/AssociationReportBuilder.java
@@ -2,22 +2,35 @@ package uk.ac.ebi.spot.goci.builder;
 
 import uk.ac.ebi.spot.goci.model.AssociationReport;
 
+import java.util.Date;
+
 /**
  * Created by emma on 09/03/2016.
- * @author emma
  *
- * Test for uk.ac.ebi.spot.goci.model.AssociationReport
+ * @author emma
+ *         <p>
+ *         Test for uk.ac.ebi.spot.goci.model.AssociationReport
  */
 public class AssociationReportBuilder {
 
     private AssociationReport associationReport = new AssociationReport();
 
-    public AssociationReportBuilder setSnpGeneOnDiffChr(String error){
+    public AssociationReportBuilder setSnpGeneOnDiffChr(String error) {
         associationReport.setSnpGeneOnDiffChr(error);
         return this;
     }
 
-    public AssociationReport build(){
+    public AssociationReportBuilder setErrorCheckedByCurator(Boolean errorCheckedByCurator) {
+        associationReport.setErrorCheckedByCurator(errorCheckedByCurator);
+        return this;
+    }
+
+    public AssociationReportBuilder setLastUpdateDate(Date lastUpdateDate) {
+        associationReport.setLastUpdateDate(lastUpdateDate);
+        return this;
+    }
+
+    public AssociationReport build() {
         return associationReport;
     }
 }
