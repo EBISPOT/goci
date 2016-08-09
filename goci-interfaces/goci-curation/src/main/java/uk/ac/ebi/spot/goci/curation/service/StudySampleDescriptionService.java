b2834996eeee8d41f2a7dbf9d8d43012df468a1e
emma-ebi <emma@ebi.ac.uk>
2016-08-09 13:16:23 +0100

    Updated imports

--- goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/StudySampleDescriptionService.java
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/StudySampleDescriptionService.java
@@ -5,11 +5,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import uk.ac.ebi.spot.goci.curation.model.InitialSampleDescription;
 import uk.ac.ebi.spot.goci.curation.model.ReplicationSampleDescription;
-import uk.ac.ebi.spot.goci.curation.service.tracking.TrackingOperationService;
 import uk.ac.ebi.spot.goci.model.EventType;
 import uk.ac.ebi.spot.goci.model.SecureUser;
 import uk.ac.ebi.spot.goci.model.Study;
 import uk.ac.ebi.spot.goci.repository.StudyRepository;
+import uk.ac.ebi.spot.goci.service.TrackingOperationService;
 
 import java.util.ArrayList;
 import java.util.List;
