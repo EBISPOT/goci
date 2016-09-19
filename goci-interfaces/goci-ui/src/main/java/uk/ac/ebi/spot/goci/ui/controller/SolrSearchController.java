5891922cd51d1ed936636a9d5f2be559ba1440ef
Tony Burdett <tburdett@ebi.ac.uk>
2016-09-19 14:10:32 +0100

    enabling cors on solr search endpoint

--- goci-interfaces/goci-ui/src/main/java/uk/ac/ebi/spot/goci/ui/controller/SolrSearchController.java
+++ goci-interfaces/goci-ui/src/main/java/uk/ac/ebi/spot/goci/ui/controller/SolrSearchController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -61,6 +62,7 @@ public class SolrSearchController {
     }
 
     @RequestMapping(value = "api/search", produces = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin
     public void doSolrSearch(
             @RequestParam("q") String query,
             @RequestParam(value = "jsonp", required = false, defaultValue = "false") boolean useJsonp,
