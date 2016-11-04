8ae4624990d6f93653da06f0301d9e776db2bcb7
Dani Welter <dwelter@ebi.ac.uk>
2016-11-04 17:14:39 +0000

    First elements of a stand alone REST app

--- goci-interfaces/goci-rest/src/main/java/uk/ac/ebi/spot/goci/RestApplication.java
+++ goci-interfaces/goci-rest/src/main/java/uk/ac/ebi/spot/goci/RestApplication.java
@@ -1,5 +1,9 @@
 package uk.ac.ebi.spot.goci;
 
+/**
+ * Created by dwelter on 04/11/16.
+ */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
@@ -9,8 +13,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
-public class CurationApplication extends SpringBootServletInitializer {
+public class RestApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        SpringApplication.run(CurationApplication.class, args);
+        SpringApplication.run(RestApplication.class, args);
     }
 }
