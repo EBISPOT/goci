28eb15ec35e1b4c057cd656c67b052e962b7ca57
Dani Welter <dwelter@ebi.ac.uk>
2017-01-25 15:33:02 +0000

    Study deletion process works and is ready for deployment

--- goci-tools/goci-public-data-export/src/main/java/uk/ac/ebi/spot/goci/service/DataDeletionService.java
+++ goci-tools/goci-public-data-export/src/main/java/uk/ac/ebi/spot/goci/service/DataDeletionService.java
@@ -4,10 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import uk.ac.ebi.spot.goci.model.Ancestry;
-import uk.ac.ebi.spot.goci.model.Association;
-import uk.ac.ebi.spot.goci.model.Locus;
-import uk.ac.ebi.spot.goci.model.RiskAllele;
 import uk.ac.ebi.spot.goci.model.Study;
 import uk.ac.ebi.spot.goci.repository.AncestryRepository;
 import uk.ac.ebi.spot.goci.repository.AssociationRepository;
@@ -15,8 +11,6 @@ import uk.ac.ebi.spot.goci.repository.LocusRepository;
 import uk.ac.ebi.spot.goci.repository.RiskAlleleRepository;
 import uk.ac.ebi.spot.goci.repository.StudyRepository;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 /**
@@ -62,53 +56,24 @@ public class DataDeletionService {
 
 
         unpublishedStudies.forEach(this::deleteStudy);
+
+        getLog().info("Study deletion process successfully completed");
     }
 
     private void deleteStudy(Study study) {
-        System.out.println("Removing study \t" + study.getAuthor() + "\t (ID:" + study.getId() + ")");
-
-        getLog().debug("Removing study \t" + study.getAuthor() + "\t (ID:" + study.getId() + ")");
-
-        Collection<Association> associations = study.getAssociations();
-
-        getLog().debug("Removing \t" + associations.size() + "\t associations");
-
-        System.out.println("Removing \t" + associations.size() + "\t associations");
-
-        associations.forEach(this::deleteAssociation);
-
-        Collection<Ancestry> ancestries = study.getAncestries();
-
-        getLog().debug("Removing \t" + ancestries.size() + "\t sample descriptions");
-        System.out.println("Removing \t" + ancestries.size() + "\t sample descriptions");
+        System.out.println("Removing study \t" + study.getAuthor() + "\t (ID:" + study.getId() + ") with \t"
+                               + study.getAssociations().size() + "\t association and \t"
+                               + study.getAncestries().size() + "\t ancestries");
+        getLog().debug("Removing study \t" + study.getAuthor() + "\t (ID:" + study.getId() + ") with \t"
+                               + study.getAssociations().size() + "\t association and \t"
+                                + study.getAncestries().size() + "\t ancestries");
 
 
-        ancestries.forEach(
-                ancestry -> ancestryRepository.delete(ancestry)
-        );
+
 
         studyRepository.delete(study);
+
     }
 
 
-    private void deleteAssociation(Association association) {
-
-        Collection<RiskAllele> riskAlleles = new ArrayList<>();
-        for (Locus locus : association.getLoci()) {
-            getLog().debug("Removing locus number\t" + locus.getId());
-            System.out.println("Removing locus number\t" + locus.getId());
-
-            locus.getStrongestRiskAlleles().forEach(riskAlleles::add);
-            locusRepository.delete(locus);
-        }
-        getLog().debug("Removing\t" + riskAlleles.size() + " risk alleles");
-        System.out.println("Removing\t" + riskAlleles.size() + " risk alleles");
-
-        riskAlleles.forEach(riskAllele -> riskAlleleRepository.delete(riskAllele));
-
-        getLog().debug("Removing association \t" + association.getId());
-        System.out.println("Removing association \t" + association.getId());
-
-        associationRepository.delete(association);
-    }
 }
