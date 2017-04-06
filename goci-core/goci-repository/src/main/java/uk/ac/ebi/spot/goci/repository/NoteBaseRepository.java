c0ca34a9b48de7f9c05d90cd7b0e030f52a23894
hxin <xin.he@ebi.ac.uk>
2017-04-06 12:13:38 +0100

    notebaserepo

--- goci-core/goci-repository/src/main/java/uk/ac/ebi/spot/goci/repository/NoteBaseRepository.java
+++ goci-core/goci-repository/src/main/java/uk/ac/ebi/spot/goci/repository/NoteBaseRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 import uk.ac.ebi.spot.goci.model.Note;
 
+import java.util.Collection;
+
 /**
  * Created by cinzia on 28/03/2017.
  */
 @NoRepositoryBean
-public interface  NoteBaseRepository <T extends Note>  extends JpaRepository<Note, Long> {
+public interface  NoteBaseRepository <T extends Note>  extends JpaRepository<T, Long> {
+        public Collection<T> findByStudyId(Long studyId);
+        public Collection<T> findByGenericId(Long genericId);
 }
