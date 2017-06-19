7b90f6022c377d6f0fae9ed7f2b524e8eeaed362
Dani Welter <dwelter@ebi.ac.uk>
2017-06-19 11:21:09 +0100

    Replaced single space replacement expression with a regex to catch all spaces

--- goci-interfaces/goci-ui/src/main/resources/static/js/fullpvalueset.js
+++ goci-interfaces/goci-ui/src/main/resources/static/js/fullpvalueset.js
@@ -85,7 +85,7 @@ function processStudyDoc(study, table) {
 
     row.append($("<td>").html(study.associationCount));
 
-    var a = (study.author_s).replace(" ","");
+    var a = (study.author_s).replace(/\s/g,"");
     var dir = a.concat("_").concat(study.pubmedId)
 
     var ftplink = "<a href='ftp://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/".concat(dir).concat("' target='_blank'>Click for summary statistics</a>");
