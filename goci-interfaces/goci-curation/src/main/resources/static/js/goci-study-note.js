a4003828ac8e7c36ee410d018e03b1aa0b2749a2
hxin <xin.he@ebi.ac.uk>
2017-07-05 15:02:58 +0100

    add confirmation when leaving the page in edit mode.

--- goci-interfaces/goci-curation/src/main/resources/static/js/goci-study-note.js
+++ goci-interfaces/goci-curation/src/main/resources/static/js/goci-study-note.js
@@ -12,3 +12,23 @@ $(document).ready(function() {
 
     $('[data-toggle="confirmation"]').confirmation({ btnOkLabel: "&nbsp;Yes", btnCancelLabel: "&nbsp;No" });
 });
+
+
+var formSubmitting = false;
+var setFormSubmitting = function() { formSubmitting = true; };
+var isEditing = ()=>{
+    return $('#editing_flag').prop('value')=="true";
+}
+window.onload = function() {
+    window.addEventListener("beforeunload", function (e) {
+        if (formSubmitting || !isEditing()) {
+            return undefined;
+        }
+
+        var confirmationMessage = 'It looks like you have been editing a note. '
+                + 'If you leave before saving, your changes will be lost.';
+
+        (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+        return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+    });
+};
