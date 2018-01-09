38140dbbbec93a5b82980870a1c2f7831030f388
Cinzia Malangone <cinzia@ebi.ac.uk>
2018-01-09 16:05:24 +0000

    disable form

--- goci-interfaces/goci-curation/src/main/resources/static/js/goci-study-duplication.js
+++ goci-interfaces/goci-curation/src/main/resources/static/js/goci-study-duplication.js
@@ -1,8 +1,39 @@
 $(document).ready(function() {
 
+    function disableForm() {
+        var inputs= $('#study-duplications-form-container').find('.studyItemFormInput');
+        inputs.each(function(j, input) {
+            $(input).prop('disabled', true);
+        });
+    
+        var removes= $('#study-duplications-form-container').find('.remove-study-duplications');
+        removes.each(function(j, remove) {
+            $(remove).attr("disabled", "disabled");
+        });
+        $('#multi-add-study-duplications').attr("disabled", "disabled");
+        $('#multi-add-study-duplications-button').attr("disabled", "disabled");
+        $('#duplicateStudy').attr("disabled", "disabled");
+        
+    }
+    
+    function enableForm() {
+        var inputs= $('#study-duplications-form-container').find('.studyItemFormInput');
+        inputs.each(function(j, input) {
+            $(input).prop('disabled', false);
+        });
+    
+        var removes= $('#study-duplications-form-container').find('.remove-study-duplications');
+        removes.each(function(j, remove) {
+            $(remove).removeAttr("disabled");
+        });
+        $('#multi-add-study-duplications').removeAttr("disabled");
+        $('#multi-add-study-duplications-button').removeAttr("disabled");
+        $('#duplicateStudy').removeAttr("disabled");
+    }
+    
     $("#duplicateStudy").click(function () {
 
-        console.log(contextPath);
+        //console.log(contextPath);
         var listTags = {};
         var studyId = $('#studyId').val();
         var action = contextPath + "studies/"+studyId+"/duplicate";
@@ -22,15 +53,17 @@ $(document).ready(function() {
                 type: "POST",
                 url: action,
                 data: dataJSON,
+                beforeSend: function() {
+                    disableForm();
+                },
                 success: function (data, textStatus, jqXHR) {
                     if ("failed" in data) {
                        alert(data["failed"]);
+                       enableForm();
                     }
                     else {
                         window.location.href = contextPath+data.success;
                     }
-                    
-                    
                 },
                 error: function (XMLHttpRequest, textStatus, errorThrown) {
                     alert("Something went wrong. Please, contact the helpdesk.");
