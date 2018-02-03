ff1841feb45277de50b0631707eb4b587b1eb5f3
Cinzia Malangone <cinzia@ebi.ac.uk>
2018-02-03 18:15:09 +0000

    annotations

--- goci-core/goci-repository/src/main/java/uk/ac/ebi/spot/goci/repository/AuthorRepository.java
+++ goci-core/goci-repository/src/main/java/uk/ac/ebi/spot/goci/repository/AuthorRepository.java
@@ -1,11 +1,12 @@
 package uk.ac.ebi.spot.goci.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 import uk.ac.ebi.spot.goci.model.Author;
 
-@Repository
+@RepositoryRestResource
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
     @RestResource(exported = false)
--- goci-core/goci-repository/src/main/java/uk/ac/ebi/spot/goci/repository/PublicationAuthorsRepository.java
+++ goci-core/goci-repository/src/main/java/uk/ac/ebi/spot/goci/repository/PublicationAuthorsRepository.java
@@ -2,11 +2,13 @@ package uk.ac.ebi.spot.goci.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import uk.ac.ebi.spot.goci.model.PublicationAuthors;
 
 @RepositoryRestResource
 public interface PublicationAuthorsRepository extends JpaRepository<PublicationAuthors, Long> {
 
+    @RestResource(exported = false)
     PublicationAuthors findByAuthorIdAndPublicationId(Long author_id, Long publication_id);
 
 }
