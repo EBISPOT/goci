3fbf6bd8856b8e62f9341d56d7e82959e61904c7
jstewart <jstewart@ebi.ac.uk>
2019-10-11 14:12:53 +0100

    redo solr indexer threading

--- goci-tools/goci-solr-indexer/src/main/java/uk/ac/ebi/spot/goci/SolrIndexerApplication.java
+++ goci-tools/goci-solr-indexer/src/main/java/uk/ac/ebi/spot/goci/SolrIndexerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
