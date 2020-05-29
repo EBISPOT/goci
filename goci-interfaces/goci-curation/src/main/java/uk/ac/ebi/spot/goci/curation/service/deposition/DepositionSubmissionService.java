bbf5758499820e0002471fe8c8f0643c8e44e4eb
jstewart <jstewart@ebi.ac.uk>
2020-05-29 11:44:41 +0100

    revert th:field changes

--- goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/deposition/DepositionSubmissionService.java
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/service/deposition/DepositionSubmissionService.java
@@ -293,50 +293,52 @@ public class DepositionSubmissionService {
     }
 
     public Submission.SubmissionType getSubmissionType(DepositionSubmission submission){
-        if(submission.getBodyOfWork() != null) {
+        if(submission.getBodyOfWork() != null && submission.getPublication() == null) {
             return Submission.SubmissionType.PRE_PUBLISHED;
         }
         else if(submission.getBodyOfWork() == null && submission.getPublication() == null) {
             return Submission.SubmissionType.UNKNOWN;
         }
-        String publicationStatus = submission.getPublication().getStatus();
-        boolean hasSumStats = false;
-        boolean hasMetadata = false;
-        boolean hasAssociations = false;
-        if(publicationStatus.equals("UNDER_SUBMISSION")){
-            hasMetadata = true;
-        }
-        else if(publicationStatus.equals("UNDER_SUMMARY_STATS_SUBMISSION")){
-            hasSumStats = true;
-        }
-        if(submission.getStudies() != null) {
-            for (DepositionStudyDto studyDto : submission.getStudies()) {
-                if(studyDto.getSummaryStatisticsFile() != null && !studyDto.getSummaryStatisticsFile().equals("") && !studyDto.getSummaryStatisticsFile().equals("NR")){
-                    hasSumStats = true;
+        else if(submission.getPublication() != null) {
+            String publicationStatus = submission.getPublication().getStatus();
+            boolean hasSumStats = false;
+            boolean hasMetadata = false;
+            boolean hasAssociations = false;
+            if (publicationStatus.equals("UNDER_SUBMISSION")) {
+                hasMetadata = true;
+            } else if (publicationStatus.equals("UNDER_SUMMARY_STATS_SUBMISSION")) {
+                hasSumStats = true;
+            }
+            if (submission.getStudies() != null) {
+                for (DepositionStudyDto studyDto : submission.getStudies()) {
+                    if (studyDto.getSummaryStatisticsFile() != null && !studyDto.getSummaryStatisticsFile().equals("") &&
+                            !studyDto.getSummaryStatisticsFile().equals("NR")) {
+                        hasSumStats = true;
+                    }
                 }
             }
-        }
-        if(submission.getAssociations() != null) {
-            for (DepositionAssociationDto associationDto : submission.getAssociations()) {
-                if (associationDto.getStudyTag() != null) {
-                    hasAssociations = true;
+            if (submission.getAssociations() != null) {
+                for (DepositionAssociationDto associationDto : submission.getAssociations()) {
+                    if (associationDto.getStudyTag() != null) {
+                        hasAssociations = true;
+                    }
                 }
             }
-        }
-        if(hasMetadata && hasSumStats && hasAssociations){
-            return Submission.SubmissionType.METADATA_AND_SUM_STATS_AND_TOP_ASSOCIATIONS;
-        }
-        if(hasMetadata && hasSumStats && !hasAssociations){
-            return Submission.SubmissionType.METADATA_AND_SUM_STATS;
-        }
-        if(hasMetadata && !hasSumStats && hasAssociations){
-            return Submission.SubmissionType.METADATA_AND_TOP_ASSOCIATIONS;
-        }
-        if(hasMetadata && !hasSumStats && !hasAssociations){
-            return Submission.SubmissionType.METADATA;
-        }
-        if(!hasMetadata && hasSumStats && !hasAssociations){
-            return Submission.SubmissionType.SUM_STATS;
+            if (hasMetadata && hasSumStats && hasAssociations) {
+                return Submission.SubmissionType.METADATA_AND_SUM_STATS_AND_TOP_ASSOCIATIONS;
+            }
+            if (hasMetadata && hasSumStats && !hasAssociations) {
+                return Submission.SubmissionType.METADATA_AND_SUM_STATS;
+            }
+            if (hasMetadata && !hasSumStats && hasAssociations) {
+                return Submission.SubmissionType.METADATA_AND_TOP_ASSOCIATIONS;
+            }
+            if (hasMetadata && !hasSumStats && !hasAssociations) {
+                return Submission.SubmissionType.METADATA;
+            }
+            if (!hasMetadata && hasSumStats && !hasAssociations) {
+                return Submission.SubmissionType.SUM_STATS;
+            }
         }
         return Submission.SubmissionType.UNKNOWN;
     }
