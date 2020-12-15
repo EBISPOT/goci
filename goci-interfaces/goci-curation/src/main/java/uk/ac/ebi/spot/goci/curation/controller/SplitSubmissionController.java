d8913e079543794496050bd5a683221541a05776
Tudor Groza <tudor.groza@pryzm.health>
2020-12-15 22:18:57 +0800

    Fixed readiness status for submissions. Added additional navigation tabs for submissions.

--- goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/controller/SplitSubmissionController.java
+++ goci-interfaces/goci-curation/src/main/java/uk/ac/ebi/spot/goci/curation/controller/SplitSubmissionController.java
@@ -10,14 +10,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import uk.ac.ebi.spot.goci.curation.service.deposition.DepositionSubmissionService;
+import uk.ac.ebi.spot.goci.curation.service.deposition.SubmissionImportProgressService;
 import uk.ac.ebi.spot.goci.model.deposition.Submission;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class SplitSubmissionController {
 
     private final DepositionSubmissionService submissionService;
+
+    private final SubmissionImportProgressService submissionImportProgressService;
 
     private Logger log = LoggerFactory.getLogger(getClass());
 
@@ -28,13 +32,30 @@ public class SplitSubmissionController {
     @Value("${deposition.ingest.uri}")
     private String depositionIngestURL;
 
-    public SplitSubmissionController(@Autowired DepositionSubmissionService submissionService) {
+    public SplitSubmissionController(@Autowired DepositionSubmissionService submissionService,
+                                     SubmissionImportProgressService submissionImportProgressService) {
         this.submissionService = submissionService;
+        this.submissionImportProgressService = submissionImportProgressService;
     }
 
     @RequestMapping(value = "/imported_submissions", produces = MediaType.TEXT_HTML_VALUE, method = RequestMethod.GET)
     public String importedSubmissionsPage(Model model) {
-        Map<String, Submission> submissionList = submissionService.getImportedSubmissions();
+        Map<String, Submission> submissionList = submissionService.getSubmissionsByStatus("CURATION_COMPLETE");
+        model.addAttribute("submissions", submissionList.values());
+        return "view_submissions";
+    }
+
+    @RequestMapping(value = "/in_progress_submissions", produces = MediaType.TEXT_HTML_VALUE, method = RequestMethod.GET)
+    public String inProgressSubmissionsPage(Model model) {
+        List<String> submissionIds = submissionImportProgressService.getSubmissions();
+        Map<String, Submission> submissionList = submissionService.getSubmissionsById(submissionIds);
+        model.addAttribute("submissions", submissionList.values());
+        return "view_submissions";
+    }
+
+    @RequestMapping(value = "/failed_submissions", produces = MediaType.TEXT_HTML_VALUE, method = RequestMethod.GET)
+    public String failedSubmissionsPage(Model model) {
+        Map<String, Submission> submissionList = submissionService.getSubmissionsByStatus("IMPORT_FAILED");
         model.addAttribute("submissions", submissionList.values());
         return "view_submissions";
     }
