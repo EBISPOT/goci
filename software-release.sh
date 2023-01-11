381af3a232101714911dec997ad50294926671e8
ala <63405719+ala-ebi@users.noreply.github.com>
2023-01-11 16:06:29 +0100

    3.3.9:  Release done

--- software-release.sh
+++ software-release.sh
@@ -6,7 +6,7 @@
 # Ensure all branches ready for release are merged into dev / master
 
 
-DEV_BRANCH='goci-911-autocurator-mail-spamming'
+DEV_BRANCH='goci#882-NEW-fix-Ensembl-versioning-code'
 
 MASTER_BRANCH='2.x-stable'
 
