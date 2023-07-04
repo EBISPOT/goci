42712481c6aa43ac2d8b7142d704ac5e82f8d487
sajo-ebi <77010716+sajo-ebi@users.noreply.github.com>
2023-07-04 19:31:17 +0100

    3.4.2:  Release done

--- software-release.sh
+++ software-release.sh
@@ -6,7 +6,7 @@
 # Ensure all branches ready for release are merged into dev / master
 
 
-DEV_BRANCH='gwas-ui-296-Release-Prod'
+DEV_BRANCH='gwas-ui-296-fix-solr-indexer-nullpointer'
 
 MASTER_BRANCH='2.x-stable'
 
