03e4dc97c43c63ea1f0463aca7004c1128116e6d
sajo-ebi <77010716+sajo-ebi@users.noreply.github.com>
2023-10-02 14:41:54 +0100

    goci-1122 Depo Sync mail issue fix

--- goci-tools/goci-deposition-sync/src/main/resources/scripts/run-bsub-sync-wrapper.sh
+++ goci-tools/goci-deposition-sync/src/main/resources/scripts/run-bsub-sync-wrapper.sh
@@ -1,5 +1,5 @@
-mode={1}
-
+mode=${1}
+echo "mode is ${mode}"
 if [ -z "$1" ]
 then
   bsub -M 4096 -R "rusage[mem=4096]" "/hps/software/users/parkinso/spot/gwas/prod/sw/deposition_sync/sync_wrapper.sh"
